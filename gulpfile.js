94e95dc05c93542802b051c43dfed9f135fb52c0
Yevhenii Molodyi <eugmolody@gmail.com>
2019-02-01 11:41:32 +0200

    Update Gulp pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const gulp = require('gulp')  
+const gulp = require('gulp')
   , del = require('del')
   , concat = require('gulp-concat')
   , cleanCss = require('gulp-clean-css')
@@ -25,29 +25,29 @@ gulp.task('clean', function(){
   return del(['./css/*.min.css']);
 })
 
-gulp.task('styles', ['clean'], function(){
+gulp.task('styles', gulp.series(['clean'], function(){
   return gulp.src(['./css/*.css', '!./css/*.min.css'])
     .pipe(concat('app.min.css'))
     .pipe(cleanCss())
     .pipe(rev())
     .pipe(gulp.dest('./css'));
-});
+}));
 
-gulp.task('inject-css', ['styles'], function(){
+gulp.task('inject-css', gulp.series(['styles'], function(){
    const target = gulp.src('./template/gulp.ejs')
        , source = gulp.src(['./css/*.min.css'], { read:false });
    return target
      .pipe(inject(source))
      .pipe(gulp.dest('./template'));
-});
+}));
 
-gulp.task('template', ['inject-css'], function(){
+gulp.task('template', gulp.series(['inject-css'], function(){
   return gulp
     .src('./template/gulp.ejs')
     .pipe(replace('<!-- inject:css -->',''))
     .pipe(replace('<!-- endinject  -->', ''))
     .pipe(rename('index.ejs'))
     .pipe(gulp.dest('./template'));
-})
+}))
 
-gulp.task('default', ['template']);
+gulp.task('default', gulp.series(['template']));
