775c246f2834c0c078a6d22f4c8428e3e3e7fdc5
Yevhenii Molodyi <eugmolody@gmail.com>
2020-05-06 16:47:11 +0300

    Improve adapter for stat-json data

--- js/adapters/stat-norway/fnUtil.js
+++ js/adapters/stat-norway/fnUtil.js
@@ -7,28 +7,43 @@ exports["default"] = void 0;
 
 var _compose = _interopRequireDefault(require("../../utils/compose"));
 
+var _toMonthUTC = function _toMonthUTC(str) {
+  var arrDate = str.split('M'),
+      _month = parseInt(arrDate[1], 10) - 1,
+      _day = _month === 1 ? 28 : 30;
+
+  return Date.UTC(arrDate[0], _month, _day);
+};
+
+var _toQuarterUTC = function _toQuarterUTC(quaterChart, str) {
+  var arrDate = str.split(quaterChart),
+      _month = parseInt(arrDate[1], 10) * 3 - 1;
+
+  return Date.UTC(arrDate[0], _month, 30);
+};
+
+var _toYearUTC = function _toYearUTC(str) {
+  return Date.UTC(str, 11, 31);
+};
+
 var fnUtil = {
   compose: _compose["default"],
   toUTC: function toUTC(str) {
+    str = str.toUpperCase();
+
     if (str.indexOf('M') !== -1) {
-      var arrDate = str.split('M'),
-          _month = parseInt(arrDate[1], 10) - 1,
-          _day = _month === 1 ? 28 : 30;
-
-      return Date.UTC(arrDate[0], _month, _day);
-    } else if (str.indexOf('Q') !== -1) {
-      var _arrDate = str.split('Q'),
-          _month2 = parseInt(_arrDate[1], 10) * 3 - 1;
-
-      return Date.UTC(_arrDate[0], _month2, 30);
-    } else if (str.indexOf('K') !== -1) {
-      var _arrDate2 = str.split('K'),
-          _month3 = parseInt(_arrDate2[1], 10) * 3 - 1;
-
-      return Date.UTC(_arrDate2[0], _month3, 30);
-    } else {
-      return Date.UTC(str, 11, 31);
+      return _toMonthUTC(str);
     }
+
+    if (str.indexOf('Q') !== -1) {
+      return _toQuarterUTC('Q', str);
+    }
+
+    if (str.indexOf('K') !== -1) {
+      return _toQuarterUTC('K', str);
+    }
+
+    return _toYearUTC(str);
   },
   toYMD: function toYMD(str) {
     var ms = fnUtil.toUTC(str),
--- src/adapters/stat-norway/fnUtil.js
+++ src/adapters/stat-norway/fnUtil.js
@@ -1,28 +1,35 @@
 
 import compose from '../../utils/compose'
 
+const _toMonthUTC = str => {
+  const arrDate = str.split('M')
+  , _month = parseInt(arrDate[1], 10)-1
+  , _day = (_month === 1) ? 28 : 30;
+  return Date.UTC(arrDate[0], _month, _day);
+};
+
+const _toQuarterUTC = (quaterChart, str) => {
+  const arrDate = str.split(quaterChart)
+  , _month = (parseInt(arrDate[1], 10)*3) - 1;
+  return Date.UTC(arrDate[0], _month, 30);
+};
+
+const _toYearUTC = str => Date.UTC(str, 11, 31);
+
 const fnUtil = {
   compose,
   toUTC: (str) => {
-      if (str.indexOf('M') !== -1) {
-        const arrDate = str.split('M')
-            , _month = parseInt(arrDate[1], 10)-1
-            , _day = (_month === 1) ? 28 : 30
-
-        return Date.UTC(arrDate[0], _month, _day);
-      } else if (str.indexOf('Q') !== -1) {
-        const arrDate = str.split('Q')
-            , _month = (parseInt(arrDate[1], 10)*3) - 1
-
-        return Date.UTC(arrDate[0], _month, 30);
-      } else if (str.indexOf('K') !== -1) {
-        const arrDate = str.split('K')
-            , _month = (parseInt(arrDate[1], 10)*3) - 1
-
-        return Date.UTC(arrDate[0], _month, 30);
-      } else {
-        return Date.UTC(str, 11, 31);
-      }
+    str = str.toUpperCase()
+    if (str.indexOf('M') !== -1) {
+      return _toMonthUTC(str);
+    }
+    if (str.indexOf('Q') !== -1) {
+      return _toQuarterUTC('Q', str);
+    }
+    if (str.indexOf('K') !== -1) {
+      return _toQuarterUTC('K', str);
+    }
+    return _toYearUTC(str);
   },
 
   toYMD: (str) => {
