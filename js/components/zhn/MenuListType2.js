fc64029b78b957713b2e7ca81b6185c19f12419d
Yevhenii Molodyi <eugmolody@gmail.com>
2020-06-24 20:06:37 +0300

    Replace MenuListType2 by functional component implementation

--- js/components/zhn/MenuListType2.js
+++ js/components/zhn/MenuListType2.js
@@ -1,15 +1,11 @@
 "use strict";
 
-var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard");
-
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 exports.__esModule = true;
 exports["default"] = void 0;
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
-
-var _react = _interopRequireWildcard(require("react"));
+var _react = _interopRequireDefault(require("react"));
 
 var _Color = _interopRequireDefault(require("../styles/Color"));
 
@@ -52,107 +48,77 @@ var STYLE = {
   }
 };
 
-var MenuListType2 = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(MenuListType2, _Component);
-
-  function MenuListType2() {
-    var _this;
-
-    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
-
-    _this = _Component.call.apply(_Component, [this].concat(args)) || this;
-    _this.state = {};
-
-    _this._renderLevel3 = function (items, captionProp) {
-      if (items === void 0) {
-        items = [];
-      }
-
-      var _this$props = _this.props,
-          itemClassName = _this$props.itemClassName,
-          ItemComp = _this$props.ItemComp,
-          onClickItem = _this$props.onClickItem;
-      return items.map(function (item, index) {
-        var caption = item[captionProp];
-        return /*#__PURE__*/_react["default"].createElement(ItemComp, {
-          key: index,
-          className: itemClassName,
-          caption: caption,
-          item: item,
-          onClickItem: onClickItem
-        });
-      });
-    };
-
-    _this._renderLevel2 = function (lists, captionProp, itemsProp) {
-      if (lists === void 0) {
-        lists = [];
-      }
-
-      return lists.map(function (list, index) {
-        var caption = list[captionProp],
-            items = list[itemsProp];
-        return /*#__PURE__*/_react["default"].createElement(_OpenClose["default"], {
-          key: index,
-          style: STYLE.LIST_DIV,
-          openColor: LIST_OPEN_COLOR,
-          caption: caption
-        }, _this._renderLevel3(items, captionProp));
-      });
-    };
-
-    _this._renderLevel1 = function (model) {
-      if (model === void 0) {
-        model = {};
-      }
-
-      var _model = model,
-          _model$meta = _model.meta,
-          meta = _model$meta === void 0 ? {} : _model$meta,
-          caption = meta.caption,
-          level1 = meta.level1,
-          level2 = meta.level2,
-          level3 = meta.level3,
-          _captionProp = caption || MODEL_PROP.CAPTION,
-          _groupsProp = level1 || MODEL_PROP.GROUPS,
-          _listsProp = level2 || MODEL_PROP.LISTS,
-          _itemsProp = level3 || MODEL_PROP.ITEMS,
-          groups = model[_groupsProp] || [];
-
-      return groups.map(function (group, index) {
-        var caption = group[_captionProp],
-            lists = group[_listsProp];
-        return /*#__PURE__*/_react["default"].createElement(_OpenClose["default"], {
-          key: index,
-          style: STYLE.GROUP_DIV,
-          openColor: GROUP_OPEN_COLOR,
-          caption: caption
-        }, _this._renderLevel2(lists, _captionProp, _itemsProp));
-      });
-    };
-
-    return _this;
+var _renderLevel3 = function _renderLevel3(items, captionProp, props) {
+  if (items === void 0) {
+    items = [];
   }
 
-  var _proto = MenuListType2.prototype;
+  var itemClassName = props.itemClassName,
+      ItemComp = props.ItemComp,
+      onClickItem = props.onClickItem;
+  return items.map(function (item, index) {
+    var caption = item[captionProp];
+    return /*#__PURE__*/_react["default"].createElement(ItemComp, {
+      key: index,
+      className: itemClassName,
+      caption: caption,
+      item: item,
+      onClickItem: onClickItem
+    });
+  });
+};
 
-  _proto.shouldComponentUpdate = function shouldComponentUpdate(nextProps, nextState) {
-    if (this.state === nextState && this.props.model === nextProps.model) {
-      return false;
-    }
+var _renderLevel2 = function _renderLevel2(lists, captionProp, itemsProp, props) {
+  if (lists === void 0) {
+    lists = [];
+  }
 
-    return true;
-  };
+  return lists.map(function (list, index) {
+    var caption = list[captionProp],
+        items = list[itemsProp];
+    return /*#__PURE__*/_react["default"].createElement(_OpenClose["default"], {
+      key: index,
+      style: STYLE.LIST_DIV,
+      openColor: LIST_OPEN_COLOR,
+      caption: caption
+    }, _renderLevel3(items, captionProp, props));
+  });
+};
 
-  _proto.render = function render() {
-    var model = this.props.model;
-    return /*#__PURE__*/_react["default"].createElement("div", null, this._renderLevel1(model));
-  };
+var _renderLevel1 = function _renderLevel1(props) {
+  var _props$model = props.model,
+      model = _props$model === void 0 ? {} : _props$model,
+      _model$meta = model.meta,
+      meta = _model$meta === void 0 ? {} : _model$meta,
+      caption = meta.caption,
+      level1 = meta.level1,
+      level2 = meta.level2,
+      level3 = meta.level3,
+      _captionProp = caption || MODEL_PROP.CAPTION,
+      _groupsProp = level1 || MODEL_PROP.GROUPS,
+      _listsProp = level2 || MODEL_PROP.LISTS,
+      _itemsProp = level3 || MODEL_PROP.ITEMS,
+      groups = model[_groupsProp] || [];
 
-  return MenuListType2;
-}(_react.Component);
+  return groups.map(function (group, index) {
+    var caption = group[_captionProp],
+        lists = group[_listsProp];
+    return /*#__PURE__*/_react["default"].createElement(_OpenClose["default"], {
+      key: index,
+      style: STYLE.GROUP_DIV,
+      openColor: GROUP_OPEN_COLOR,
+      caption: caption
+    }, _renderLevel2(lists, _captionProp, _itemsProp, props));
+  });
+};
+
+var _areEqual = function _areEqual(prevProps, nextProps) {
+  return prevProps.model === nextProps.model;
+};
+
+var MenuListType2 = /*#__PURE__*/_react["default"].memo(function (props) {
+  return /*#__PURE__*/_react["default"].createElement("div", null, _renderLevel1(props));
+}, _areEqual);
 
 var _default = MenuListType2;
 exports["default"] = _default;
--- src/components/zhn/MenuListType2.jsx
+++ src/components/zhn/MenuListType2.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import C from '../styles/Color'
 import OpenClose2 from './OpenClose2'
@@ -42,83 +42,70 @@ const STYLE = {
   }
 };
 
+const _renderLevel3 = (items=[], captionProp, props) => {
+  const { itemClassName, ItemComp, onClickItem } = props;
+  return items.map((item, index) => {
+    const caption  = item[captionProp];
+    return (
+      <ItemComp
+         key={index}
+         className={itemClassName}
+         caption={caption}
+         item={item}
+         onClickItem={onClickItem}
+      />
+    );
+  })
+}
 
-class MenuListType2 extends Component {
-  state = {}
+const _renderLevel2 = (lists=[], captionProp, itemsProp, props) => {
+  return lists.map((list, index) => {
+    const caption  = list[captionProp]
+        , items = list[itemsProp];
+    return (
+      <OpenClose2
+         key={index}
+         style={STYLE.LIST_DIV}
+         openColor={LIST_OPEN_COLOR}
+         caption={caption}
+      >
+        {_renderLevel3(items, captionProp, props)}
+      </OpenClose2>
+    );
+  })
+};
 
-  shouldComponentUpdate(nextProps, nextState){
-      if (this.state === nextState && this.props.model === nextProps.model){
-        return false;
-      }
-      return true;
-  }
+const _renderLevel1 = (props) => {
+   const { model={} } = props
+   , { meta={} } = model
+   , { caption, level1, level2, level3 } = meta
+   , _captionProp = caption || MODEL_PROP.CAPTION
+   , _groupsProp = level1 || MODEL_PROP.GROUPS
+   , _listsProp = level2 || MODEL_PROP.LISTS
+   , _itemsProp = level3 || MODEL_PROP.ITEMS
+   , groups = model[_groupsProp] || [];
 
- _renderLevel3 = (items=[], captionProp) => {
-   const { itemClassName, ItemComp, onClickItem } = this.props;
-   return items.map((item, index) => {
-     const caption  = item[captionProp];
-     return (
-       <ItemComp
-          key={index}
-          className={itemClassName}
-          caption={caption}
-          item={item}
-          onClickItem={onClickItem}
-       />
-     );
-   })
- }
-
-  _renderLevel2 = (lists=[], captionProp, itemsProp) => {
-    return lists.map((list, index) => {
-      const caption  = list[captionProp]
-          , items = list[itemsProp];
+   return groups.map((group, index) => {
+      const caption  = group[_captionProp]
+      , lists = group[_listsProp]
       return (
         <OpenClose2
            key={index}
-           style={STYLE.LIST_DIV}
-           openColor={LIST_OPEN_COLOR}
+           style={STYLE.GROUP_DIV}
+           openColor={GROUP_OPEN_COLOR}
            caption={caption}
         >
-          {this._renderLevel3(items, captionProp)}
+          {_renderLevel2(lists, _captionProp, _itemsProp, props)}
         </OpenClose2>
       );
-    })
-  }
+   })
+};
 
-  _renderLevel1 = (model={}) => {
-     const { meta={} } = model
-     , { caption, level1, level2, level3 } = meta
-     , _captionProp = caption || MODEL_PROP.CAPTION
-     , _groupsProp = level1 || MODEL_PROP.GROUPS
-     , _listsProp = level2 || MODEL_PROP.LISTS
-     , _itemsProp = level3 || MODEL_PROP.ITEMS
-     , groups = model[_groupsProp] || [];
-
-     return groups.map((group, index) => {
-        const caption  = group[_captionProp]
-        , lists = group[_listsProp]
-        return (
-          <OpenClose2
-             key={index}
-             style={STYLE.GROUP_DIV}
-             openColor={GROUP_OPEN_COLOR}
-             caption={caption}
-          >
-            {this._renderLevel2(lists, _captionProp, _itemsProp)}
-          </OpenClose2>
-        );
-     })
-  }
-
-  render(){
-    const { model } = this.props;
-    return (
-      <div>
-         {this._renderLevel1(model)}
-      </div>
-    )
-  }
-}
+const _areEqual = (prevProps, nextProps) => prevProps.model === nextProps.model;
+const MenuListType2 = React.memo(props => (
+  <div>
+     {_renderLevel1(props)}
+  </div>
+), _areEqual);
 
 export default MenuListType2
