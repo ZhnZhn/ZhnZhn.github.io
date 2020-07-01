c1cbff077ddc94cc19d2872bafee57c5a3e3794f
Yevhenii Molodyi <eugmolody@gmail.com>
2020-07-01 12:46:59 +0300

    Cover getV util by unit tests

--- js/utils/__tests__/getV.test.js
+++ js/utils/__tests__/getV.test.js
@@ -16,9 +16,6 @@ describe('getV', function () {
     expect(fn({
       value: 0
     })).toBe('0');
-    expect(fn({})).toBe('');
-    expect(fn()).toBe('');
-    expect(fn(null)).toBe('');
   });
   test('should return string item value by item short prop name v', function () {
     expect(fn({
@@ -44,6 +41,43 @@ describe('getV', function () {
     expect(fn({
       value: 0
     }, option)).toBe('0');
+  });
+  test('should return option dfValue or empty str in case value null or undefined', function () {
+    var option = {
+      dfValue: 'dfValue'
+    };
+    expect(fn({
+      value: void 0
+    }, option)).toBe('dfValue');
+    expect(fn({
+      v: void 0
+    }, option)).toBe('dfValue');
+    expect(fn({
+      value: null
+    }, option)).toBe('dfValue');
+    expect(fn({
+      v: null
+    }, option)).toBe('dfValue');
+    expect(fn({
+      value: void 0
+    })).toBe('');
+    expect(fn({
+      v: void 0
+    })).toBe('');
+    expect(fn({
+      value: null
+    })).toBe('');
+    expect(fn({
+      v: null
+    })).toBe('');
+  });
+  test('should retun empty string for edge case', function () {
+    var option = {
+      isUpper: true
+    };
+    expect(fn({})).toBe('');
+    expect(fn()).toBe('');
+    expect(fn(null)).toBe('');
     expect(fn({}, option)).toBe('');
     expect(fn(void 0, option)).toBe('');
     expect(fn(null, option)).toBe('');
--- src/utils/__tests__/getV.test.js
+++ src/utils/__tests__/getV.test.js
@@ -6,10 +6,6 @@ describe('getV', ()=>{
     expect(fn({ value: 'Abc'})).toBe('Abc')
     expect(fn({ value: '0'})).toBe('0')
     expect(fn({ value: 0})).toBe('0')
-
-    expect(fn({})).toBe('')
-    expect(fn()).toBe('')
-    expect(fn(null)).toBe('')
   })
   test('should return string item value by item short prop name v', ()=>{
     expect(fn({ v: 'Abc'})).toBe('Abc')
@@ -22,6 +18,25 @@ describe('getV', ()=>{
     expect(fn({ value: 'Abc'}, option)).toBe('ABC')
     expect(fn({ value: '0'}, option)).toBe('0')
     expect(fn({ value: 0}, option)).toBe('0')
+  })
+  test('should return option dfValue or empty str in case value null or undefined', ()=>{
+    const option = { dfValue: 'dfValue' };
+    expect(fn({ value: void 0 }, option)).toBe('dfValue')
+    expect(fn({ v: void 0 }, option)).toBe('dfValue')
+    expect(fn({ value: null }, option)).toBe('dfValue')
+    expect(fn({ v: null }, option)).toBe('dfValue')
+
+    expect(fn({ value: void 0 })).toBe('')
+    expect(fn({ v: void 0 })).toBe('')
+    expect(fn({ value: null })).toBe('')
+    expect(fn({ v: null })).toBe('')
+  })
+
+  test('should retun empty string for edge case', () => {
+    const option = { isUpper: true };
+    expect(fn({})).toBe('')
+    expect(fn()).toBe('')
+    expect(fn(null)).toBe('')
 
     expect(fn({}, option)).toBe('')
     expect(fn(void 0, option)).toBe('')
