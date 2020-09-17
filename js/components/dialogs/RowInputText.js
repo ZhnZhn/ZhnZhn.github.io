3c4719721a5e6fea6f939b2815f3994e18290294
Yevhenii Molodyi <eugmolody@gmail.com>
2020-09-17 13:11:14 +0300

    Replace dialogs' RowInputText by react hooks implementation

--- js/components/dialogs/RowInputText.js
+++ js/components/dialogs/RowInputText.js
@@ -1,7 +1,5 @@
 "use strict";
 
-var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard");
-
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 exports.__esModule = true;
@@ -11,9 +9,7 @@ var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends")
 
 var _objectWithoutPropertiesLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/objectWithoutPropertiesLoose"));
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
-
-var _react = _interopRequireWildcard(require("react"));
+var _react = _interopRequireDefault(require("react"));
 
 var _InputText = _interopRequireDefault(require("../zhn/InputText"));
 
@@ -40,62 +36,42 @@ var S = {
 };
 var COLLON = ':';
 
-var RowInputText = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(RowInputText, _Component);
+var RowInputText = /*#__PURE__*/_react["default"].forwardRef(function (props, ref) {
+  var styleRoot = props.styleRoot,
+      rootStyle = props.rootStyle,
+      captionStyle = props.captionStyle,
+      styleCaption = props.styleCaption,
+      _props$caption = props.caption,
+      caption = _props$caption === void 0 ? 'Input' : _props$caption,
+      inputStyle = props.inputStyle,
+      styleInput = props.styleInput,
+      rest = (0, _objectWithoutPropertiesLoose2["default"])(props, ["styleRoot", "rootStyle", "captionStyle", "styleCaption", "caption", "inputStyle", "styleInput"]),
+      _rootStyle = rootStyle || (0, _extends2["default"])({}, S.ROOT, styleRoot),
+      _captionStyle = captionStyle || (0, _extends2["default"])({}, S.CAPTION, styleCaption),
+      _inputStyle = inputStyle || (0, _extends2["default"])({}, S.INPUT_TEXT, styleInput),
+      _caption = caption.indexOf(COLLON) === -1 ? caption + COLLON : caption;
 
-  function RowInputText() {
-    var _this;
+  return /*#__PURE__*/_react["default"].createElement("div", {
+    style: _rootStyle
+  }, /*#__PURE__*/_react["default"].createElement("label", null, /*#__PURE__*/_react["default"].createElement("span", {
+    style: _captionStyle
+  }, _caption), /*#__PURE__*/_react["default"].createElement(_InputText["default"], (0, _extends2["default"])({
+    ref: ref,
+    style: _inputStyle
+  }, rest))));
+});
+/*
+RowInputText.propTypes= {
+  styleRoot: PropTypes.object,
+  styleCaption: PropTypes.object,
+  styleInput: PropTypes.object,
+  caption: PropTypes.string,
+  initValue: PropTypes.string,
+  onEnter: PropTypes.func
+}
+*/
 
-    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
 
-    _this = _Component.call.apply(_Component, [this].concat(args)) || this;
-
-    _this._refInput = function (node) {
-      return _this._inputComp = node;
-    };
-
-    return _this;
-  }
-
-  var _proto = RowInputText.prototype;
-
-  _proto.render = function render() {
-    var _this$props = this.props,
-        styleRoot = _this$props.styleRoot,
-        rootStyle = _this$props.rootStyle,
-        captionStyle = _this$props.captionStyle,
-        styleCaption = _this$props.styleCaption,
-        caption = _this$props.caption,
-        inputStyle = _this$props.inputStyle,
-        styleInput = _this$props.styleInput,
-        rest = (0, _objectWithoutPropertiesLoose2["default"])(_this$props, ["styleRoot", "rootStyle", "captionStyle", "styleCaption", "caption", "inputStyle", "styleInput"]),
-        _rootStyle = rootStyle || (0, _extends2["default"])({}, S.ROOT, styleRoot),
-        _captionStyle = captionStyle || (0, _extends2["default"])({}, S.CAPTION, styleCaption),
-        _inputStyle = inputStyle || (0, _extends2["default"])({}, S.INPUT_TEXT, styleInput),
-        _caption = caption.indexOf(COLLON) === -1 ? caption + COLLON : caption;
-
-    return /*#__PURE__*/_react["default"].createElement("div", {
-      style: _rootStyle
-    }, /*#__PURE__*/_react["default"].createElement("label", null, /*#__PURE__*/_react["default"].createElement("span", {
-      style: _captionStyle
-    }, _caption), /*#__PURE__*/_react["default"].createElement(_InputText["default"], (0, _extends2["default"])({
-      ref: this._refInput,
-      style: _inputStyle
-    }, rest))));
-  };
-
-  _proto.getValue = function getValue() {
-    return this._inputComp.getValue();
-  };
-
-  return RowInputText;
-}(_react.Component);
-
-RowInputText.defaultProps = {
-  caption: 'Input'
-};
 var _default = RowInputText;
 exports["default"] = _default;
 //# sourceMappingURL=RowInputText.js.map
--- src/components/dialogs/RowInputText.jsx
+++ src/components/dialogs/RowInputText.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 //import PropTypes from "prop-types";
 
 import InputText from '../zhn/InputText'
@@ -26,59 +26,48 @@ const S = {
 
 const COLLON = ':';
 
-class RowInputText extends Component {
-  /*
-  static propTypes={
-    styleRoot: PropTypes.object,
-    styleCaption: PropTypes.object,
-    styleInput: PropTypes.object,
-    caption: PropTypes.string,
-    initValue: PropTypes.string,
-    onEnter: PropTypes.func
-  }
-  */
-  static defaultProps = {
-    caption: 'Input'
-  }
+const RowInputText = React.forwardRef((props, ref) => {
+  const {
+    styleRoot, rootStyle,
+    captionStyle, styleCaption,
+    caption='Input',
+    inputStyle, styleInput,
+    ...rest
+  } = props
+  , _rootStyle = rootStyle
+       || {...S.ROOT, ...styleRoot}
+  , _captionStyle = captionStyle
+       || {...S.CAPTION, ...styleCaption}
+  , _inputStyle = inputStyle
+       || {...S.INPUT_TEXT, ...styleInput}
+  , _caption = caption.indexOf(COLLON) === -1
+      ? caption + COLLON
+      : caption;
+  return (
+    <div style={_rootStyle}>
+      <label>
+        <span style={_captionStyle}>
+          {_caption}
+        </span>
+        <InputText
+           ref={ref}
+           style={_inputStyle}
+           {...rest}
+        />
+      </label>
+    </div>
+  );
+})
 
-  _refInput = (node) => this._inputComp = node
-
-  render(){
-    const {
-      styleRoot, rootStyle,
-      captionStyle, styleCaption,
-      caption,
-      inputStyle, styleInput,
-      ...rest
-    } = this.props
-    , _rootStyle = rootStyle
-         || {...S.ROOT, ...styleRoot}
-    , _captionStyle = captionStyle
-         || {...S.CAPTION, ...styleCaption}
-    , _inputStyle = inputStyle
-         || {...S.INPUT_TEXT, ...styleInput}
-    , _caption = caption.indexOf(COLLON) === -1
-        ? caption + COLLON
-        : caption;
-    return (
-      <div style={_rootStyle}>
-        <label>
-          <span style={_captionStyle}>
-            {_caption}
-          </span>
-          <InputText
-             ref={this._refInput}
-             style={_inputStyle}
-             {...rest}
-          />
-        </label>
-      </div>
-    );
-  }
-
-  getValue(){
-    return this._inputComp.getValue();
-  }
+/*
+RowInputText.propTypes= {
+  styleRoot: PropTypes.object,
+  styleCaption: PropTypes.object,
+  styleInput: PropTypes.object,
+  caption: PropTypes.string,
+  initValue: PropTypes.string,
+  onEnter: PropTypes.func
 }
+*/
 
 export default RowInputText
