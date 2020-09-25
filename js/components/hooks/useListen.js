781cf9ed97c3e11021a312d0452531baff71c651
Yevhenii Molodyi <eugmolody@gmail.com>
2020-09-25 15:39:59 +0300

    Edit useListen

--- js/components/hooks/useListen.js
+++ js/components/hooks/useListen.js
@@ -6,12 +6,14 @@ exports["default"] = void 0;
 var _react = require("react");
 
 /*eslint-disable react-hooks/exhaustive-deps */
-var useListen = function useListen(store, onStore) {
+var useListen = function useListen(store, onStore, propNameListen) {
+  if (propNameListen === void 0) {
+    propNameListen = 'listen';
+  }
+
   (0, _react.useEffect)(function () {
-    var unsubscribe = store.listen(onStore);
-    return function () {
-      unsubscribe();
-    };
+    var unsubscribe = store[propNameListen](onStore);
+    return unsubscribe;
   }, []);
 };
 /*eslint-enable react-hooks/exhaustive-deps */
--- src/components/hooks/useListen.js
+++ src/components/hooks/useListen.js
@@ -1,12 +1,10 @@
 import { useEffect } from 'react'
 
 /*eslint-disable react-hooks/exhaustive-deps */
-const useListen = (store, onStore) => {
+const useListen = (store, onStore, propNameListen='listen') => {
   useEffect(() => {
-    const unsubscribe = store.listen(onStore)
-    return () => {
-      unsubscribe()
-    }
+    const unsubscribe = store[propNameListen](onStore)
+    return unsubscribe;
   }, [])
 }
 /*eslint-enable react-hooks/exhaustive-deps */
