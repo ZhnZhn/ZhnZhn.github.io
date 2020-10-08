55564748766c5a18dcb6be49cd06a261ae04128a
Yevhenii Molodyi <eugmolody@gmail.com>
2020-10-08 17:26:14 +0300

    Improve details

--- src/components/AppErc.jsx
+++ src/components/AppErc.jsx
@@ -21,7 +21,7 @@ import ThemeContext from './hoc/ThemeContext'
 
 import checkBuild from './checkBuild'
 
-const BUILD_DATE = '01-10-2020';
+const BUILD_DATE = '08-10-2020';
 const CL = "component-container"
 
 const showSettings = CA.showSettings
