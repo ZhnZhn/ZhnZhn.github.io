d4f315bc371feaf7cf75af08ce6a399112526719
Yevhenii Molodyi <eugmolody@gmail.com>
2021-02-11 16:30:33 +0200

    Replace PaneOptions by react custom hooks implementation

--- js/components/header/PaneOptions.js
+++ js/components/header/PaneOptions.js
@@ -7,17 +7,15 @@ exports["default"] = void 0;
 
 var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
-
 var _jsxRuntime = require("react/jsx-runtime.js");
 
 var _react = require("react");
 
 var _highcharts = _interopRequireDefault(require("highcharts"));
 
-var _safeFn = _interopRequireDefault(require("../../utils/safeFn"));
+var _ThemeContext = _interopRequireDefault(require("../hoc/ThemeContext"));
 
-var _withTheme = _interopRequireDefault(require("../hoc/withTheme"));
+var _safeFn = _interopRequireDefault(require("../../utils/safeFn"));
 
 var _DialogCell = _interopRequireDefault(require("../dialogs/DialogCell"));
 
@@ -44,9 +42,7 @@ var UI_THEME_OPTIONS = [{
   caption: 'Sand Light',
   value: 'SAND_L'
 }];
-var SET = {
-  PROXY: 'setProxy'
-};
+var SET_PROXY = 'setProxy';
 var MODE_ADMIN = 'isAdminMode';
 var MODE_DELTA = 'isDrawDeltaExtrems';
 var MODE_ZOOM = 'isNotZoomToMinMax';
@@ -69,123 +65,110 @@ var _crHaloOption = function _crHaloOption(is) {
   };
 };
 
-var PaneOptions = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(PaneOptions, _Component);
+var _useProxy = function _useProxy(data) {
+  var _refProxy = (0, _react.useRef)(),
+      _setProxy = (0, _safeFn["default"])(data, SET_PROXY),
+      _proxy = data.getProxy();
 
-  /*
-  static propTypes = {
-    titleStyle: PropTypes.object,
-    btStyle: PropTypes.object,
-    data: PropTypes.object,
-    onClose: PropTypes.func
-  }
-  */
-  function PaneOptions(props) {
-    var _this;
+  return [_refProxy, _proxy, _setProxy, function () {
+    return _setProxy(_refProxy.current.getValue());
+  }, function () {
+    return _setProxy('');
+  }];
+};
 
-    _this = _Component.call(this, props) || this;
+var _useTheme = function _useTheme(onChangeTheme) {
+  var theme = (0, _react.useContext)(_ThemeContext["default"]);
+  return function (item) {
+    if (item && theme.getThemeName() !== item.value) {
+      theme.setThemeName(item.value);
+      onChangeTheme(item.value);
+    }
+  };
+};
 
-    _this._hMode = function (fnName, mode) {
-      var data = _this.props.data,
-          fnMode = (0, _safeFn["default"])(data, fnName);
-      fnMode(mode);
-    };
+var PaneOptions = function PaneOptions(_ref) {
+  var titleStyle = _ref.titleStyle,
+      btStyle = _ref.btStyle,
+      data = _ref.data,
+      onClose = _ref.onClose,
+      onChangeTheme = _ref.onChangeTheme;
 
-    _this._hSetProxy = function () {
-      _this._setProxy(_this.proxyComp.getValue());
-    };
-
-    _this._hClearProxy = function () {
-      _this._setProxy('');
-    };
-
-    _this._hSelectTheme = function (item) {
-      var _this$props = _this.props,
-          theme = _this$props.theme,
-          onChangeTheme = _this$props.onChangeTheme;
-
-      if (item && theme.getThemeName() !== item.value) {
-        theme.setThemeName(item.value);
-        onChangeTheme(item.value);
-      }
-    };
-
-    _this._setHalo = function (is) {
-      _highcharts["default"].setOptions(_crHaloOption(is));
-    };
-
-    _this._refProxy = function (n) {
-      return _this.proxyComp = n;
-    };
-
-    var _data = props.data;
-    _this._setProxy = (0, _safeFn["default"])(_data, SET.PROXY);
-    return _this;
-  }
-
-  var _proto = PaneOptions.prototype;
-
-  _proto.render = function render() {
-    var _this$props2 = this.props,
-        titleStyle = _this$props2.titleStyle,
-        btStyle = _this$props2.btStyle,
-        data = _this$props2.data,
-        onClose = _this$props2.onClose,
-        _proxy = data.getProxy(),
-        _isAdminMode = (0, _safeFn["default"])(data, MODE_ADMIN, false)(),
-        _isDrawDeltaExtrems = (0, _safeFn["default"])(data, MODE_DELTA, false)(),
-        _isNotZoomToMinMax = (0, _safeFn["default"])(data, MODE_ZOOM, false)();
-
-    return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
-      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_DialogCell["default"].RowPattern, {
-        ref: this._refProxy,
-        captionStyle: titleStyle,
-        caption: "Proxy:",
-        placeholder: "Https Proxy Server",
-        initValue: _proxy,
-        onEnter: this._setProxy,
-        onClear: this._hClearProxy
-      }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_DialogCell["default"].RowInputSelect, {
-        caption: "UI Theme",
-        captionStyle: titleStyle,
-        options: UI_THEME_OPTIONS,
-        onSelect: this._hSelectTheme
-      }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_DialogCell["default"].RowCheckBox, {
-        initValue: _isAdminMode,
-        caption: "View in Admin Mode",
-        onCheck: this._hMode.bind(null, MODE_ADMIN, true),
-        onUnCheck: this._hMode.bind(null, MODE_ADMIN, false)
-      }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_DialogCell["default"].RowCheckBox, {
-        initValue: _isDrawDeltaExtrems,
-        caption: "Draw Deltas to Min-Max",
-        onCheck: this._hMode.bind(null, MODE_DELTA, true),
-        onUnCheck: this._hMode.bind(null, MODE_DELTA, false)
-      }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_DialogCell["default"].RowCheckBox, {
-        initValue: _isNotZoomToMinMax,
-        caption: "Not Zoom to Min-Max",
-        onCheck: this._hMode.bind(null, MODE_ZOOM, true),
-        onUnCheck: this._hMode.bind(null, MODE_ZOOM, false)
-      }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_DialogCell["default"].RowCheckBox, {
-        initValue: false,
-        caption: "Without Points Halo",
-        onCheck: this._setHalo.bind(null, false),
-        onUnCheck: this._setHalo.bind(null, true)
-      }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_RowButtons["default"], {
-        btStyle: btStyle,
-        onClose: onClose,
-        children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_FlatButton["default"], {
-          style: (0, _extends2["default"])({}, btStyle, S.BT_PROXY),
-          caption: "SET PROXY",
-          onClick: this._hSetProxy
-        })
-      })]
-    });
+  var _useProxy2 = _useProxy(data),
+      _refProxy = _useProxy2[0],
+      _proxy = _useProxy2[1],
+      _setProxy = _useProxy2[2],
+      _hSetProxy = _useProxy2[3],
+      _hClearProxy = _useProxy2[4],
+      _hSelectTheme = _useTheme(onChangeTheme),
+      _hMode = function _hMode(fnName, mode) {
+    return (0, _safeFn["default"])(data, fnName)(mode);
+  },
+      _hSetHalo = function _hSetHalo(is) {
+    return _highcharts["default"].setOptions(_crHaloOption(is));
   };
 
-  return PaneOptions;
-}(_react.Component);
+  var _isAdminMode = (0, _safeFn["default"])(data, MODE_ADMIN, false)(),
+      _isDrawDeltaExtrems = (0, _safeFn["default"])(data, MODE_DELTA, false)(),
+      _isNotZoomToMinMax = (0, _safeFn["default"])(data, MODE_ZOOM, false)();
 
-var _default = (0, _withTheme["default"])(PaneOptions);
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+    children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_DialogCell["default"].RowPattern, {
+      ref: _refProxy,
+      captionStyle: titleStyle,
+      caption: "Proxy",
+      placeholder: "Https Proxy Server",
+      initValue: _proxy,
+      onEnter: _setProxy,
+      isClearBlank: true,
+      onClear: _hClearProxy
+    }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_DialogCell["default"].RowInputSelect, {
+      caption: "UI Theme",
+      captionStyle: titleStyle,
+      options: UI_THEME_OPTIONS,
+      onSelect: _hSelectTheme
+    }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_DialogCell["default"].RowCheckBox, {
+      initValue: _isAdminMode,
+      caption: "View in Admin Mode",
+      onCheck: _hMode.bind(null, MODE_ADMIN, true),
+      onUnCheck: _hMode.bind(null, MODE_ADMIN, false)
+    }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_DialogCell["default"].RowCheckBox, {
+      initValue: _isDrawDeltaExtrems,
+      caption: "Draw Deltas to Min-Max",
+      onCheck: _hMode.bind(null, MODE_DELTA, true),
+      onUnCheck: _hMode.bind(null, MODE_DELTA, false)
+    }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_DialogCell["default"].RowCheckBox, {
+      initValue: _isNotZoomToMinMax,
+      caption: "Not Zoom to Min-Max",
+      onCheck: _hMode.bind(null, MODE_ZOOM, true),
+      onUnCheck: _hMode.bind(null, MODE_ZOOM, false)
+    }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_DialogCell["default"].RowCheckBox, {
+      initValue: false,
+      caption: "Without Points Halo",
+      onCheck: _hSetHalo.bind(null, false),
+      onUnCheck: _hSetHalo.bind(null, true)
+    }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_RowButtons["default"], {
+      btStyle: btStyle,
+      onClose: onClose,
+      children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_FlatButton["default"], {
+        style: (0, _extends2["default"])({}, btStyle, S.BT_PROXY),
+        caption: "SET PROXY",
+        onClick: _hSetProxy
+      })
+    })]
+  });
+};
+/*
+PaneOptions.propTypes = {
+  titleStyle: PropTypes.object,
+  btStyle: PropTypes.object,
+  data: PropTypes.object,
+  onClose: PropTypes.func,
+  onChangeTheme: PropTypes.func
+}
+*/
 
+
+var _default = PaneOptions;
 exports["default"] = _default;
 //# sourceMappingURL=PaneOptions.js.map
--- src/components/header/PaneOptions.jsx
+++ src/components/header/PaneOptions.jsx
@@ -1,10 +1,9 @@
-import { Component } from 'react'
-import Highcharts from 'highcharts'
 //import PropTypes from 'prop-types'
+import { useRef, useContext } from 'react'
+import Highcharts from 'highcharts'
 
+import ThemeContext  from '../hoc/ThemeContext'
 import safeFn from '../../utils/safeFn'
-
-import withTheme from '../hoc/withTheme'
 
 import D from '../dialogs/DialogCell'
 import FlatButton from '../zhn-m/FlatButton'
@@ -23,10 +22,8 @@ const UI_THEME_OPTIONS = [
   { caption: 'Sand Light', value: 'SAND_L' }
 ];
 
-const SET = {
-  PROXY: 'setProxy'
-};
 
+const SET_PROXY = 'setProxy'
 const MODE_ADMIN = 'isAdminMode';
 const MODE_DELTA = 'isDrawDeltaExtrems';
 const MODE_ZOOM = 'isNotZoomToMinMax';
@@ -43,109 +40,109 @@ const _crHaloOption = (is=false) => ({
   }
 });
 
-class PaneOptions extends Component {
-  /*
-  static propTypes = {
-    titleStyle: PropTypes.object,
-    btStyle: PropTypes.object,
-    data: PropTypes.object,
-    onClose: PropTypes.func
-  }
-  */
-
-  constructor(props){
-    super(props)
-    const { data } = props;
-
-    this._setProxy = safeFn(data, SET.PROXY)
-  }
-
-  _hMode = (fnName, mode) => {
-    const { data } = this.props
-        , fnMode = safeFn(data, fnName);
-    fnMode(mode)
-  }
-  _hSetProxy = () => {
-    this._setProxy(this.proxyComp.getValue())
-  }
-  _hClearProxy = () => {
-    this._setProxy('')
-  }
-  _hSelectTheme = (item) => {
-    const { theme, onChangeTheme } = this.props;
-    if (
-        item &&
-        theme.getThemeName() !== item.value
-    ) {
-      theme.setThemeName(item.value)
-      onChangeTheme(item.value)
-    }
-  }
-
-  _setHalo = (is) => {
-    Highcharts.setOptions(_crHaloOption(is))
-  }
-
-
- _refProxy = n => this.proxyComp = n
-
-  render(){
-    const { titleStyle, btStyle, data, onClose } = this.props
-        , _proxy = data.getProxy()
-        , _isAdminMode = safeFn(data, MODE_ADMIN, false)()
-        , _isDrawDeltaExtrems = safeFn(data, MODE_DELTA, false)()
-        , _isNotZoomToMinMax = safeFn(data, MODE_ZOOM, false)();
-    return (
-      <div>
-        <D.RowPattern
-           ref={this._refProxy}
-           captionStyle={titleStyle}
-           caption="Proxy:"
-           placeholder="Https Proxy Server"
-           initValue={_proxy}
-           onEnter={this._setProxy}
-           onClear={this._hClearProxy}
-        />
-        <D.RowInputSelect
-           caption="UI Theme"
-           captionStyle={titleStyle}
-           options={UI_THEME_OPTIONS}
-           onSelect={this._hSelectTheme}
-        />
-       <D.RowCheckBox
-          initValue={_isAdminMode}
-          caption="View in Admin Mode"
-          onCheck={this._hMode.bind(null, MODE_ADMIN, true)}
-          onUnCheck={this._hMode.bind(null, MODE_ADMIN, false)}
-       />
-       <D.RowCheckBox
-          initValue={_isDrawDeltaExtrems}
-          caption="Draw Deltas to Min-Max"
-          onCheck={this._hMode.bind(null, MODE_DELTA, true)}
-          onUnCheck={this._hMode.bind(null, MODE_DELTA, false)}
-       />
-       <D.RowCheckBox
-          initValue={_isNotZoomToMinMax}
-          caption="Not Zoom to Min-Max"
-          onCheck={this._hMode.bind(null, MODE_ZOOM, true)}
-          onUnCheck={this._hMode.bind(null, MODE_ZOOM, false)}
-       />
-       <D.RowCheckBox
-          initValue={false}
-          caption="Without Points Halo"
-          onCheck={this._setHalo.bind(null, false)}
-          onUnCheck={this._setHalo.bind(null, true)}
-       />
-       <RowButtons btStyle={btStyle} onClose={onClose}>
-         <FlatButton
-           style={{...btStyle, ...S.BT_PROXY}}
-           caption="SET PROXY"
-           onClick={this._hSetProxy}
-         />
-       </RowButtons>
-      </div>
-    );
-  }
+const _useProxy = (data) => {
+  const _refProxy = useRef()
+  , _setProxy = safeFn(data, SET_PROXY)
+  , _proxy = data.getProxy();
+  return [
+    _refProxy,
+    _proxy,
+    _setProxy,
+    () => _setProxy(_refProxy.current.getValue()),
+    () => _setProxy('')
+  ];
 }
 
-export default withTheme(PaneOptions)
+const _useTheme = (onChangeTheme) => {
+  const theme = useContext(ThemeContext);
+  return item => {
+     if (item && theme.getThemeName() !== item.value) {
+       theme.setThemeName(item.value)
+       onChangeTheme(item.value)
+     }
+  };
+};
+
+const PaneOptions = ({
+  titleStyle, btStyle,
+  data,
+  onClose,
+  onChangeTheme
+}) => {
+  const [
+    _refProxy,
+    _proxy, _setProxy,
+    _hSetProxy, _hClearProxy
+  ] = _useProxy(data)
+  , _hSelectTheme = _useTheme(onChangeTheme)
+  , _hMode = (fnName, mode) => safeFn(data, fnName)(mode)
+  , _hSetHalo = is => Highcharts.setOptions(_crHaloOption(is));
+
+  const _isAdminMode = safeFn(data, MODE_ADMIN, false)()
+  , _isDrawDeltaExtrems = safeFn(data, MODE_DELTA, false)()
+  , _isNotZoomToMinMax = safeFn(data, MODE_ZOOM, false)();
+
+  return (
+    <div>
+      <D.RowPattern
+         ref={_refProxy}
+         captionStyle={titleStyle}
+         caption="Proxy"
+         placeholder="Https Proxy Server"
+         initValue={_proxy}
+         onEnter={_setProxy}
+         isClearBlank={true}
+         onClear={_hClearProxy}
+      />
+      <D.RowInputSelect
+         caption="UI Theme"
+         captionStyle={titleStyle}
+         options={UI_THEME_OPTIONS}
+         onSelect={_hSelectTheme}
+      />
+     <D.RowCheckBox
+        initValue={_isAdminMode}
+        caption="View in Admin Mode"
+        onCheck={_hMode.bind(null, MODE_ADMIN, true)}
+        onUnCheck={_hMode.bind(null, MODE_ADMIN, false)}
+     />
+     <D.RowCheckBox
+        initValue={_isDrawDeltaExtrems}
+        caption="Draw Deltas to Min-Max"
+        onCheck={_hMode.bind(null, MODE_DELTA, true)}
+        onUnCheck={_hMode.bind(null, MODE_DELTA, false)}
+     />
+     <D.RowCheckBox
+        initValue={_isNotZoomToMinMax}
+        caption="Not Zoom to Min-Max"
+        onCheck={_hMode.bind(null, MODE_ZOOM, true)}
+        onUnCheck={_hMode.bind(null, MODE_ZOOM, false)}
+     />
+     <D.RowCheckBox
+        initValue={false}
+        caption="Without Points Halo"
+        onCheck={_hSetHalo.bind(null, false)}
+        onUnCheck={_hSetHalo.bind(null, true)}
+     />
+     <RowButtons btStyle={btStyle} onClose={onClose}>
+       <FlatButton
+         style={{...btStyle, ...S.BT_PROXY}}
+         caption="SET PROXY"
+         onClick={_hSetProxy}
+       />
+     </RowButtons>
+    </div>
+  );
+}
+
+/*
+PaneOptions.propTypes = {
+  titleStyle: PropTypes.object,
+  btStyle: PropTypes.object,
+  data: PropTypes.object,
+  onClose: PropTypes.func,
+  onChangeTheme: PropTypes.func
+}
+*/
+
+export default PaneOptions
