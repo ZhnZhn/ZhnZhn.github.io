cf52a9a515ce5be774c799ca22c7fc7ea2cdd01b
Yevhenii Molodyi <eugmolody@gmail.com>
2021-02-17 11:40:59 +0200

    Edit color palette Model

--- js/constants/Model.js
+++ js/constants/Model.js
@@ -6,7 +6,7 @@ var Model = {
   palette: {
     rows: 3,
     cols: 3,
-    colors: ['#7cb5ec', '#90ed7d', '#f7a35c', '#8085e9', '#f15c80', '#e4d354', '#2b908f', '#f45b5b', '#91e8e1']
+    colors: ['#7cb5ec', '#8abb5d', '#f7a35c', '#8085e9', '#f15c80', '#e4d354', '#2b908f', '#f45b5b', '#91e8e1']
   }
 };
 var _default = Model;
--- src/constants/Model.js
+++ src/constants/Model.js
@@ -4,7 +4,7 @@ const Model = {
     rows: 3,
     cols: 3,
     colors: [
-      '#7cb5ec', '#90ed7d', '#f7a35c',
+      '#7cb5ec', '#8abb5d', '#f7a35c',
       '#8085e9', '#f15c80', '#e4d354',
       '#2b908f', '#f45b5b', '#91e8e1'
     ]
