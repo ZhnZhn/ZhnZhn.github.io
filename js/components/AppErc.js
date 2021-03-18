0f7e7298d2bce222efd46997628c64defea139a3
Yevhenii Molodyi <eugmolody@gmail.com>
2021-03-18 17:20:29 +0200

    Improve details

--- js/components/AppErc.js
+++ js/components/AppErc.js
@@ -41,7 +41,7 @@ var _ThemeContext = _interopRequireDefault(require("./hoc/ThemeContext"));
 
 var _checkBuild = _interopRequireDefault(require("./checkBuild"));
 
-var BUILD_DATE = '16-03-2021';
+var BUILD_DATE = '18-03-2021';
 var CL = "component-container";
 
 var showSettings = _ComponentActions["default"].showSettings.bind(null, _ChartStore["default"].exportSettingFn());
