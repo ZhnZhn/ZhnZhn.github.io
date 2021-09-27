9d702136fed116a9b8b46189d5683acad6d25012
Yevhenii Molodyi <eugmolody@gmail.com>
2021-09-27 18:55:47 +0300

    Add ItemStack to FlexTokens

--- js/components/items/FlexTokens.js
+++ js/components/items/FlexTokens.js
@@ -3,47 +3,44 @@
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 exports.__esModule = true;
-exports["default"] = void 0;
-
-var _jsxRuntime = require("react/jsx-runtime.js");
-
-var _react = require("react");
+exports.default = void 0;
 
 var _Comp = _interopRequireDefault(require("../Comp"));
 
-var S = {
-  TOKENS: {
-    display: 'flex',
-    flexFlow: 'wrap',
-    lineHeight: 2
-  },
-  TOKEN: {
-    display: 'inline-block',
-    paddingLeft: 8,
-    paddingRight: 8,
-    fontWeight: 600,
-    whiteSpace: 'nowrap'
-  }
+var _jsxRuntime = require("react/jsx-runtime");
+
+const S_TOKENS = {
+  display: 'flex',
+  flexFlow: 'wrap',
+  lineHeight: 2
+},
+      S_TOKEN = {
+  display: 'inline-block',
+  padding: '0 8px',
+  fontWeight: 600,
+  whiteSpace: 'nowrap'
 };
 
-var FlexSpans = function FlexSpans(_ref) {
-  var _ref$tokens = _ref.tokens,
-      tokens = _ref$tokens === void 0 ? [] : _ref$tokens;
-  return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-    style: S.TOKENS,
-    children: tokens.map(function (token) {
-      return /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-        style: S.TOKEN,
-        children: token
-      }, token);
-    })
-  });
-};
+const _crTokenItem = token => /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+  style: S_TOKEN,
+  children: token
+}, token);
 
-var FlexTokens = /*#__PURE__*/(0, _react.memo)(function (_ref2) {
-  var caption = _ref2.caption,
-      tokens = _ref2.tokens;
-  return caption ? /*#__PURE__*/(0, _jsxRuntime.jsx)(_Comp["default"].OpenClose, {
+const FlexSpans = ({
+  tokens
+}) => /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+  style: S_TOKENS,
+  children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_Comp.default.ItemStack, {
+    items: tokens,
+    crItem: _crTokenItem
+  })
+});
+
+const FlexTokens = ({
+  caption,
+  tokens
+}) => {
+  return caption ? /*#__PURE__*/(0, _jsxRuntime.jsx)(_Comp.default.OpenClose, {
     isClose: false,
     caption: caption,
     children: /*#__PURE__*/(0, _jsxRuntime.jsx)(FlexSpans, {
@@ -52,7 +49,8 @@ var FlexTokens = /*#__PURE__*/(0, _react.memo)(function (_ref2) {
   }) : /*#__PURE__*/(0, _jsxRuntime.jsx)(FlexSpans, {
     tokens: tokens
   });
-});
+};
+
 var _default = FlexTokens;
-exports["default"] = _default;
+exports.default = _default;
 //# sourceMappingURL=FlexTokens.js.map
--- src/components/items/FlexTokens.jsx
+++ src/components/items/FlexTokens.jsx
@@ -1,33 +1,36 @@
-import { memo } from 'react'
+import A from '../Comp';
 
-import A from '../Comp'
-
-const S = {
-  TOKENS: {
-    display: 'flex',
-    flexFlow: 'wrap',
-    lineHeight: 2
-  },
-  TOKEN: {
-    display: 'inline-block',
-    paddingLeft: 8,
-    paddingRight: 8,
-    fontWeight: 600,
-    whiteSpace: 'nowrap'
-  }
+const S_TOKENS = {
+  display: 'flex',
+  flexFlow: 'wrap',
+  lineHeight: 2
 }
+, S_TOKEN = {
+   display: 'inline-block',
+   padding: '0 8px',
+   fontWeight: 600,
+   whiteSpace: 'nowrap'
+};
 
-const FlexSpans = ({ tokens=[] }) => (
-  <div style={S.TOKENS}>
-    {tokens.map(token => (
-      <span key={token} style={S.TOKEN}>
-          {token}
-      </span>
-    ))}
+const _crTokenItem = token => (
+  <span key={token} style={S_TOKEN}>
+    {token}
+  </span>
+);
+
+const FlexSpans = ({ tokens }) => (
+  <div style={S_TOKENS}>
+    <A.ItemStack
+      items={tokens}
+      crItem={_crTokenItem}
+    />
   </div>
 );
 
-const FlexTokens = memo(({ caption, tokens }) => {
+const FlexTokens = ({
+  caption,
+  tokens
+}) => {
   return caption
     ? <A.OpenClose
         isClose={false}
@@ -36,6 +39,6 @@ const FlexTokens = memo(({ caption, tokens }) => {
         <FlexSpans tokens={tokens}/>
       </A.OpenClose>
     : <FlexSpans tokens={tokens}/>
-});
+};
 
 export default FlexTokens
