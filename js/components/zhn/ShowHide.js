7a0392283a1e31bda21d8532dca678e069bdffb3
Yevhenii Molodyi <eugmolody@gmail.com>
2021-10-05 18:26:05 +0300

    Edit ShowHide

--- js/components/zhn/ShowHide.js
+++ js/components/zhn/ShowHide.js
@@ -10,14 +10,12 @@ var _crCn = _interopRequireDefault(require("../zhn-utils/crCn"));
 var _jsxRuntime = require("react/jsx-runtime");
 
 //import PropTypes from "prop-types";
-const CL_SHOW_POPUP = 'show-popup';
-const S = {
-  SHOW: {
-    display: 'block'
-  },
-  HIDE: {
-    display: 'none'
-  }
+const CL_SHOW_POPUP = 'show-popup',
+      S_SHOW = {
+  display: 'block'
+},
+      S_HIDE = {
+  display: 'none'
 };
 
 const ShowHide = ({
@@ -28,7 +26,7 @@ const ShowHide = ({
   children
 }) => {
   const _cn = (0, _crCn.default)(className, [isShow && !withoutAnimation, CL_SHOW_POPUP]),
-        _styleShow = isShow ? S.SHOW : S.HIDE;
+        _styleShow = isShow ? S_SHOW : S_HIDE;
 
   return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
     "aria-expanded": isShow,
--- src/components/zhn/ShowHide.jsx
+++ src/components/zhn/ShowHide.jsx
@@ -1,16 +1,9 @@
+//import PropTypes from "prop-types";
 import crCn from '../zhn-utils/crCn';
 
-//import PropTypes from "prop-types";
-
-const CL_SHOW_POPUP = 'show-popup';
-const S = {
-  SHOW: {
-    display: 'block'
-  },
-  HIDE: {
-    display: 'none'
-  }
-};
+const CL_SHOW_POPUP = 'show-popup'
+, S_SHOW = { display: 'block'}
+, S_HIDE = { display: 'none' };
 
 const ShowHide = ({
   isShow,
@@ -19,7 +12,7 @@ const ShowHide = ({
   children
 }) => {
     const _cn = crCn(className, [isShow && !withoutAnimation, CL_SHOW_POPUP])
-    , _styleShow = isShow ? S.SHOW : S.HIDE;
+    , _styleShow = isShow ? S_SHOW : S_HIDE;
 
     return (
       <div
