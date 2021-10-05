3aec44802aa62f63f355628c3cce5210af3d0669
Yevhenii Molodyi <eugmolody@gmail.com>
2021-10-05 18:43:59 +0300

    Add Svg to OpenClose

--- js/components/zhn/OpenClose.js
+++ js/components/zhn/OpenClose.js
@@ -3,116 +3,105 @@
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 exports.__esModule = true;
-exports["default"] = void 0;
-
-var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
-
-var _jsxRuntime = require("react/jsx-runtime.js");
+exports.default = void 0;
 
 var _use = _interopRequireDefault(require("../hooks/use"));
 
 var _Color = _interopRequireDefault(require("../styles/Color"));
 
+var _Svg = _interopRequireDefault(require("./svg/Svg"));
+
+var _jsxRuntime = require("react/jsx-runtime");
+
 //import PropTypes from 'prop-types'
-var useToggle = _use["default"].useToggle,
-    useKeyEnter = _use["default"].useKeyEnter;
-var CL = {
-  ROOT: 'zhn-oc',
-  SHOW_POPUP: 'show-popup',
-  NOT_SELECTED: 'not-selected',
-  OC_EXP: 'zhn-oc__exp'
-};
-var FILL_CLOSE_COLOR = _Color["default"].BLANK;
-var S = {
-  ROOT_DIV: {
-    lineHeight: 2
-  },
-  SVG: {
-    display: 'inline-block',
-    position: 'relative',
-    top: 1,
-    width: 16,
-    height: 16,
-    marginLeft: 8
-  },
-  CAPTION: {
-    paddingLeft: 4,
-    fontFamily: 'Roboto, Arial, Lato, sans-serif',
-    fontWeight: 'bold',
-    fontSize: '16px',
-    cursor: 'pointer'
-  },
-  BLOCK: {
-    display: 'block'
-  },
-  NONE: {
-    display: 'none'
-  }
-};
-var PATH_OPEN = "M 2,14 L 14,14 14,2 2,14";
-var PATH_CLOSE = "M 2,2 L 14,8 2,14 2,2";
+const {
+  useToggle,
+  useKeyEnter
+} = _use.default;
+const CL_ROOT = 'zhn-oc',
+      CL_SHOW_POPUP = 'show-popup',
+      CL_NOT_SELECTED = 'not-selected',
+      CL_OC_EXP = 'zhn-oc__exp',
+      FILL_CLOSE_COLOR = _Color.default.BLANK,
+      S_ROOT_DIV = {
+  lineHeight: 2
+},
+      S_SVG = {
+  display: 'inline-block',
+  position: 'relative',
+  top: 1,
+  marginLeft: 8
+},
+      S_CAPTION = {
+  paddingLeft: 4,
+  fontWeight: 'bold',
+  fontSize: '16px',
+  cursor: 'pointer'
+},
+      S_BLOCK = {
+  display: 'block'
+},
+      S_NONE = {
+  display: 'none'
+},
+      PATH_OPEN = "M 2,14 L 14,14 14,2 2,14",
+      PATH_CLOSE = "M 2,2 L 14,8 2,14 2,2";
 
-var _crConf = function _crConf(_ref) {
-  var isOpen = _ref.isOpen,
-      openColor = _ref.openColor;
-  return isOpen ? {
-    _pathV: PATH_OPEN,
-    _fillV: openColor,
-    _childCl: CL.OC_EXP + " " + CL.SHOW_POPUP,
-    _childStyle: S.BLOCK
-  } : {
-    _pathV: PATH_CLOSE,
-    _fillV: FILL_CLOSE_COLOR,
-    _childCl: CL.OC_EXP,
-    _childStyle: S.NONE
-  };
+const _crConf = ({
+  isOpen,
+  openColor
+}) => isOpen ? {
+  _pathV: PATH_OPEN,
+  _fillV: openColor,
+  _childCl: CL_OC_EXP + " " + CL_SHOW_POPUP,
+  _childStyle: S_BLOCK
+} : {
+  _pathV: PATH_CLOSE,
+  _fillV: FILL_CLOSE_COLOR,
+  _childCl: CL_OC_EXP,
+  _childStyle: S_NONE
 };
 
-var OpenClose = function OpenClose(_ref2) {
-  var _ref2$isClose = _ref2.isClose,
-      isClose = _ref2$isClose === void 0 ? true : _ref2$isClose,
-      _ref2$role = _ref2.role,
-      role = _ref2$role === void 0 ? 'button' : _ref2$role,
-      style = _ref2.style,
-      ocStyle = _ref2.ocStyle,
-      caption = _ref2.caption,
-      captionStyle = _ref2.captionStyle,
-      openColor = _ref2.openColor,
-      CompAfter = _ref2.CompAfter,
-      childStyle = _ref2.childStyle,
-      children = _ref2.children;
-
-  var _useToggle = useToggle(!isClose),
-      isOpen = _useToggle[0],
-      toggleIsOpen = _useToggle[1],
-      _hKeyDown = useKeyEnter(toggleIsOpen),
-      _crConf2 = _crConf({
-    isOpen: isOpen,
-    openColor: openColor
-  }),
-      _pathV = _crConf2._pathV,
-      _fillV = _crConf2._fillV,
-      _childCl = _crConf2._childCl,
-      _childStyle = _crConf2._childStyle;
+const OpenClose = ({
+  isClose = true,
+  role = 'button',
+  style,
+  ocStyle,
+  caption,
+  captionStyle,
+  openColor,
+  CompAfter,
+  childStyle,
+  children
+}) => {
+  const [isOpen, toggleIsOpen] = useToggle(!isClose),
+        _hKeyDown = useKeyEnter(toggleIsOpen),
+        {
+    _pathV,
+    _fillV,
+    _childCl,
+    _childStyle
+  } = _crConf({
+    isOpen,
+    openColor
+  });
 
   return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
-    style: (0, _extends2["default"])({}, S.ROOT_DIV, style),
+    style: { ...S_ROOT_DIV,
+      ...style
+    },
     children: [/*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
-      className: CL.NOT_SELECTED,
+      className: CL_NOT_SELECTED,
       children: [/*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
         tabIndex: "0",
         role: role,
-        className: CL.ROOT,
+        className: CL_ROOT,
         style: ocStyle,
         onClick: toggleIsOpen,
         onKeyDown: _hKeyDown,
-        children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("svg", {
-          viewBox: "0 0 16 16",
-          width: "100%",
-          height: "100%",
-          preserveAspectRatio: "none",
-          xmlns: "http://www.w3.org/2000/svg",
-          style: S.SVG,
+        children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_Svg.default, {
+          w: "16",
+          style: S_SVG,
           children: /*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
             fill: _fillV,
             strokeWidth: "1",
@@ -120,14 +109,18 @@ var OpenClose = function OpenClose(_ref2) {
             d: _pathV
           })
         }), /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-          style: (0, _extends2["default"])({}, S.CAPTION, captionStyle),
+          style: { ...S_CAPTION,
+            ...captionStyle
+          },
           children: caption
         })]
       }), CompAfter]
     }), /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
       "aria-expanded": isOpen,
       className: _childCl,
-      style: (0, _extends2["default"])({}, childStyle, _childStyle),
+      style: { ...childStyle,
+        ..._childStyle
+      },
       children: children
     })]
   });
@@ -148,5 +141,5 @@ OpenClose.propTypes = {
 
 
 var _default = OpenClose;
-exports["default"] = _default;
+exports.default = _default;
 //# sourceMappingURL=OpenClose.js.map
--- src/components/zhn/OpenClose.jsx
+++ src/components/zhn/OpenClose.jsx
@@ -1,61 +1,48 @@
 //import PropTypes from 'prop-types'
-import use from '../hooks/use'
-import C from '../styles/Color'
+import use from '../hooks/use';
+import C from '../styles/Color';
+import Svg from './svg/Svg';
 
 const { useToggle, useKeyEnter } = use
 
-const CL = {
-  ROOT: 'zhn-oc',
-  SHOW_POPUP: 'show-popup',
-  NOT_SELECTED: 'not-selected',
-  OC_EXP: 'zhn-oc__exp'
-};
+const CL_ROOT = 'zhn-oc'
+, CL_SHOW_POPUP = 'show-popup'
+, CL_NOT_SELECTED = 'not-selected'
+, CL_OC_EXP = 'zhn-oc__exp'
 
-const FILL_CLOSE_COLOR = C.BLANK
+, FILL_CLOSE_COLOR = C.BLANK
 
-const S = {
-  ROOT_DIV: {
-    lineHeight: 2
-  },
-  SVG: {
-    display: 'inline-block',
-    position: 'relative',
-    top: 1,
-    width: 16,
-    height: 16,
-    marginLeft: 8
-  },
-  CAPTION: {
-    paddingLeft: 4,
-    fontFamily: 'Roboto, Arial, Lato, sans-serif',
-    fontWeight: 'bold',
-    fontSize: '16px',
-    cursor: 'pointer'
-  },
+, S_ROOT_DIV = { lineHeight: 2 }
+, S_SVG = {
+  display: 'inline-block',
+  position: 'relative',
+  top: 1,
+  marginLeft: 8
+}
+, S_CAPTION = {
+  paddingLeft: 4,
+  fontWeight: 'bold',
+  fontSize: '16px',
+  cursor: 'pointer'
+}
+, S_BLOCK = { display: 'block' }
+, S_NONE = { display: 'none' }
 
-  BLOCK: {
-    display: 'block'
-  },
-  NONE: {
-    display: 'none'
-  }
-};
-
-const PATH_OPEN = "M 2,14 L 14,14 14,2 2,14";
-const PATH_CLOSE = "M 2,2 L 14,8 2,14 2,2";
+, PATH_OPEN = "M 2,14 L 14,14 14,2 2,14"
+, PATH_CLOSE = "M 2,2 L 14,8 2,14 2,2";
 
 const _crConf = ({ isOpen, openColor }) => isOpen
   ? {
       _pathV: PATH_OPEN,
       _fillV: openColor,
-      _childCl: `${CL.OC_EXP} ${CL.SHOW_POPUP}`,
-      _childStyle: S.BLOCK
+      _childCl: `${CL_OC_EXP} ${CL_SHOW_POPUP}`,
+      _childStyle: S_BLOCK
     }
   : {
       _pathV: PATH_CLOSE,
       _fillV: FILL_CLOSE_COLOR,
-      _childCl: CL.OC_EXP,
-      _childStyle: S.NONE
+      _childCl: CL_OC_EXP,
+      _childStyle: S_NONE
     };
 
 const OpenClose = ({
@@ -73,29 +60,25 @@ const OpenClose = ({
      _childCl, _childStyle
    } = _crConf({ isOpen, openColor });
   return (
-    <div style={{...S.ROOT_DIV, ...style}}>
-      <div className={CL.NOT_SELECTED}>
+    <div style={{...S_ROOT_DIV, ...style}}>
+      <div className={CL_NOT_SELECTED}>
         <div
           tabIndex="0"
           role={role}
-          className={CL.ROOT}
+          className={CL_ROOT}
           style={ocStyle}
           onClick={toggleIsOpen}
           onKeyDown={_hKeyDown}
         >
-         <svg
-            viewBox="0 0 16 16" width="100%" height="100%"
-            preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg"
-            style={S.SVG}
-          >
+         <Svg w="16" style={S_SVG}>
            <path
               fill={_fillV}
               strokeWidth="1"
               stroke={openColor}
               d={_pathV}
            />
-         </svg>
-         <span style={{...S.CAPTION, ...captionStyle}} >
+         </Svg>
+         <span style={{...S_CAPTION, ...captionStyle}} >
             {caption}
          </span>
        </div>
