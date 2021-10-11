a087e67a6cb0d70658ec2eeaeff40fcd039f4474
Yevhenii Molodyi <eugmolody@gmail.com>
2021-10-11 15:11:33 +0300

    Add restProps to ItemStack

--- js/components/zhn/ItemStack.js
+++ js/components/zhn/ItemStack.js
@@ -1,17 +1,19 @@
 "use strict";
 
 exports.__esModule = true;
-exports["default"] = void 0;
+exports.default = void 0;
 
 var _react = require("react");
 
-var _isArr = Array.isArray;
-var ItemStack = /*#__PURE__*/(0, _react.memo)(function (_ref) {
-  var items = _ref.items,
-      crItem = _ref.crItem;
+const _isArr = Array.isArray;
+const ItemStack = /*#__PURE__*/(0, _react.memo)(({
+  items,
+  crItem,
+  ...restProps
+}) => {
   if (!_isArr(items)) return null;
-  return items.map(crItem);
+  return items.map((item, index) => crItem(item, index, restProps));
 });
 var _default = ItemStack;
-exports["default"] = _default;
+exports.default = _default;
 //# sourceMappingURL=ItemStack.js.map
--- src/components/zhn/ItemStack.jsx
+++ src/components/zhn/ItemStack.jsx
@@ -2,9 +2,10 @@ import { memo } from 'react';
 
 const _isArr = Array.isArray;
 
-const ItemStack = memo(({ items, crItem }) => {
+const ItemStack = memo(({ items, crItem, ...restProps }) => {
   if (!_isArr(items)) return null;
-  return items.map(crItem);
+  return items
+    .map((item, index) => crItem(item, index, restProps));
 });
 
 export default ItemStack
