9b6fdd8512740e0007496d51487d4f9c8af24df5
Yevhenii Molodyi <eugmolody@gmail.com>
2021-10-12 10:44:10 +0300

    Add ItemStack to OptionList

--- js/components/zhn-select/OptionList.js
+++ js/components/zhn-select/OptionList.js
@@ -1,45 +1,49 @@
 "use strict";
 
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
 exports.__esModule = true;
-exports["default"] = void 0;
+exports.default = void 0;
 
-var _jsxRuntime = require("react/jsx-runtime.js");
+var _ItemStack = _interopRequireDefault(require("../zhn/ItemStack"));
 
-var OptionList = function OptionList(_ref) {
-  var options = _ref.options,
-      selectedIndex = _ref.selectedIndex,
-      className = _ref.className,
-      refOptionNode = _ref.refOptionNode,
-      _onClick = _ref.onClick,
-      propCaption = _ref.propCaption,
-      ItemComp = _ref.ItemComp;
-  return options.map(function (item, index) {
-    return (
-      /*#__PURE__*/
+var _jsxRuntime = require("react/jsx-runtime");
 
-      /*eslint-disable jsx-a11y/click-events-have-key-events*/
-      (0, _jsxRuntime.jsx)("div", {
-        role: "option",
-        "aria-selected": selectedIndex === index,
-        tabIndex: "-1",
-        className: className,
-        ref: function ref(n) {
-          return refOptionNode(n, index);
-        },
-        onClick: function onClick() {
-          return _onClick(item, index, propCaption);
-        },
-        children: /*#__PURE__*/(0, _jsxRuntime.jsx)(ItemComp, {
-          item: item,
-          propCaption: propCaption
-        })
-      }, index)
-      /*eslint-enable jsx-a11y/click-events-have-key-events*/
+const _crItem = (item, index, {
+  selectedIndex,
+  className,
+  refOptionNode,
+  onClick,
+  propCaption,
+  ItemComp
+}) =>
+/*#__PURE__*/
 
-    );
-  });
-};
+/*eslint-disable jsx-a11y/click-events-have-key-events*/
+(0, _jsxRuntime.jsx)("div", {
+  role: "option",
+  "aria-selected": selectedIndex === index,
+  tabIndex: "-1",
+  className: className,
+  ref: n => refOptionNode(n, index),
+  onClick: () => onClick(item, index, propCaption),
+  children: /*#__PURE__*/(0, _jsxRuntime.jsx)(ItemComp, {
+    item: item,
+    propCaption: propCaption
+  })
+}, index)
+/*eslint-enable jsx-a11y/click-events-have-key-events*/
+;
+
+const OptionList = ({
+  options,
+  ...restProps
+}) => /*#__PURE__*/(0, _jsxRuntime.jsx)(_ItemStack.default, {
+  items: options,
+  crItem: _crItem,
+  ...restProps
+});
 
 var _default = OptionList;
-exports["default"] = _default;
+exports.default = _default;
 //# sourceMappingURL=OptionList.js.map
--- src/components/zhn-select/OptionList.jsx
+++ src/components/zhn-select/OptionList.jsx
@@ -1,32 +1,43 @@
+import ItemStack from '../zhn/ItemStack';
+
+const _crItem = (
+  item,
+  index, {
+    selectedIndex,
+    className,
+    refOptionNode,
+    onClick,
+    propCaption,
+    ItemComp
+  }
+) => (
+  /*eslint-disable jsx-a11y/click-events-have-key-events*/
+  <div
+    role="option"
+    aria-selected={selectedIndex === index}
+    tabIndex="-1"
+    key={index}
+    className={className}
+    ref={n => refOptionNode(n, index)}
+    onClick={() => onClick(item, index, propCaption)}
+  >
+    <ItemComp
+       item={item}
+       propCaption={propCaption}
+    />
+  </div>
+  /*eslint-enable jsx-a11y/click-events-have-key-events*/
+);
 
 const OptionList = ({
   options,
-  selectedIndex,
-  className,
-  refOptionNode,
-  onClick,
-  propCaption,
-  ItemComp
-}) => options.map((item, index)=>(
-   /*eslint-disable jsx-a11y/click-events-have-key-events*/
-   <div
-     role="option"
-     aria-selected={selectedIndex === index}
-     tabIndex="-1"
-     key={index}
-     className={className}
-     ref={n => refOptionNode(n, index)}
-     onClick={() => onClick(item, index, propCaption)}
-   >
-     <ItemComp
-        item={item}
-        propCaption={propCaption}
-     />
-   </div>
-   /*eslint-enable jsx-a11y/click-events-have-key-events*/
- ));
+  ...restProps
+}) => (
+  <ItemStack
+    items={options}
+    crItem={_crItem}
+    {...restProps}
+  />
+);
 
-
-
-
-export default OptionList
+ export default OptionList
