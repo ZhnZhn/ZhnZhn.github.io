44d6b4a0ab262856cf1953e57961e6c95f5c8b64
Yevhenii Molodyi <eugmolody@gmail.com>
2021-10-12 10:28:54 +0300

    Remove style from ArrowCell to css class

--- js/components/zhn-select/ArrowCell.js
+++ js/components/zhn-select/ArrowCell.js
@@ -3,84 +3,46 @@
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 exports.__esModule = true;
-exports["default"] = void 0;
-
-var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
-
-var _jsxRuntime = require("react/jsx-runtime.js");
+exports.default = void 0;
 
 var _react = require("react");
 
 var _CL = _interopRequireDefault(require("./CL"));
 
-var S = {
-  ARROW_CELL: {
-    position: 'absolute',
-    top: 10,
-    right: 0,
-    width: 35,
-    paddingRight: 5,
-    textAlign: 'center',
-    verticalAlign: 'middle',
-    cursor: 'pointer'
-  },
-  ARROW: {
-    position: 'relative',
-    top: 2,
-    display: 'inline-block',
-    height: 0,
-    width: 0,
-    borderColor: '#999 transparent transparent',
-    borderStyle: 'solid',
-    borderWidth: '10px 8px 4px'
-  }
-};
-var C = {
-  ANIMATION_CIRCLE: "circle infinite 1.25s linear",
-  BORDER_COLOR: "#1b75bb transparent transparent"
-};
+var _jsxRuntime = require("react/jsx-runtime");
 
-var _getStyle = function _getStyle(ref) {
-  return ref.current.style;
-};
+const ANIMATION_CIRCLE = "circle infinite 1.25s linear",
+      BORDER_COLOR = "#1b75bb transparent transparent";
 
-var ArrowCell = /*#__PURE__*/(0, _react.forwardRef)(function (_ref, ref) {
-  var arrowStyle = _ref.arrowStyle,
-      onClick = _ref.onClick;
+const _getStyle = ref => ref.current.style;
 
-  var _refArrowCell = (0, _react.useRef)(),
-      _refArrow = (0, _react.useRef)();
+const ArrowCell = /*#__PURE__*/(0, _react.forwardRef)(({
+  arrowStyle,
+  onClick
+}, ref) => {
+  const _refArrowCell = (0, _react.useRef)(),
+        _refArrow = (0, _react.useRef)();
 
-  (0, _react.useImperativeHandle)(ref, function () {
-    return {
-      startAnimation: function startAnimation() {
-        _getStyle(_refArrowCell).animation = C.ANIMATION_CIRCLE;
-        _getStyle(_refArrow).borderColor = C.BORDER_COLOR;
-      },
-      stopAnimation: function stopAnimation() {
-        _getStyle(_refArrowCell).animation = "";
-      }
-    };
-  }, []);
+  (0, _react.useImperativeHandle)(ref, () => ({
+    startAnimation: () => {
+      _getStyle(_refArrowCell).animation = ANIMATION_CIRCLE;
+      _getStyle(_refArrow).borderColor = BORDER_COLOR;
+    },
+    stopAnimation: () => {
+      _getStyle(_refArrowCell).animation = "";
+    }
+  }), []);
   return /*#__PURE__*/(0, _jsxRuntime.jsx)("button", {
     ref: _refArrowCell,
-    className: _CL["default"].BT_ARROW,
-    style: S.ARROW_CELL,
+    className: _CL.default.BT_ARROW,
     tabIndex: "-1",
     onClick: onClick,
     children: /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
       ref: _refArrow,
-      style: (0, _extends2["default"])({}, S.ARROW, arrowStyle)
+      style: arrowStyle
     })
   });
 });
-/*
-ArrowCell.propTypes = {
- arrowStyle: PropTypes.object,
- onClick: PropTypes.func
-}
-*/
-
 var _default = ArrowCell;
-exports["default"] = _default;
+exports.default = _default;
 //# sourceMappingURL=ArrowCell.js.map
--- src/components/zhn-select/ArrowCell.jsx
+++ src/components/zhn-select/ArrowCell.jsx
@@ -2,33 +2,8 @@ import { forwardRef, useRef, useImperativeHandle } from 'react';
 
 import CL from './CL';
 
-const S = {
-  ARROW_CELL: {
-    position: 'absolute',
-    top: 10,
-    right: 0,
-    width: 35,
-    paddingRight: 5,
-    textAlign: 'center',
-    verticalAlign: 'middle',
-    cursor: 'pointer'
-  },
-  ARROW: {
-    position: 'relative',
-    top: 2,
-    display: 'inline-block',
-    height: 0,
-    width: 0,
-    borderColor: '#999 transparent transparent',
-    borderStyle: 'solid',
-    borderWidth: '10px 8px 4px'
-  }
-};
-
-const C = {
-  ANIMATION_CIRCLE: "circle infinite 1.25s linear",
-  BORDER_COLOR: "#1b75bb transparent transparent"
-};
+const ANIMATION_CIRCLE = "circle infinite 1.25s linear"
+, BORDER_COLOR = "#1b75bb transparent transparent";
 
 const _getStyle = ref => ref.current.style;
 
@@ -41,8 +16,8 @@ const ArrowCell = forwardRef(({
 
   useImperativeHandle(ref, () => ({
     startAnimation: () => {
-      _getStyle(_refArrowCell).animation = C.ANIMATION_CIRCLE;
-      _getStyle(_refArrow).borderColor = C.BORDER_COLOR;
+      _getStyle(_refArrowCell).animation = ANIMATION_CIRCLE;
+      _getStyle(_refArrow).borderColor = BORDER_COLOR;
     },
     stopAnimation: () => {
       _getStyle(_refArrowCell).animation = "";
@@ -53,23 +28,15 @@ const ArrowCell = forwardRef(({
     <button
        ref={_refArrowCell}
        className={CL.BT_ARROW}
-       style={S.ARROW_CELL}
        tabIndex="-1"
        onClick={onClick}
     >
       <span
          ref={_refArrow}
-         style={{...S.ARROW, ...arrowStyle}}
+         style={arrowStyle}
       />
     </button>
   );
-})
-
-/*
-ArrowCell.propTypes = {
- arrowStyle: PropTypes.object,
- onClick: PropTypes.func
-}
-*/
+});
 
 export default ArrowCell
