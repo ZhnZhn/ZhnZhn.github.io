78a8ee6fd41faa86e7789e1432b6e5ff4e0146e1
Yevhenii Molodyi <eugmolody@gmail.com>
2021-10-22 17:05:13 +0300

    Add crDimConfig

--- js/components/stat-dialogs/loadDimsWithOptions.js
+++ js/components/stat-dialogs/loadDimsWithOptions.js
@@ -5,61 +5,19 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 exports.__esModule = true;
 exports.default = void 0;
 
-var _dimConfigFn = require("./dimConfigFn");
-
 var _crEsDimConfig = _interopRequireDefault(require("./crEsDimConfig"));
 
 var _crSdnDimConfig = _interopRequireDefault(require("./crSdnDimConfig"));
+
+var _crDimConfig = _interopRequireDefault(require("./crDimConfig"));
 
 var _loadJson = _interopRequireDefault(require("./loadJson"));
 
 const _isArr = Array.isArray;
 
-const _isNotCorrectDim = (values, valueTexts) => !_isArr(values) || !_isArr(valueTexts) || values.length !== valueTexts.length;
-
-const _crDimOptions = ({
-  values,
-  valueTexts,
-  code
-}) => {
-  if (_isNotCorrectDim(values, valueTexts)) {
-    return;
-  }
-
-  return valueTexts.map((text, index) => (0, _dimConfigFn.crDimItem)(text, code, values[index]));
-};
-
-const _crDateOptions = ({
-  values,
-  valueTexts
-}) => {
-  if (_isNotCorrectDim(values, valueTexts)) {
-    return;
-  }
-
-  const _dateOptions = [];
-  valueTexts.forEach((valueText, i) => {
-    _dateOptions.push({
-      caption: valueText,
-      value: values[i]
-    });
-  });
-  return _dateOptions.reverse();
-};
+const _isEs = (dimension, source) => dimension && source === 'Eurostat';
 
 const _isSdn = item => item && item.id && item.text && !_isArr(item.valueTexts) && _isArr(item.values);
-
-const TIME_IDS = ['Tid', 'Year', 'Month', 'Vuosi', 'Vuosineljännes'];
-
-const _isNotTimeDimension = (time, code) => !time && TIME_IDS.indexOf(code) === -1 && (code + '').indexOf('TLIST(') === -1; //'TLIST(' //'TLIST(M1)', 'TLIST(A1)' SIR
-
-
-const FREQUENCY_HM = {
-  month: 'M',
-  quarter: 'K'
-};
-
-const _isEs = (dimension, source) => dimension && source === 'Eurostat';
 
 const _crDimsConfig = json => {
   const dims = [],
@@ -87,35 +45,10 @@ const _crDimsConfig = json => {
     return (0, _crSdnDimConfig.default)(variables);
   }
 
-  variables.forEach(item => {
-    const {
-      code,
-      time
-    } = item,
-          _text = item.text || '';
-
-    if (_isNotTimeDimension(time, code)) {
-      dims.push({
-        c: (0, _dimConfigFn.toFirstUpperCase)(_text),
-        v: code,
-        options: _crDimOptions(item)
-      });
-    } else {
-      timeId = code;
-      mapFrequency = FREQUENCY_HM[_text.toLowerCase()];
-      dims.dateOptions = _crDateOptions(item);
-    }
-  });
-  return {
-    mapFrequency,
-    dims,
-    timeId
-  };
+  return (0, _crDimConfig.default)(variables);
 };
 
-const loadDimsWithOptions = url => {
-  return (0, _loadJson.default)(url).then(_crDimsConfig);
-};
+const loadDimsWithOptions = url => (0, _loadJson.default)(url).then(_crDimsConfig);
 
 var _default = loadDimsWithOptions;
 exports.default = _default;
--- src/components/stat-dialogs/loadDimsWithOptions.js
+++ src/components/stat-dialogs/loadDimsWithOptions.js
@@ -1,64 +1,18 @@
-import {
-  crDimItem,
-  toFirstUpperCase
-} from './dimConfigFn';
-
 import crEsDimConfig from './crEsDimConfig';
 import crSdnDimConfig from './crSdnDimConfig';
+import crDimConfig from './crDimConfig';
 
 import loadJson from './loadJson';
 
 const _isArr = Array.isArray;
 
-const _isNotCorrectDim = (values, valueTexts) =>
- !_isArr(values) || !_isArr(valueTexts)
- || values.length !== valueTexts.length;
-
-const _crDimOptions = ({ values, valueTexts, code }) => {
-  if (_isNotCorrectDim(values, valueTexts)) {
-    return;
-  }
-  return valueTexts.map((text, index) =>
-    crDimItem(text, code, values[index]));
-};
-
-const _crDateOptions = ({ values, valueTexts }) => {
-  if (_isNotCorrectDim(values, valueTexts)) {
-    return;
-  }
-  const _dateOptions = [];
-  valueTexts.forEach((valueText, i) => {
-    _dateOptions.push({
-      caption: valueText,
-      value: values[i]
-    })
-  });
-  return _dateOptions.reverse();
-};
+const _isEs = (dimension, source) =>
+  dimension && source === 'Eurostat'
 
 const _isSdn = item => item
   && item.id && item.text
   && !_isArr(item.valueTexts)
   && _isArr(item.values);
-
-const TIME_IDS = [
-  'Tid',
-  'Year','Month','Vuosi','Vuosineljännes'
-];
-
-const _isNotTimeDimension = (time, code) => !time &&
-  TIME_IDS.indexOf(code) === -1
-  && (code+'').indexOf('TLIST(') === -1;
-  //'TLIST(' //'TLIST(M1)', 'TLIST(A1)' SIR
-
-const FREQUENCY_HM = {
-  month: 'M',
-  quarter: 'K'
-};
-
-
-const _isEs = (dimension, source) =>
-  dimension && source === 'Eurostat'
 
 const _crDimsConfig = (json) => {
   const dims = []
@@ -73,26 +27,10 @@ const _crDimsConfig = (json) => {
   if (_isSdn(variables[0])) {
     return crSdnDimConfig(variables);
   }
-  variables.forEach(item => {
-    const { code, time } = item
-    , _text = item.text || '';
-    if (_isNotTimeDimension(time, code)) {
-      dims.push({
-        c: toFirstUpperCase(_text),
-        v: code,
-        options: _crDimOptions(item)
-     })
-    } else {
-      timeId = code
-      mapFrequency = FREQUENCY_HM[_text.toLowerCase()]
-      dims.dateOptions = _crDateOptions(item)
-   }
-  })
-  return { mapFrequency, dims, timeId };
+  return crDimConfig(variables);
 };
 
-const loadDimsWithOptions = (url) => {
-  return loadJson(url).then(_crDimsConfig);
-};
+const loadDimsWithOptions = url => loadJson(url)
+ .then(_crDimsConfig);
 
 export default loadDimsWithOptions
