78c36a9165e6ed247617459e00bf0990e284e992
Yevhenii Molodyi <eugmolody@gmail.com>
2021-11-04 12:29:49 +0200

    Add helper function updateStateIf

---
+++ src/components/stat-dialogs/updateStateIf.js
@@ -0,0 +1,12 @@
+
+const udpdateStateIf = (setState, propName, propValue) => {
+  setState(prevState => {
+    if (prevState[propName] !== propValue) {
+      prevState[propName] = propValue
+      return {...prevState};
+    }
+    return prevState;
+  })
+};
+
+export default udpdateStateIf
