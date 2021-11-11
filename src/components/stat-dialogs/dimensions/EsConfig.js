5f95d14b268267e1c9b1c718c9d809bc047da44f
Yevhenii Molodyi <eugmolody@gmail.com>
2021-11-11 12:13:45 +0200

    Edit EsConfig ADJ value

--- src/components/stat-dialogs/dimensions/EsConfig.js
+++ src/components/stat-dialogs/dimensions/EsConfig.js
@@ -2,5 +2,5 @@
 export const TIME_ID = "time"
 export const GEO_ID = "geo"
 export const GEO_ENTITY = "Geo Entity"
-export const ADJ = "Adjust"
+export const ADJ = "Seasonal Adjustment"
 export const ADJ_ID = "s_adj"
