28ad2ca05d676b9e55137a97aa63983c5bb28a1a
Yevhenii Molodyi <eugmolody@gmail.com>
2021-11-18 15:36:54 +0200

    Add factory crRetryableLazy

---
+++ js/components/zhn-lazy/crRetryableLazy.js
@@ -0,0 +1,64 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+exports.__esModule = true;
+exports.default = void 0;
+
+var _react = require("react");
+
+var _useBool = _interopRequireDefault(require("../hooks/useBool"));
+
+var _jsxRuntime = require("react/jsx-runtime");
+
+const S_LOADING = {
+  color: '#2f7ed8'
+},
+      CL_BT = 'bt',
+      S_RETRY = {
+  color: '#1b2836'
+};
+
+const DfLoaderView = () => /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+  style: S_LOADING,
+  children: "Loading..."
+});
+
+const DfErrorRetryView = ({
+  retry
+}) => /*#__PURE__*/(0, _jsxRuntime.jsx)("button", {
+  className: CL_BT,
+  style: S_RETRY,
+  onClick: retry,
+  children: "Retry"
+});
+
+const crRetryableLazy = (crLoadPromise, LoaderView = DfLoaderView, ErrorRetryView = DfErrorRetryView) => {
+  const RetryableLazy = props => {
+    const [loading, retry, setLoadingFalse] = (0, _useBool.default)(true)
+    /*eslint-disable react-hooks/exhaustive-deps */
+    ,
+          LazyComponent = (0, _react.useMemo)(() => /*#__PURE__*/(0, _react.lazy)(() => crLoadPromise().catch(() => {
+      setLoadingFalse();
+      return {
+        default: () => /*#__PURE__*/(0, _jsxRuntime.jsx)(ErrorRetryView, {
+          retry: retry
+        })
+      };
+    })), [loading]); // crLoadPromise, retry
+
+    /*eslint-enable react-hooks/exhaustive-deps */
+
+    return /*#__PURE__*/(0, _jsxRuntime.jsx)(_react.Suspense, {
+      fallback: /*#__PURE__*/(0, _jsxRuntime.jsx)(LoaderView, {}),
+      children: /*#__PURE__*/(0, _jsxRuntime.jsx)(LazyComponent, { ...props
+      })
+    });
+  };
+
+  return RetryableLazy;
+};
+
+var _default = crRetryableLazy;
+exports.default = _default;
+//# sourceMappingURL=crRetryableLazy.js.map
---
+++ src/components/zhn-lazy/crRetryableLazy.jsx
@@ -0,0 +1,47 @@
+import { lazy, useMemo, Suspense } from 'react';
+import useBool from '../hooks/useBool';
+
+const S_LOADING = { color: '#2f7ed8'}
+, CL_BT = 'bt'
+, S_RETRY = { color: '#1b2836' };
+
+const DfLoaderView = () => (
+  <div style={S_LOADING}>Loading...</div>
+);
+const DfErrorRetryView = ({ retry }) => (
+  <button className={CL_BT} style={S_RETRY} onClick={retry}>
+    Retry
+  </button>
+);
+
+const crRetryableLazy = (
+	crLoadPromise,
+	LoaderView = DfLoaderView,
+	ErrorRetryView = DfErrorRetryView
+) => {
+	const RetryableLazy = props => {
+    const [loading, retry, setLoadingFalse] = useBool(true)
+    /*eslint-disable react-hooks/exhaustive-deps */
+		, LazyComponent = useMemo(() =>
+       lazy(() => crLoadPromise()
+         .catch(() => {
+            setLoadingFalse()
+					    return {
+              default: () => <ErrorRetryView retry={retry} />
+            };
+					})
+			 ), [loading]);
+    // crLoadPromise, retry
+    /*eslint-enable react-hooks/exhaustive-deps */
+
+		return (
+			<Suspense fallback={<LoaderView />}>
+				<LazyComponent {...props} />
+			</Suspense>
+		);
+	};
+
+	return RetryableLazy;
+};
+
+export default crRetryableLazy;
