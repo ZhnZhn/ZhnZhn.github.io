5041657a30954601b0d8460d33ac9fbb164cf2b9
Yevhenii Molodyi <eugmolody@gmail.com>
2021-11-19 16:09:27 +0200

    Edit EuroStatApi

--- js/adapters/eurostat/EuroStatApi.js
+++ js/adapters/eurostat/EuroStatApi.js
@@ -3,58 +3,57 @@
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 exports.__esModule = true;
-exports["default"] = void 0;
+exports.default = void 0;
 
 var _AdapterFn = _interopRequireDefault(require("../AdapterFn"));
 
 var _api = _interopRequireDefault(require("./api/api"));
 
-var MESSAGE_HEADER = '400: Bad Request\n',
-    RES_ERR_STATUS = [400],
-    MSG_400 = '400: Bad request.\nDataset contains no data. One or more filtering elements (query parameters) are probably not valid.\nMaybe try to request this data set with older date or another country.';
-var crError = _AdapterFn["default"].crError;
-
-var _crDetailMsg = function _crDetailMsg(label, option) {
-  var _option$alertGeo = option.alertGeo,
-      alertGeo = _option$alertGeo === void 0 ? '' : _option$alertGeo,
-      _option$alertMetric = option.alertMetric,
-      alertMetric = _option$alertMetric === void 0 ? '' : _option$alertMetric;
-  return MESSAGE_HEADER + label + ("\n\nIt seems country-dataset doesn't exsist.\n" + alertGeo + ":" + alertMetric + "\n\nIf you use For Date input field in Dialog\ntry to use more late date.");
+const RES_ERR_STATUS = [400],
+      MSG_400 = '400: Bad request.\nDataset contains no data. One or more filtering elements (query parameters) are probably not valid.\nMaybe try to request this data set with older date or another country.',
+      {
+  crError
+} = _AdapterFn.default,
+      _crDetailMsg = errLabel => errLabel + "\n\nIf you use For Date input field in Dialog\ntry to use more late date.",
+      _addPropTo = option => {
+  option.resErrStatus = [...RES_ERR_STATUS];
 };
 
-var _addPropTo = function _addPropTo(option) {
-  option.resErrStatus = [].concat(RES_ERR_STATUS);
-};
-
-var EuroStatApi = {
-  getRequestUrl: function getRequestUrl(option) {
+const EuroStatApi = {
+  getRequestUrl(option) {
     _addPropTo(option);
 
     if (option.url) {
       return option.url;
     }
 
-    var _url = _api["default"].crUrlN(option);
+    const _url = _api.default.crUrlN(option);
 
     return option.url = _url;
   },
-  checkResponse: function checkResponse(json, option, status) {
+
+  checkResponse(json, option, status) {
     if (status === 400) {
       throw crError('', MSG_400);
     }
 
-    var error = json.error;
+    const {
+      error
+    } = json;
 
     if (error) {
-      var label = error.label,
-          _msgErr = label ? _crDetailMsg(label, option) : void 0;
+      const {
+        label
+      } = error,
+            _msgErr = label ? _crDetailMsg(label) : void 0;
 
       throw crError('', _msgErr);
     }
 
     return true;
   }
+
 };
 var _default = EuroStatApi;
-exports["default"] = _default;
+exports.default = _default;
 //# sourceMappingURL=EuroStatApi.js.map
--- src/adapters/eurostat/EuroStatApi.js
+++ src/adapters/eurostat/EuroStatApi.js
@@ -1,18 +1,13 @@
 import AdapterFn from '../AdapterFn';
 import api from './api/api';
 
-const MESSAGE_HEADER = '400: Bad Request\n'
-, RES_ERR_STATUS = [ 400 ]
-, MSG_400 = '400: Bad request.\nDataset contains no data. One or more filtering elements (query parameters) are probably not valid.\nMaybe try to request this data set with older date or another country.';
+const RES_ERR_STATUS = [400]
+, MSG_400 = '400: Bad request.\nDataset contains no data. One or more filtering elements (query parameters) are probably not valid.\nMaybe try to request this data set with older date or another country.'
 
-const { crError } = AdapterFn;
-
-const _crDetailMsg = function(label, option){
-  const { alertGeo='', alertMetric='' } = option;
-  return MESSAGE_HEADER + label + `\n\nIt seems country-dataset doesn't exsist.\n${alertGeo}:${alertMetric}\n\nIf you use For Date input field in Dialog\ntry to use more late date.`;
-};
-
-const _addPropTo = (option) => {
+, { crError } = AdapterFn
+, _crDetailMsg = errLabel =>
+    `${errLabel}\n\nIf you use For Date input field in Dialog\ntry to use more late date.`
+, _addPropTo = option => {
   option.resErrStatus = [...RES_ERR_STATUS]
 };
 
@@ -35,7 +30,7 @@ const EuroStatApi = {
     if (error){
       const { label } = error
       , _msgErr = label
-          ? _crDetailMsg(label, option)
+          ? _crDetailMsg(label)
           : void 0
       throw crError('', _msgErr);
     }
