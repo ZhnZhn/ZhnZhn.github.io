7800935bde18bd9c00f3781a24023e779485066e
Yevhenii Molodyi <eugmolody@gmail.com>
2021-11-23 12:51:11 +0200

    Edit ModalPalette

--- js/components/zhn-moleculs/ModalPalette.js
+++ js/components/zhn-moleculs/ModalPalette.js
@@ -3,42 +3,39 @@
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 exports.__esModule = true;
-exports["default"] = void 0;
-
-var _jsxRuntime = require("react/jsx-runtime.js");
+exports.default = void 0;
 
 var _ModalPopup = _interopRequireDefault(require("./ModalPopup"));
 
 var _CellColorPane = _interopRequireDefault(require("./CellColorPane"));
 
-var S = {
-  SHOW_HIDE: {
-    zIndex: 1010,
-    position: 'absolute',
-    top: 35,
-    left: -10,
-    backgroundColor: 'inherit',
-    borderRadius: 5,
-    boxShadow: 'rgba(0, 0, 0, 0.2) 0 0 0 5px'
-  }
+var _jsxRuntime = require("react/jsx-runtime");
+
+const S_MODAL_POPUP = {
+  zIndex: 1010,
+  position: 'absolute',
+  top: 35,
+  left: -10,
+  backgroundColor: 'inherit',
+  borderRadius: 5,
+  boxShadow: 'rgba(0, 0, 0, 0.2) 0 0 0 5px'
 };
 
-var ModalPalette = function ModalPalette(_ref) {
-  var isShow = _ref.isShow,
-      model = _ref.model,
-      onClickCell = _ref.onClickCell,
-      onClose = _ref.onClose;
-  return /*#__PURE__*/(0, _jsxRuntime.jsx)(_ModalPopup["default"], {
-    style: S.SHOW_HIDE,
-    isShow: isShow,
-    onClose: onClose,
-    children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_CellColorPane["default"], {
-      model: model,
-      onClickCell: onClickCell
-    })
-  });
-};
+const ModalPalette = ({
+  isShow,
+  model,
+  onClickCell,
+  onClose
+}) => /*#__PURE__*/(0, _jsxRuntime.jsx)(_ModalPopup.default, {
+  style: S_MODAL_POPUP,
+  isShow: isShow,
+  onClose: onClose,
+  children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_CellColorPane.default, {
+    model: model,
+    onClickCell: onClickCell
+  })
+});
 
 var _default = ModalPalette;
-exports["default"] = _default;
+exports.default = _default;
 //# sourceMappingURL=ModalPalette.js.map
--- src/components/zhn-moleculs/ModalPalette.jsx
+++ src/components/zhn-moleculs/ModalPalette.jsx
@@ -1,21 +1,24 @@
 import ModalPopup from './ModalPopup';
-import CellColorPane from './CellColorPane'
+import CellColorPane from './CellColorPane';
 
-const S = {
-  SHOW_HIDE: {
-    zIndex: 1010,
-    position: 'absolute',
-    top: 35,
-    left: -10,
-    backgroundColor: 'inherit',
-    borderRadius: 5,
-    boxShadow: 'rgba(0, 0, 0, 0.2) 0 0 0 5px'
-  }
+const S_MODAL_POPUP = {
+  zIndex: 1010,
+  position: 'absolute',
+  top: 35,
+  left: -10,
+  backgroundColor: 'inherit',
+  borderRadius: 5,
+  boxShadow: 'rgba(0, 0, 0, 0.2) 0 0 0 5px'
 };
 
-const ModalPalette = ({ isShow, model, onClickCell, onClose }) => (
+const ModalPalette = ({
+  isShow,
+  model,
+  onClickCell,
+  onClose
+}) => (
   <ModalPopup
-    style={S.SHOW_HIDE}
+    style={S_MODAL_POPUP}
     isShow={isShow}
     onClose={onClose}
   >
