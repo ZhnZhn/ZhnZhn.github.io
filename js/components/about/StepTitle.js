e8cdda8eba5f48c8fc5dcce22af3c6169301425f
Yevhenii Molodyi <eugmolody@gmail.com>
2021-12-28 12:25:12 +0200

    Edit StepTitle style

--- js/components/about/StepTitle.js
+++ js/components/about/StepTitle.js
@@ -5,29 +5,34 @@ exports.default = void 0;
 
 var _jsxRuntime = require("react/jsx-runtime");
 
-const S_STEP = {
+var STEP_COLOR = '#80c040',
+    S_PB_4 = {
+  paddingBottom: 4
+},
+    S_STEP = {
   display: 'inline-block',
-  color: '#80c040',
-  lineHeight: '24px',
+  color: STEP_COLOR,
   width: 26,
   height: 26,
-  border: '2px solid #80c040',
+  border: "2px solid " + STEP_COLOR,
   borderRadius: '50%',
   textAlign: 'center',
   textTransform: 'uppercase'
 };
 
-const StepTitle = ({
-  step,
-  title
-}) => /*#__PURE__*/(0, _jsxRuntime.jsxs)("p", {
-  children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-    style: S_STEP,
-    children: step
-  }), /*#__PURE__*/(0, _jsxRuntime.jsxs)("span", {
-    children: ["\xA0", title, "."]
-  })]
-});
+var StepTitle = function StepTitle(_ref) {
+  var step = _ref.step,
+      title = _ref.title;
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)("p", {
+    style: S_PB_4,
+    children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+      style: S_STEP,
+      children: step
+    }), /*#__PURE__*/(0, _jsxRuntime.jsxs)("span", {
+      children: ["\xA0", title, "."]
+    })]
+  });
+};
 
 var _default = StepTitle;
 exports.default = _default;
--- src/components/about/StepTitle.jsx
+++ src/components/about/StepTitle.jsx
@@ -1,17 +1,18 @@
-const S_STEP = {
+const STEP_COLOR = '#80c040' 
+, S_PB_4 = { paddingBottom: 4 }
+, S_STEP = {
   display: 'inline-block',
-  color: '#80c040',
-  lineHeight: '24px',
+  color: STEP_COLOR,
   width: 26,
   height: 26,
-  border: '2px solid #80c040',
+  border: `2px solid ${STEP_COLOR}`,
   borderRadius: '50%',
   textAlign: 'center',
   textTransform: 'uppercase'
 };
 
 const StepTitle = ({ step, title }) => (
-  <p>
+  <p style={S_PB_4}>
     <span style={S_STEP}>
         {step}
     </span>
