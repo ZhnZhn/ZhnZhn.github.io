09a7080a94b13108de61edcffea7de83dff75946
Yevhenii Molodyi <eugmolody@gmail.com>
2021-12-29 14:06:22 +0200

    Edit babel config

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,21 @@
-module.exports = {
+module.exports = {  
   presets: [
     ['@babel/preset-env', { 
        loose: true,
        useBuiltIns: "entry",
-       corejs: "3",      
+       corejs: "3",
+       //debug: true,      
        targets: {
          browsers: [
             ">0.25%", 
+            /* support arrow-functions */
             "not ie 11", 
             "not op_mini all", 
+            "not android 4.4.3-4.4.4", 
             "not dead"
          ] 
-       } 
+       },
+       exclude: []      
     }],
     ['@babel/preset-react', { runtime: "automatic" }]
   ],
