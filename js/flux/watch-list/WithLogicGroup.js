bc63ed64a84b873ca164a3cfb485593ca48e1ab5
Yevhenii Molodyi <eugmolody@gmail.com>
2022-05-06 16:46:09 +0300

    Replace watch-list WithLogicGroup default exports by named exports

--- js/flux/watch-list/WithLogicGroup.js
+++ js/flux/watch-list/WithLogicGroup.js
@@ -1,66 +1,67 @@
 "use strict";
 
 exports.__esModule = true;
-exports.default = void 0;
+exports.renameGroup = exports.deleteGroup = exports.addGroup = void 0;
 
 var _LogicFn = require("./LogicFn");
 
-const WithLogicGroup = {
-  addGroup(watchList, _ref) {
-    let {
-      caption
-    } = _ref;
-    const groups = watchList.groups;
+const addGroup = (watchList, _ref) => {
+  let {
+    caption
+  } = _ref;
+  const groups = watchList.groups;
 
-    if ((0, _LogicFn.isInArraySameCaption)(groups, caption)) {
-      return (0, _LogicFn.crMsgGroupExisted)(caption);
-    }
-
-    const _captionObj = caption ? {
-      caption
-    } : {
-      caption: "Default"
-    };
-
-    watchList.groups = (0, _LogicFn.getArrayWithObj)(groups, _captionObj);
-    return {
-      isDone: true
-    };
-  },
-
-  renameGroup(watchList, _ref2) {
-    let {
-      captionFrom,
-      captionTo
-    } = _ref2;
-    const groups = watchList.groups,
-          groupIndex = (0, _LogicFn.findIndex)(groups, captionFrom);
-
-    if (groupIndex === -1) {
-      return (0, _LogicFn.crMsgNotFound)('group', captionFrom);
-    }
-
-    if ((0, _LogicFn.isInArraySameCaption)(groups, captionTo)) {
-      return (0, _LogicFn.crMsgGroupExisted)(captionTo);
-    }
-
-    watchList.groups = (0, _LogicFn.getArrayWithRename)(groups, groupIndex, captionTo);
-    return {
-      isDone: true
-    };
-  },
-
-  deleteGroup(watchList, _ref3) {
-    let {
-      caption
-    } = _ref3;
-    watchList.groups = (0, _LogicFn.filter)(watchList.groups, caption);
-    return {
-      isDone: true
-    };
+  if ((0, _LogicFn.isInArraySameCaption)(groups, caption)) {
+    return (0, _LogicFn.crMsgGroupExisted)(caption);
   }
 
+  const _captionObj = caption ? {
+    caption
+  } : {
+    caption: "Default"
+  };
+
+  watchList.groups = (0, _LogicFn.getArrayWithObj)(groups, _captionObj);
+  return {
+    isDone: true
+  };
 };
-var _default = WithLogicGroup;
-exports.default = _default;
+
+exports.addGroup = addGroup;
+
+const renameGroup = (watchList, _ref2) => {
+  let {
+    captionFrom,
+    captionTo
+  } = _ref2;
+  const groups = watchList.groups,
+        groupIndex = (0, _LogicFn.findIndex)(groups, captionFrom);
+
+  if (groupIndex === -1) {
+    return (0, _LogicFn.crMsgNotFound)('group', captionFrom);
+  }
+
+  if ((0, _LogicFn.isInArraySameCaption)(groups, captionTo)) {
+    return (0, _LogicFn.crMsgGroupExisted)(captionTo);
+  }
+
+  watchList.groups = (0, _LogicFn.getArrayWithRename)(groups, groupIndex, captionTo);
+  return {
+    isDone: true
+  };
+};
+
+exports.renameGroup = renameGroup;
+
+const deleteGroup = (watchList, _ref3) => {
+  let {
+    caption
+  } = _ref3;
+  watchList.groups = (0, _LogicFn.filter)(watchList.groups, caption);
+  return {
+    isDone: true
+  };
+};
+
+exports.deleteGroup = deleteGroup;
 //# sourceMappingURL=WithLogicGroup.js.map
--- src/flux/watch-list/WithLogicGroup.js
+++ src/flux/watch-list/WithLogicGroup.js
@@ -9,26 +9,30 @@ import {
   filter
 } from './LogicFn';
 
-const WithLogicGroup = {
+export const addGroup = (
+  watchList,
+  { caption }
+) => {
+  const groups = watchList.groups;
 
-  addGroup(watchList, { caption }){
-    const groups = watchList.groups;
+  if ( isInArraySameCaption(groups, caption)){
+    return crMsgGroupExisted(caption);
+  }
 
-    if ( isInArraySameCaption(groups, caption)){
-      return crMsgGroupExisted(caption);
-    }
+  const _captionObj = caption
+    ? { caption }
+    : { caption: "Default" };
 
-    const _captionObj = (caption)
-             ? { caption }
-             : { caption: "Default" };
+  watchList.groups = getArrayWithObj(groups, _captionObj);
+  return { isDone: true};
+}
 
-    watchList.groups = getArrayWithObj(groups, _captionObj);
-    return { isDone: true};
-  },
-
-  renameGroup(watchList, {captionFrom, captionTo}){
+export const renameGroup = (
+  watchList,
+  {captionFrom, captionTo}
+) => {
     const groups = watchList.groups
-        , groupIndex = findIndex(groups, captionFrom);
+    , groupIndex = findIndex(groups, captionFrom);
 
     if (groupIndex === -1){
       return crMsgNotFound('group', captionFrom);
@@ -39,13 +43,12 @@ const WithLogicGroup = {
 
     watchList.groups = getArrayWithRename(groups, groupIndex, captionTo)
     return { isDone: true };
-  },
+}
 
-  deleteGroup(watchList, {caption}){
-    watchList.groups = filter(watchList.groups, caption);
-    return { isDone: true };
-  }
-
-};
-
-export default WithLogicGroup
+export const deleteGroup = (
+  watchList,
+  {caption}
+) => {
+  watchList.groups = filter(watchList.groups, caption);
+  return { isDone: true };
+}
