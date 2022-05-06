072482f232a142112a90a9e23be42daf81effb10
Yevhenii Molodyi <eugmolody@gmail.com>
2022-05-06 17:09:59 +0300

    Edit WatchDefault

--- js/constants/WatchDefault.js
+++ js/constants/WatchDefault.js
@@ -1,64 +1,23 @@
 "use strict";
 
 exports.__esModule = true;
-exports["default"] = void 0;
-var WatchDefault = {
-  groups: [{
-    caption: 'Economic Metrics',
-    lists: [{
-      caption: 'List1'
-    }, {
-      caption: 'List2'
-    }, {
-      caption: 'List3'
-    }]
+exports.default = void 0;
+
+const GROUP_CAPTIONS = ['Economic Metrics', 'Currencies', 'Commodities', 'Stocks', 'Indexes', 'Futures'],
+      _crGroup = caption => ({
+  caption,
+  lists: [{
+    caption: 'List1'
   }, {
-    caption: 'Currencies',
-    lists: [{
-      caption: 'List1'
-    }, {
-      caption: 'List2'
-    }, {
-      caption: 'List3'
-    }]
+    caption: 'List2'
   }, {
-    caption: 'Commodities',
-    lists: [{
-      caption: 'List1'
-    }, {
-      caption: 'List2'
-    }, {
-      caption: 'List3'
-    }]
-  }, {
-    caption: 'Stocks',
-    lists: [{
-      caption: 'List1'
-    }, {
-      caption: 'List2'
-    }, {
-      caption: 'List3'
-    }]
-  }, {
-    caption: 'Indexes',
-    lists: [{
-      caption: 'List1'
-    }, {
-      caption: 'List2'
-    }, {
-      caption: 'List3'
-    }]
-  }, {
-    caption: 'Futures',
-    lists: [{
-      caption: 'List1'
-    }, {
-      caption: 'List2'
-    }, {
-      caption: 'List3'
-    }]
+    caption: 'List3'
   }]
+});
+
+const WatchDefault = {
+  groups: GROUP_CAPTIONS.map(_crGroup)
 };
 var _default = WatchDefault;
-exports["default"] = _default;
+exports.default = _default;
 //# sourceMappingURL=WatchDefault.js.map
--- src/constants/WatchDefault.js
+++ src/constants/WatchDefault.js
@@ -1,55 +1,22 @@
+const GROUP_CAPTIONS = [
+  'Economic Metrics',
+  'Currencies',
+  'Commodities',
+  'Stocks',
+  'Indexes',
+  'Futures',
+]
+, _crGroup = caption => ({
+  caption,
+  lists : [
+    { caption : 'List1'},
+    { caption : 'List2'},
+    { caption : 'List3'}
+  ]
+});
 
 const WatchDefault = {
-    groups : [
-      {
-         caption : 'Economic Metrics',
-         lists : [
-           { caption : 'List1'},
-           { caption : 'List2'},
-           { caption : 'List3'}
-         ]
-      },
-      {
-        caption : 'Currencies' ,
-        lists : [
-          { caption : 'List1' },
-          { caption : 'List2' },
-          { caption : 'List3' }
-        ]
-      },
-      {
-        caption : 'Commodities',
-        lists : [
-          { caption : 'List1' },
-          { caption : 'List2' },
-          { caption : 'List3' }
-        ]
-      },
-      {
-        caption : 'Stocks' ,
-        lists : [
-          { caption : 'List1' },
-          { caption : 'List2' },
-          { caption : 'List3' }
-        ]
-      },
-      {
-        caption : 'Indexes',
-        lists : [
-          { caption : 'List1' },
-          { caption : 'List2' },
-          { caption : 'List3' }
-        ]
-      },
-      {
-        caption : 'Futures',
-        lists : [
-          { caption : 'List1' },
-          { caption : 'List2' },
-          { caption : 'List3' }
-        ]
-      }
-    ]
+  groups: GROUP_CAPTIONS.map(_crGroup)
 }
 
 export default WatchDefault
