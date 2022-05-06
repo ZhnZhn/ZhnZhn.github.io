3d1c7e0ef786c615975466ff7a4331b29ce2b97d
Yevhenii Molodyi <eugmolody@gmail.com>
2022-05-06 16:52:40 +0300

    Replace watch-list WithLogicItem default export by named exports

--- js/flux/watch-list/WithLogicItem.js
+++ js/flux/watch-list/WithLogicItem.js
@@ -1,65 +1,64 @@
 "use strict";
 
 exports.__esModule = true;
-exports.default = void 0;
+exports.removeItem = exports.addItem = void 0;
 
 var _LogicFn = require("./LogicFn");
 
-const WithLogicItem = {
-  addItem(watchList, item) {
-    const {
-      caption,
-      groupCaption,
-      listCaption,
-      config
-    } = item,
-          {
-      zhConfig
-    } = config,
-          {
-      id,
-      title,
-      subtitle,
-      itemConf = {}
-    } = zhConfig,
-          toGroup = (0, _LogicFn.findGroup)(watchList, groupCaption),
-          toList = (0, _LogicFn.findList)(toGroup, listCaption);
+const addItem = (watchList, item) => {
+  const {
+    caption,
+    groupCaption,
+    listCaption,
+    config
+  } = item,
+        {
+    zhConfig
+  } = config,
+        {
+    id,
+    title,
+    subtitle,
+    itemConf = {}
+  } = zhConfig,
+        toGroup = (0, _LogicFn.findGroup)(watchList, groupCaption),
+        toList = (0, _LogicFn.findList)(toGroup, listCaption);
 
-    if ((0, _LogicFn.isInArraySameCaption)(toList.items, caption)) {
-      return (0, _LogicFn.crMsgItemExisted)(caption, listCaption);
-    }
-
-    const _item = {
-      id,
-      title,
-      subtitle,
-      caption,
-      itemConf
-    };
-
-    if (toList.items) {
-      toList.items.push(_item);
-    } else {
-      toList.items = [_item];
-    }
-
-    return {
-      isDone: true
-    };
-  },
-
-  removeItem(watchList, _ref) {
-    let {
-      groupCaption,
-      listCaption,
-      caption
-    } = _ref;
-    const groupFrom = (0, _LogicFn.findGroup)(watchList, groupCaption),
-          listFrom = (0, _LogicFn.findList)(groupFrom, listCaption);
-    listFrom.items = (0, _LogicFn.filter)(listFrom.items, caption);
+  if ((0, _LogicFn.isInArraySameCaption)(toList.items, caption)) {
+    return (0, _LogicFn.crMsgItemExisted)(caption, listCaption);
   }
 
+  const _item = {
+    id,
+    title,
+    subtitle,
+    caption,
+    itemConf
+  };
+
+  if (toList.items) {
+    toList.items.push(_item);
+  } else {
+    toList.items = [_item];
+  }
+
+  return {
+    isDone: true
+  };
 };
-var _default = WithLogicItem;
-exports.default = _default;
+
+exports.addItem = addItem;
+
+const removeItem = (watchList, _ref) => {
+  let {
+    groupCaption,
+    listCaption,
+    caption
+  } = _ref;
+  const groupFrom = (0, _LogicFn.findGroup)(watchList, groupCaption),
+        listFrom = (0, _LogicFn.findList)(groupFrom, listCaption);
+  listFrom.items = (0, _LogicFn.filter)(listFrom.items, caption);
+};
+
+exports.removeItem = removeItem;
 //# sourceMappingURL=WithLogicItem.js.map
--- src/flux/watch-list/WithLogicItem.js
+++ src/flux/watch-list/WithLogicItem.js
@@ -6,45 +6,48 @@ import {
   filter
 } from './LogicFn';
 
-const WithLogicItem = {
+export const addItem = (
+  watchList,
+  item
+) => {
+  const {
+    caption,
+    groupCaption,
+    listCaption,
+    config
+  } = item
+  , { zhConfig } = config
+  , {
+     id, title, subtitle,
+     itemConf={}
+  } = zhConfig
+  , toGroup = findGroup(watchList, groupCaption)
+  , toList = findList(toGroup, listCaption);
 
-  addItem(watchList, item){
-    const {
-      caption,
-      groupCaption, listCaption,
-      config
-    } = item
-    , { zhConfig } = config
-    , {
-       id, title, subtitle,
-       itemConf={}
-    } = zhConfig
-    , toGroup = findGroup(watchList, groupCaption)
-    , toList = findList(toGroup, listCaption);
-
-    if ( isInArraySameCaption(toList.items, caption) ){
-      return crMsgItemExisted(caption, listCaption);
-    }
-    const _item = {
-       id, title, subtitle,
-       caption,
-       itemConf
-     }
-    if (toList.items){
-      toList.items.push(_item);
-    } else {
-      toList.items = [_item];
-    }
-    return { isDone: true };
-  },
-
-  removeItem(watchList, {groupCaption, listCaption, caption}){
-    const groupFrom = findGroup(watchList, groupCaption)
-        , listFrom = findList(groupFrom, listCaption);
-
-    listFrom.items = filter(listFrom.items, caption);
+  if ( isInArraySameCaption(toList.items, caption) ){
+    return crMsgItemExisted(caption, listCaption);
   }
+  const _item = {
+     id,
+     title,
+     subtitle,
+     caption,
+     itemConf
+   }
+  if (toList.items){
+    toList.items.push(_item);
+  } else {
+    toList.items = [_item];
+  }
+  return { isDone: true };
+}
 
-};
+export const removeItem = (
+  watchList,
+  {groupCaption, listCaption, caption}
+) => {
+  const groupFrom = findGroup(watchList, groupCaption)
+  , listFrom = findList(groupFrom, listCaption);
 
-export default WithLogicItem
+  listFrom.items = filter(listFrom.items, caption);
+}
