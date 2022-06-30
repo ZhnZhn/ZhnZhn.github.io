1c551df8d08a2b7d31c6b67b2e2cd111b3d4aaa9
Yevhenii Molodyi <eugmolody@gmail.com>
2022-06-30 14:27:56 +0300

    Add memoize-one to findItem

--- js/flux/stores/browser/findItem.js
+++ js/flux/stores/browser/findItem.js
@@ -1,21 +1,17 @@
 "use strict";
 
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
 exports.__esModule = true;
-exports["default"] = void 0;
+exports.default = void 0;
 
-function _createForOfIteratorHelperLoose(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; return function () { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } it = o[Symbol.iterator](); return it.next.bind(it); }
+var _memoizeOne = _interopRequireDefault(require("memoize-one"));
 
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+const _isArr = Array.isArray;
 
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-var _isArr = Array.isArray;
-
-var _findOrCheckItem = function _findOrCheckItem(item, chartType) {
+const _findItem = (item, chartType) => {
   if (_isArr(item.items)) {
-    for (var _iterator = _createForOfIteratorHelperLoose(item.items), _step; !(_step = _iterator()).done;) {
-      var subItem = _step.value;
-
+    for (const subItem of item.items) {
       if (subItem.id === chartType) {
         return subItem;
       }
@@ -25,29 +21,33 @@ var _findOrCheckItem = function _findOrCheckItem(item, chartType) {
   }
 };
 
-var findItem = function findItem(menu, chartType) {
+const findItem = (menu, chartType) => {
   if (!_isArr(menu)) {
     return;
   }
 
-  for (var _iterator2 = _createForOfIteratorHelperLoose(menu), _step2; !(_step2 = _iterator2()).done;) {
-    var topics = _step2.value;
-    var items = topics.items;
+  for (const topics of menu) {
+    const items = topics.items;
 
     if (_isArr(items)) {
-      for (var _iterator3 = _createForOfIteratorHelperLoose(items), _step3; !(_step3 = _iterator3()).done;) {
-        var item = _step3.value;
-
-        var _item = _findOrCheckItem(item, chartType);
+      for (const item of items) {
+        const _item = _findItem(item, chartType);
 
         if (_item) {
           return _item;
         }
       }
+    } else {
+      const _item = _findItem(topics, chartType);
+
+      if (_item) {
+        return _item;
+      }
     }
   }
 };
 
-var _default = findItem;
-exports["default"] = _default;
+var _default = (0, _memoizeOne.default)(findItem);
+
+exports.default = _default;
 //# sourceMappingURL=findItem.js.map
--- src/flux/stores/browser/findItem.js
+++ src/flux/stores/browser/findItem.js
@@ -1,8 +1,11 @@
+import memoizeOne from "memoize-one";
 
 const _isArr = Array.isArray;
 
-
-const _findOrCheckItem = (item, chartType) => {
+const _findItem = (
+  item,
+  chartType
+) => {
   if (_isArr(item.items)) {
     for(const subItem of item.items) {
       if (subItem.id === chartType) {
@@ -14,18 +17,24 @@ const _findOrCheckItem = (item, chartType) => {
   }
 };
 
-const findItem = (menu, chartType) => {
+const findItem = (
+  menu,
+  chartType
+) => {
   if (!_isArr(menu)) {return;}
 
   for (const topics of menu){
     const items = topics.items;
     if (_isArr(items)) {
       for(const item of items){
-        const _item = _findOrCheckItem(item, chartType)
+        const _item = _findItem(item, chartType)
         if (_item) {return _item;}
       }
+    } else {
+      const _item = _findItem(topics, chartType)
+      if (_item) {return _item;}
     }
   }
 };
 
-export default findItem
+export default memoizeOne(findItem)
