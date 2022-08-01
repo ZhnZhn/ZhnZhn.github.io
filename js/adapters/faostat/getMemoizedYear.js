07c4dd0a18fc65e32577b654fe81a18762c40ca1
Yevhenii Molodyi <eugmolody@gmail.com>
2022-08-01 16:07:24 +0300

    Add function getMemoizedYear

---
+++ js/adapters/faostat/getMemoizedYear.js
@@ -0,0 +1,23 @@
+"use strict";
+
+exports.__esModule = true;
+exports.default = void 0;
+
+let _memoized_year;
+
+const _crYear = () => {
+  const year = new Date().getUTCFullYear() + 1,
+        arr = [];
+  let i = 1980;
+
+  for (; i < year; i++) {
+    arr.push(i);
+  }
+
+  return _memoized_year = arr.join(',');
+},
+      getMemoizedYear = () => _memoized_year || _crYear();
+
+var _default = getMemoizedYear;
+exports.default = _default;
+//# sourceMappingURL=getMemoizedYear.js.map
---
+++ src/adapters/faostat/getMemoizedYear.js
@@ -0,0 +1,14 @@
+
+let _memoized_year;
+const _crYear = () => {
+  const year = (new Date()).getUTCFullYear()+1
+  , arr = [];
+  let i = 1980;
+  for(;i<year;i++){
+    arr.push(i)
+  }
+  return _memoized_year = arr.join(',');
+}
+, getMemoizedYear = () => _memoized_year || _crYear();
+
+export default getMemoizedYear
