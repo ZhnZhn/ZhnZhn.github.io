af4e08abff8200badd876d2d58ab9d9110d04548
Yevhenii Molodyi <eugmolody@gmail.com>
2023-01-10 11:45:33 +0200

    Add uiApi getRefValue to items useVm

--- js/components/items/useVm.js
+++ js/components/items/useVm.js
@@ -2,20 +2,15 @@
 
 exports.__esModule = true;
 exports.default = void 0;
-
 var _uiApi = require("../uiApi");
-
 const useVm = () => {
   const refVm = (0, _uiApi.useRef)(),
-        compareTo = (0, _uiApi.useCallback)(dateTo => {
-    const {
-      current
-    } = refVm;
-    return current ? current._updateDateTo(dateTo) : void 0;
-  }, []);
+    compareTo = (0, _uiApi.useCallback)(dateTo => {
+      const _vmInst = (0, _uiApi.getRefValue)(refVm);
+      return _vmInst ? _vmInst._updateDateTo(dateTo) : void 0;
+    }, []);
   return [refVm, compareTo];
 };
-
 var _default = useVm;
 exports.default = _default;
 //# sourceMappingURL=useVm.js.map
--- src/components/items/useVm.js
+++ src/components/items/useVm.js
@@ -1,17 +1,21 @@
 import {
   useRef,
-  useCallback
+  useCallback,
+  getRefValue
 } from '../uiApi';
 
 const useVm = () => {
   const refVm = useRef()
   , compareTo = useCallback(dateTo => {
-    const { current } = refVm;
-    return current
-      ? current._updateDateTo(dateTo)
+    const _vmInst = getRefValue(refVm);
+    return _vmInst
+      ? _vmInst._updateDateTo(dateTo)
       : void 0;
   }, []);
-  return [refVm, compareTo];
+  return [
+    refVm,
+    compareTo
+  ];
 };
 
 export default useVm
