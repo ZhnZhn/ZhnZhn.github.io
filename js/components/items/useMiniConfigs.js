270199ee7457eb378415b325dd99128eb9ad7804
Yevhenii Molodyi <eugmolody@gmail.com>
2023-01-10 11:41:00 +0200

    Replace useCallbacks by useMemo in item useMiniConfigs

--- js/components/items/useMiniConfigs.js
+++ js/components/items/useMiniConfigs.js
@@ -2,26 +2,22 @@
 
 exports.__esModule = true;
 exports.default = void 0;
-
 var _uiApi = require("../uiApi");
-
 const useMiniConfigs = () => {
   const [miniConfigs, setMiniConfigs] = (0, _uiApi.useState)([]),
-        addConfig = (0, _uiApi.useCallback)((config, id) => {
-    setMiniConfigs(prevState => {
-      prevState.push({
-        config,
-        id
+    [addConfig, removeConfig] = (0, _uiApi.useMemo)(() => [(config, id) => {
+      setMiniConfigs(prevState => {
+        prevState.push({
+          config,
+          id
+        });
+        return [...prevState];
       });
-      return [...prevState];
-    });
-  }, []),
-        removeConfig = (0, _uiApi.useCallback)(id => {
-    setMiniConfigs(prevState => prevState.filter(c => c.id !== id));
-  }, []);
+    }, id => {
+      setMiniConfigs(prevState => prevState.filter(c => c.id !== id));
+    }], []);
   return [miniConfigs, addConfig, removeConfig];
 };
-
 var _default = useMiniConfigs;
 exports.default = _default;
 //# sourceMappingURL=useMiniConfigs.js.map
--- src/components/items/useMiniConfigs.js
+++ src/components/items/useMiniConfigs.js
@@ -1,21 +1,34 @@
 import {
   useState,
-  useCallback
+  useMemo
 } from '../uiApi';
 
 const useMiniConfigs = () => {
-  const [miniConfigs, setMiniConfigs] = useState([])
-  , addConfig = useCallback((config, id) => {
-      setMiniConfigs(prevState => {
-        prevState.push({ config, id })
-        return [...prevState];
-      })
-    }, [])
-  , removeConfig = useCallback((id) => {
-      setMiniConfigs(prevState => prevState
-       .filter(c => c.id !== id))
-    }, []);
-  return [miniConfigs, addConfig, removeConfig];
+  const [
+    miniConfigs,
+    setMiniConfigs
+  ] = useState([])
+  , [
+    addConfig,
+    removeConfig
+  ] = useMemo(() => [
+    (config, id) => {
+       setMiniConfigs(prevState => {
+         prevState.push({ config, id })
+         return [...prevState];
+       })
+    },
+    (id) => {
+       setMiniConfigs(prevState => prevState
+         .filter(c => c.id !== id))
+    }
+  ], []);
+
+  return [
+    miniConfigs,
+    addConfig,
+    removeConfig
+  ];
 };
 
 export default useMiniConfigs
