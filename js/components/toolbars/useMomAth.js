b23f15303c6a86518126d110cf7cabed28a16b08
Yevhenii Molodyi <eugmolody@gmail.com>
2023-02-13 12:47:52 +0200

    Add useMomAth

---
+++ js/components/toolbars/useMomAth.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+exports.__esModule = true;
+exports.default = void 0;
+var _uiApi = require("../uiApi");
+var _useBool = _interopRequireDefault(require("../hooks/useBool"));
+var _IndicatorBuilder = require("../../charts/IndicatorBuilder");
+const MOM_ATH = 'MOM_ATH';
+const useMomAth = (isMomAth, getChart, onAddMfi, onRemoveMfi) => {
+  /*eslint-disable react-hooks/exhaustive-deps */
+  const [_isMomAth, showMomAth, hideMomAth] = (0, _useBool.default)(),
+    [_addMomAth, _removeMomAth] = (0, _uiApi.useMemo)(() => isMomAth ? [() => {
+      const _chart = getChart(),
+        _momAthConfig = (0, _IndicatorBuilder.crMomAthConfig)(_chart);
+      if (_momAthConfig) {
+        onAddMfi(_momAthConfig, MOM_ATH);
+        showMomAth();
+      }
+    }, () => {
+      onRemoveMfi(MOM_ATH);
+      hideMomAth();
+    }] : [], [isMomAth, getChart, onAddMfi, onRemoveMfi]);
+  return [_isMomAth, _addMomAth, _removeMomAth];
+};
+var _default = useMomAth;
+exports.default = _default;
+//# sourceMappingURL=useMomAth.js.map
---
+++ src/components/toolbars/useMomAth.js
@@ -0,0 +1,47 @@
+import { useMemo } from '../uiApi';
+import useBool from '../hooks/useBool';
+
+import {
+  crMomAthConfig
+} from '../../charts/IndicatorBuilder';
+
+const MOM_ATH = 'MOM_ATH';
+
+const useMomAth = (
+  isMomAth,
+  getChart,
+  onAddMfi,
+  onRemoveMfi
+) => {
+  /*eslint-disable react-hooks/exhaustive-deps */
+  const [
+    _isMomAth,
+    showMomAth,
+    hideMomAth
+  ] = useBool()
+  , [
+    _addMomAth,
+    _removeMomAth
+  ] = useMemo(() => isMomAth ? [
+    () => {
+      const _chart = getChart()
+      , _momAthConfig = crMomAthConfig(_chart);
+      if (_momAthConfig) {
+        onAddMfi(_momAthConfig, MOM_ATH)
+        showMomAth()
+      }
+    },
+    () => {
+      onRemoveMfi(MOM_ATH)
+      hideMomAth()
+    }
+  ] : [], [isMomAth, getChart, onAddMfi, onRemoveMfi]);
+
+  return [
+    _isMomAth,
+    _addMomAth,
+    _removeMomAth
+  ];
+}
+
+export default useMomAth
