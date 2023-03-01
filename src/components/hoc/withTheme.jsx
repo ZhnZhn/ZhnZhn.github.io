d3d514806bf0f0765e843226578483abafac5851
Yevhenii Molodyi <eugmolody@gmail.com>
2023-03-01 14:35:34 +0200

    Delete hoc withTheme, withThemeRef

--- src/components/hoc/withTheme.jsx
+++
@@ -1,9 +0,0 @@
-import ThemeContext from './ThemeContext';
-
-const withTheme = Wrapper => props => (
-  <ThemeContext.Consumer>
-    { theme => <Wrapper {...props} theme={theme} /> }
-  </ThemeContext.Consumer>
-);
-
-export default withTheme
