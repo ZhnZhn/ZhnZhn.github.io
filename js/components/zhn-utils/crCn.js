45e3617975fc72f262843d695963382b13bf5c95
Yevhenii Molodyi <eugmolody@gmail.com>
2023-03-14 16:13:47 +0200

    Add named parameters to crCn function

--- js/components/zhn-utils/crCn.js
+++ js/components/zhn-utils/crCn.js
@@ -1,20 +1,14 @@
 "use strict";
 
 exports.__esModule = true;
-exports["default"] = void 0;
-var _isArr = Array.isArray;
-
-var _getCn = function _getCn(arrOrStr) {
-  return _isArr(arrOrStr) ? arrOrStr[0] ? arrOrStr[1] : '' : arrOrStr || '';
-};
-
-var crCn = function crCn() {
-  var _cl1 = _getCn(arguments.length <= 0 ? undefined : arguments[0]),
-      _cl2 = _getCn(arguments.length <= 1 ? undefined : arguments[1]);
-
+exports.default = void 0;
+const _isArr = Array.isArray;
+const _getCn = arrOrStr => _isArr(arrOrStr) ? arrOrStr[0] ? arrOrStr[1] : '' : arrOrStr || '';
+const crCn = (arrOrStr1, arrOrStr2) => {
+  const _cl1 = _getCn(arrOrStr1),
+    _cl2 = _getCn(arrOrStr2);
   return _cl1 ? _cl2 ? _cl1 + " " + _cl2 : _cl1 : _cl2 || void 0;
 };
-
 var _default = crCn;
-exports["default"] = _default;
+exports.default = _default;
 //# sourceMappingURL=crCn.js.map
--- src/components/zhn-utils/crCn.js
+++ src/components/zhn-utils/crCn.js
@@ -4,9 +4,12 @@ const _getCn = (arrOrStr) => _isArr(arrOrStr)
   ? arrOrStr[0] ? arrOrStr[1] : ''
   : arrOrStr || '';
 
-const crCn = (...args) => {
-  const _cl1 = _getCn(args[0])
-  , _cl2 = _getCn(args[1]);
+const crCn = (
+  arrOrStr1,
+  arrOrStr2
+) => {
+  const _cl1 = _getCn(arrOrStr1)
+  , _cl2 = _getCn(arrOrStr2);
   return _cl1
     ? _cl2 ? `${_cl1} ${_cl2}` : _cl1
     : _cl2 || void 0 ;
