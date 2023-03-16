136a3b640383cbcd177f41a7048ab19c39d3f906
Yevhenii Molodyi <eugmolody@gmail.com>
2023-03-16 14:59:11 +0200

    Add option round values to ModalOptions function component

--- js/components/dialogs/modals/ModalOptions.js
+++ js/components/dialogs/modals/ModalOptions.js
@@ -4,19 +4,34 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 exports.__esModule = true;
 exports.default = void 0;
 var _ModalPopup = _interopRequireDefault(require("../../zhn-moleculs/ModalPopup"));
+var _InputText = _interopRequireDefault(require("../../zhn/InputText"));
 var _RowCheckBox = _interopRequireDefault(require("../rows/RowCheckBox2"));
 var _Style = require("./Style");
 var _jsxRuntime = require("react/jsx-runtime");
+const S_DIV_INPUT = {
+  margin: '6px 0'
+};
+const S_CAPTION = {
+  paddingRight: 4,
+  color: 'black',
+  fontSize: '16px',
+  fontWeight: 'bold'
+};
+const MIN_RT = 0,
+  MAX_RT = 3;
+const _isRt = rt => rt === '' || rt >= MIN_RT && rt <= MAX_RT;
 const ROW_CHECKBOX_CONFIGS = [["isNotZoomToMinMax", "Not Zoom to Min-Max"], ["isFilterZero", "Filter Trim Zero Values"], ["isLogarithmic", "Logarithmic Scale"]];
 const ModalOptions = _ref => {
   let {
     isShow,
     style,
     className = _Style.CL_POPUP_MENU,
+    dfRt,
+    onRoundTo,
     toggleOption,
     onClose
   } = _ref;
-  return /*#__PURE__*/(0, _jsxRuntime.jsx)(_ModalPopup.default, {
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)(_ModalPopup.default, {
     isShow: isShow,
     style: {
       ..._Style.S_MODAL_POPUP,
@@ -24,7 +39,24 @@ const ModalOptions = _ref => {
     },
     className: className,
     onClose: onClose,
-    children: ROW_CHECKBOX_CONFIGS.map(_ref2 => {
+    children: [onRoundTo && _isRt(dfRt) && /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+      style: S_DIV_INPUT,
+      children: /*#__PURE__*/(0, _jsxRuntime.jsxs)("label", {
+        children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+          style: S_CAPTION,
+          children: "Round Values to"
+        }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_InputText.default, {
+          type: "number",
+          initValue: dfRt,
+          min: MIN_RT,
+          max: MAX_RT,
+          step: 1,
+          maxLength: 2,
+          onChange: onRoundTo,
+          onEnter: onClose
+        })]
+      })
+    }), ROW_CHECKBOX_CONFIGS.map(_ref2 => {
       let [id, caption] = _ref2;
       return /*#__PURE__*/(0, _jsxRuntime.jsx)(_RowCheckBox.default, {
         style: _Style.S_ROW_CHB,
@@ -32,7 +64,7 @@ const ModalOptions = _ref => {
         onToggle: toggleOption,
         id: id
       }, id);
-    })
+    })]
   });
 };
 var _default = ModalOptions;
--- src/components/dialogs/modals/ModalOptions.jsx
+++ src/components/dialogs/modals/ModalOptions.jsx
@@ -1,10 +1,27 @@
 import ModalPopup from '../../zhn-moleculs/ModalPopup';
+import InputText from '../../zhn/InputText';
 import RowCheckBox2 from '../rows/RowCheckBox2';
 import {
   CL_POPUP_MENU,
   S_MODAL_POPUP,
   S_ROW_CHB
 } from './Style';
+
+const S_DIV_INPUT = {
+  margin: '6px 0'
+}
+const S_CAPTION = {
+  paddingRight: 4,
+  color: 'black',
+  fontSize: '16px',
+  fontWeight: 'bold'
+}
+
+const MIN_RT = 0, MAX_RT = 3;
+const _isRt = (
+  rt
+) => rt === ''
+  || rt>=MIN_RT && rt<=MAX_RT
 
 const ROW_CHECKBOX_CONFIGS = [
   ["isNotZoomToMinMax", "Not Zoom to Min-Max"],
@@ -16,6 +33,8 @@ const ModalOptions = ({
   isShow,
   style,
   className=CL_POPUP_MENU,
+  dfRt,
+  onRoundTo,
   toggleOption,
   onClose
 }) => (
@@ -25,6 +44,24 @@ const ModalOptions = ({
     className={className}
     onClose={onClose}
   >
+    {/*eslint-disable jsx-a11y/label-has-associated-control*/}
+    {onRoundTo && _isRt(dfRt) && <div style={S_DIV_INPUT}>
+        <label>
+          <span style={S_CAPTION}>Round Values to</span>
+          <InputText
+            type="number"
+            initValue={dfRt}
+            min={MIN_RT}
+            max={MAX_RT}
+            step={1}
+            maxLength={2}
+            onChange={onRoundTo}
+            onEnter={onClose}
+          />
+        </label>
+      </div>
+    }
+    {/*eslint-enable jsx-a11y/label-has-associated-control*/}
     {ROW_CHECKBOX_CONFIGS.map(([id, caption]) => (
        <RowCheckBox2
           key={id}
