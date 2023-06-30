48eea0c6f694c7b68647c231919957c3e422eea5
Yevhenii Molodyi <eugmolody@gmail.com>
2023-06-30 13:58:14 +0300

    Edit utils arrFn

--- js/utils/arrFn.js
+++ js/utils/arrFn.js
@@ -2,34 +2,13 @@
 
 exports.__esModule = true;
 exports.isInArrStr = exports.arrFactoryIsSameByProp = exports.arrFactoryFindIndexByProp = void 0;
-
 const _isArr = Array.isArray,
-      _fIsItem = (propName, propValue) => item => item[propName] === propValue,
-      _findArrIndexBy = (arr, propName, propValue) => arr.findIndex(_fIsItem(propName, propValue));
-
-const isInArrStr = arr => str => {
-  if (!_isArr(arr)) {
-    return false;
-  }
-
-  let i;
-
-  for (i = 0; i < arr.length; i++) {
-    if (str === arr[i]) {
-      return true;
-    }
-  }
-
-  return false;
-};
-
+  _fIsItem = (propName, propValue) => item => item[propName] === propValue,
+  _findArrIndexBy = (arr, propName, propValue) => arr.findIndex(_fIsItem(propName, propValue));
+const isInArrStr = arr => str => _isArr(arr) ? arr.indexOf(str) !== -1 : false;
 exports.isInArrStr = isInArrStr;
-
-const arrFactoryIsSameByProp = propName => (arr, propValue) => _isArr(arr) ? _findArrIndexBy(arr, propName, propValue) === -1 ? false : true : false;
-
+const arrFactoryIsSameByProp = propName => (arr, propValue) => _isArr(arr) ? _findArrIndexBy(arr, propName, propValue) !== -1 : false;
 exports.arrFactoryIsSameByProp = arrFactoryIsSameByProp;
-
 const arrFactoryFindIndexByProp = propName => (arr, propValue) => _isArr(arr) ? _findArrIndexBy(arr, propName, propValue) : -1;
-
 exports.arrFactoryFindIndexByProp = arrFactoryFindIndexByProp;
 //# sourceMappingURL=arrFn.js.map
--- src/utils/arrFn.js
+++ src/utils/arrFn.js
@@ -4,24 +4,20 @@ const _isArr = Array.isArray
 , _findArrIndexBy = (arr, propName, propValue) =>
     arr.findIndex(_fIsItem(propName, propValue));
 
-export const isInArrStr = (arr) => (str) => {
-  if (!_isArr(arr)) {
-    return false;
-  }
-  let i;
-  for(i=0;i<arr.length;i++){
-    if (str === arr[i]){
-      return true;
-    }
-  }
-  return false;
-}
+export const isInArrStr = (
+  arr
+) => (str) => _isArr(arr)
+  ? arr.indexOf(str) !== -1
+  : false;
 
-export const arrFactoryIsSameByProp = (propName) => (arr, propValue) => _isArr(arr)
-  ? _findArrIndexBy(arr, propName, propValue) === -1
-      ? false : true
+export const arrFactoryIsSameByProp = (
+  propName
+) => (arr, propValue) => _isArr(arr)
+  ? _findArrIndexBy(arr, propName, propValue) !== -1
   : false
 
-export const arrFactoryFindIndexByProp = (propName) => (arr, propValue) => _isArr(arr)
+export const arrFactoryFindIndexByProp = (
+  propName
+) => (arr, propValue) => _isArr(arr)
   ? _findArrIndexBy(arr, propName, propValue)
   : -1
