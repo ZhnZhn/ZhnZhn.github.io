4d67967d147029e0b307241f4cf87bf097294766
Yevhenii Molodyi <eugmolody@gmail.com>
2023-07-17 11:04:01 +0300

    Add factory crAdapterRouter to IrenaAdapter

--- js/adapters/irena/IrenaAdapter.js
+++ js/adapters/irena/IrenaAdapter.js
@@ -1,41 +1,20 @@
 "use strict";
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
 exports.__esModule = true;
 exports.default = void 0;
-
 var _CategoryFn = require("../CategoryFn");
-
 var _crAdapterType = _interopRequireDefault(require("../crAdapterType1"));
-
 var _crFromYearData = _interopRequireDefault(require("../crFromYearData"));
-
+var _crAdapterRouter = _interopRequireDefault(require("../crAdapterRouter"));
 var _toCategoryAdapter = _interopRequireDefault(require("./toCategoryAdapter"));
-
 const toLineAdapter = (0, _crAdapterType.default)({
-  crData: _crFromYearData.default
-});
-
-const _getAdapter = option => {
-  const {
-    seriaType
-  } = option;
-  return (0, _CategoryFn.isCategory)(seriaType) ? _toCategoryAdapter.default : toLineAdapter;
-};
-
-const IrenaAdapter = {
-  toConfig: (json, option) => {
-    const _adapter = _getAdapter(option);
-
-    return _adapter.toConfig(json, option);
-  },
-  toSeries: (json, option, chart) => {
-    const _adapter = _getAdapter(option);
-
-    return _adapter.toSeries(json, option, chart);
-  }
-};
+    crData: _crFromYearData.default
+  }),
+  getRoute = option => (0, _CategoryFn.isCategory)(option.seriaType) ? _toCategoryAdapter.default : toLineAdapter,
+  IrenaAdapter = (0, _crAdapterRouter.default)(void 0, {
+    getRoute
+  });
 var _default = IrenaAdapter;
 exports.default = _default;
 //# sourceMappingURL=IrenaAdapter.js.map
--- src/adapters/irena/IrenaAdapter.js
+++ src/adapters/irena/IrenaAdapter.js
@@ -1,30 +1,17 @@
 import { isCategory } from '../CategoryFn';
 import crAdapterType1 from '../crAdapterType1';
 import crFromYearData from '../crFromYearData';
+import crAdapterRouter from '../crAdapterRouter';
 import toCategoryAdapter from './toCategoryAdapter';
 
 const toLineAdapter = crAdapterType1({
   crData: crFromYearData
-});
-
-const _getAdapter = (option) => {
-  const { seriaType } = option;
-  return isCategory(seriaType)
-    ? toCategoryAdapter
-    : toLineAdapter;
-}
-
-const IrenaAdapter = {
-  toConfig: (json, option) => {
-    const _adapter = _getAdapter(option);
-    return _adapter.toConfig(json, option);
-  },
-
-  toSeries: (json, option, chart) => {
-    const _adapter = _getAdapter(option);
-    return _adapter.toSeries(json, option, chart);
-  }
-}
-
+})
+, getRoute = (
+  option
+) => isCategory(option.seriaType)
+  ? toCategoryAdapter
+  : toLineAdapter
+, IrenaAdapter = crAdapterRouter(void 0, { getRoute })
 
 export default IrenaAdapter
