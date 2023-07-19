42df20cf9d3edc8de427ba6771dc12bc6e76ed1e
Yevhenii Molodyi <eugmolody@gmail.com>
2023-07-19 11:58:10 +0300

    Add factory crAdapterCategory to IRENA toCategoryAdapter

--- src/adapters/irena/toCategoryAdapter.js
+++ src/adapters/irena/toCategoryAdapter.js
@@ -1,6 +1,4 @@
-import { isCategoryCluster } from '../CategoryFn';
-import crCategoryConfig from '../crCategoryConfig';
-import fToCategorySeries from '../fToCategorySeries';
+import crAdapterCategory from '../crAdapterCategory';
 
 const _crData = (
   json
@@ -9,42 +7,6 @@ const _crData = (
   name: arrP[0],
   c: arrP[0]
 }))
-
-const toCategoryAdapter = {
-  toConfig: (json, option) => {
-    const {
-      title,
-      subtitle,
-      seriaType,
-      seriaColor,
-      _itemKey,
-      time,
-      dataSource
-    } = option
-    , data = _crData(json)
-    , _arrSeriaType = seriaType.split('_')
-    , config = crCategoryConfig(
-        subtitle,
-        title,
-        _arrSeriaType[0],
-        seriaColor,
-        data,
-        isCategoryCluster(seriaType)
-    );
-
-    config.zhConfig = {
-      id: _itemKey,
-      key: _itemKey,
-      itemCaption: `${subtitle}: ${title}`,
-      itemTime: time,
-      dataSource
-    }
-    return { config };
-  }
-}
-
-toCategoryAdapter.toSeries = fToCategorySeries(
-  toCategoryAdapter.toConfig
-)
+, toCategoryAdapter = crAdapterCategory(_crData)
 
 export default toCategoryAdapter
