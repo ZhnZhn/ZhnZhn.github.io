7a4c3a4a9ef84263cd572d1ec1c576209b8ceb56
Yevhenii Molodyi <eugmolody@gmail.com>
2023-08-17 18:50:32 +0300

    Add function factory _fCrProperty to EmberApi

--- js/adapters/ember/EmberApi.js
+++ js/adapters/ember/EmberApi.js
@@ -7,28 +7,33 @@ const API_URL = 'https://ember-data-api-scg3n.ondigitalocean.app/ember',
   YEARLY_JSON = 'generation_yearly.json',
   MONTHLY_JSON = 'generation_monthly.json',
   QUERY_TAIL = '&_shape=array';
+const _fCrProperty = suffix => (name, value) => name + "__" + suffix + "=" + value,
+  _crExactProperty = _fCrProperty('exact'),
+  _crGteProperty = _fCrProperty('gte');
 
 // geo, _sourceQuery
 const _crQueryParams = items => {
   const source = (0, _fnAdapter.getValue)(items[2]);
-  return [(0, _fnAdapter.getCaption)(items[0]), (0, _fnAdapter.isTotalData)(source) ? '' : "&variable__exact=" + source];
+  return [(0, _fnAdapter.getCaption)(items[0]), (0, _fnAdapter.isTotalData)(source) ? '' : "&" + _crExactProperty('variable', source)];
 };
 const _crUrl = (pathToken, options) => {
   const {
       items
     } = options,
     [geo, sourceQuery] = _crQueryParams(items);
-  return API_URL + "/" + pathToken + "?country_or_region__exact=" + geo + QUERY_TAIL + sourceQuery;
+  return API_URL + "/" + pathToken + "?" + _crExactProperty('country_or_region', geo) + QUERY_TAIL + sourceQuery;
 };
+const DATE = 'date',
+  YEAR = 'year';
 const EmberApi = {
   getRequestUrl(options) {
     const _isMonthlyRoute = options.dfRId === 'M';
-    options.pnDate = _isMonthlyRoute ? 'date' : 'year';
+    options.pnDate = _isMonthlyRoute ? DATE : YEAR;
     if ((0, _fnAdapter.isCategory)(options.seriaType)) {
       const _sourceQuery = _crQueryParams(options.items)[1];
-      return API_URL + "/" + YEARLY_JSON + "?year__exact=" + options.time + QUERY_TAIL + _sourceQuery;
+      return API_URL + "/" + YEARLY_JSON + "?" + _crExactProperty(YEAR, options.time) + QUERY_TAIL + _sourceQuery;
     }
-    return _isMonthlyRoute ? _crUrl(MONTHLY_JSON, options) + "&date__gte=" + options.fromDate : _crUrl(YEARLY_JSON, options);
+    return _isMonthlyRoute ? _crUrl(MONTHLY_JSON, options) + "&" + _crGteProperty(DATE, options.fromDate) : _crUrl(YEARLY_JSON, options);
   },
   checkResponse(json) {
     if (!(0, _fnAdapter.isArr)(json)) {
--- src/adapters/ember/EmberApi.js
+++ src/adapters/ember/EmberApi.js
@@ -12,6 +12,13 @@ const API_URL = 'https://ember-data-api-scg3n.ondigitalocean.app/ember'
 , MONTHLY_JSON = 'generation_monthly.json'
 , QUERY_TAIL = '&_shape=array';
 
+const _fCrProperty = (suffix) => (
+  name,
+  value
+) => `${name}__${suffix}=${value}`
+, _crExactProperty = _fCrProperty('exact')
+, _crGteProperty = _fCrProperty('gte');
+
 // geo, _sourceQuery
 const _crQueryParams = (
   items
@@ -21,7 +28,7 @@ const _crQueryParams = (
     getCaption(items[0]),
     isTotalData(source)
       ? ''
-      : `&variable__exact=${source}`
+      : `&${_crExactProperty('variable', source)}`
   ];
 }
 
@@ -37,24 +44,27 @@ const _crUrl = (
    sourceQuery
  ] = _crQueryParams(items);
 
-  return `${API_URL}/${pathToken}?country_or_region__exact=${geo}${QUERY_TAIL}${sourceQuery}`;
+  return `${API_URL}/${pathToken}?${_crExactProperty('country_or_region', geo)}${QUERY_TAIL}${sourceQuery}`;
 }
+
+const DATE = 'date'
+, YEAR = 'year';
 
 const EmberApi = {
   getRequestUrl(options) {
     const _isMonthlyRoute = options.dfRId === 'M';
 
     options.pnDate = _isMonthlyRoute
-      ? 'date'
-      : 'year';
+      ? DATE
+      : YEAR;
 
     if (isCategory(options.seriaType)) {
       const _sourceQuery = _crQueryParams(options.items)[1];
-      return `${API_URL}/${YEARLY_JSON}?year__exact=${options.time}${QUERY_TAIL}${_sourceQuery}`;
+      return `${API_URL}/${YEARLY_JSON}?${_crExactProperty(YEAR, options.time)}${QUERY_TAIL}${_sourceQuery}`;
     }
 
     return _isMonthlyRoute
-      ? `${_crUrl(MONTHLY_JSON, options)}&date__gte=${options.fromDate}`
+      ? `${_crUrl(MONTHLY_JSON, options)}&${_crGteProperty(DATE, options.fromDate)}`
       : _crUrl(YEARLY_JSON, options);
   },
 
