b502a72471db8541cb85fd72c8e7f651b51c92c9
Yevhenii Molodyi <eugmolody@gmail.com>
2023-09-21 14:43:53 +0300

    Add colors and css custom property names to uiTheme

--- js/components/styles/uiTheme.js
+++ js/components/styles/uiTheme.js
@@ -3,66 +3,75 @@
 exports.__esModule = true;
 exports.setUiTheme = void 0;
 var _ChartUiTheme = require("../../charts/ChartUiTheme");
-const EL_BG_GREY = '#1b2836';
-const DF_T_C = EL_BG_GREY;
-const DF_S_C1 = '#8bc34a';
+const COLOR_SILVER = "silver";
+const COLOR_DARK_BLUE = "#1b2836";
+const DF_B_BG = "#5f5f5f";
+const DF_T_C = COLOR_DARK_BLUE;
+const DF_S_C1 = "#8bc34a";
 const DF_S_C2 = DF_T_C;
-const DF_C_BG = '#4d4d4d';
-const DF_BH_C = '#c0c0c0';
-const DF_LB_BC = '#1b2836';
-const DF_EL_BG = EL_BG_GREY;
-const DF_EL_C = 'silver';
+const DF_C_BG = "#4d4d4d";
+const DF_BH_C = "#c0c0c0";
+const DF_LB_BC = COLOR_DARK_BLUE;
+const DF_EL_BG = COLOR_DARK_BLUE;
+const DF_EL_C = COLOR_SILVER;
+const PROPERTY_B_BG = "b-bg",
+  PROPERTY_T_C = "t-c",
+  PROPERTY_S_C1 = "s-c1",
+  PROPERTY_S_C2 = "s-c2",
+  PROPERTY_C_BG = "c-bg",
+  PROPERTY_BH_C = "bh-c",
+  PROPERTY_LB_BC = "lb-bc",
+  PROPERTY_EL_BG = "el-bg",
+  PROPERTY_EL_C = "el-c";
 const P_GREY = {
-  BG_BODY: '#5f5f5f',
-  't-c': EL_BG_GREY,
-  's-c1': DF_S_C1,
-  's-c2': DF_S_C2,
-  'c-bg': DF_C_BG,
-  'bh-c': DF_BH_C,
-  'lb-bc': DF_LB_BC,
-  'el-bg': DF_EL_BG,
-  'el-c': DF_EL_C
+  [PROPERTY_B_BG]: DF_B_BG,
+  [PROPERTY_T_C]: COLOR_DARK_BLUE,
+  [PROPERTY_S_C1]: DF_S_C1,
+  [PROPERTY_S_C2]: DF_S_C2,
+  [PROPERTY_C_BG]: DF_C_BG,
+  [PROPERTY_BH_C]: DF_BH_C,
+  [PROPERTY_LB_BC]: DF_LB_BC,
+  [PROPERTY_EL_BG]: DF_EL_BG,
+  [PROPERTY_EL_C]: DF_EL_C
 };
-const EL_BG_WHITE = '#bcd8f5';
-const LB_BC_LIGHT = 'grey';
+const EL_BG_WHITE = "#bcd8f5";
+const LB_BC_LIGHT = "grey";
 const P_WHITE = {
-  BG_BODY: '#e1e1e1',
-  't-c': '#1b75bb',
-  's-c1': DF_S_C1,
-  's-c2': EL_BG_WHITE,
-  'c-bg': '#ebf1f5',
-  'bh-c': DF_BH_C,
-  'lb-bc': LB_BC_LIGHT,
-  'el-bg': EL_BG_WHITE,
-  'el-c': '#212020'
+  [PROPERTY_B_BG]: "#e1e1e1",
+  [PROPERTY_T_C]: "#1b75bb",
+  [PROPERTY_S_C1]: DF_S_C1,
+  [PROPERTY_S_C2]: EL_BG_WHITE,
+  [PROPERTY_C_BG]: "#ebf1f5",
+  [PROPERTY_BH_C]: DF_BH_C,
+  [PROPERTY_LB_BC]: LB_BC_LIGHT,
+  [PROPERTY_EL_BG]: EL_BG_WHITE,
+  [PROPERTY_EL_C]: "#212020"
 };
-const EL_BG_SAND_L = '#64473d';
+const EL_BG_SAND_L = "#64473d";
 const SAND_L_P = {
-  BG_BODY: '#9e9e9e',
-  't-c': '#785133',
-  's-c1': DF_S_C1,
-  's-c2': EL_BG_SAND_L,
-  'c-bg': '#e8e0cb',
-  'bh-c': '#5b5b5b',
-  'lb-bc': LB_BC_LIGHT,
-  'el-bg': EL_BG_SAND_L,
-  'el-c': 'silver'
+  [PROPERTY_B_BG]: "#9e9e9e",
+  [PROPERTY_T_C]: "#785133",
+  [PROPERTY_S_C1]: DF_S_C1,
+  [PROPERTY_S_C2]: EL_BG_SAND_L,
+  [PROPERTY_C_BG]: "#e8e0cb",
+  [PROPERTY_BH_C]: "#5b5b5b",
+  [PROPERTY_LB_BC]: LB_BC_LIGHT,
+  [PROPERTY_EL_BG]: EL_BG_SAND_L,
+  [PROPERTY_EL_C]: COLOR_SILVER
 };
 const P_SAND_L = {
   ...SAND_L_P
 };
-const EL_BG_SAND = '#463222';
+const EL_BG_SAND = "#463222";
 const P_SAND = {
   ...SAND_L_P,
-  BG: '#e6d5a9',
-  'c-bg': '#e6d5a9',
-  's-c2': EL_BG_SAND,
-  'el-bg': EL_BG_SAND
+  [PROPERTY_S_C2]: EL_BG_SAND,
+  [PROPERTY_C_BG]: "#e6d5a9",
+  [PROPERTY_EL_BG]: EL_BG_SAND
 };
-const CUSTOM_CSS_PROPERTY_CONFIGS = [["t-c", DF_T_C], ["s-c1", DF_S_C1], ["s-c2", DF_S_C2], ["c-bg", DF_C_BG], ["bh-c", DF_BH_C], ["lb-bc", DF_LB_BC], ["el-bg", DF_EL_BG], ["el-c", DF_EL_C]];
+const CUSTOM_CSS_PROPERTY_CONFIGS = [[PROPERTY_B_BG, DF_B_BG], [PROPERTY_T_C, DF_T_C], [PROPERTY_S_C1, DF_S_C1], [PROPERTY_S_C2, DF_S_C2], [PROPERTY_C_BG, DF_C_BG], [PROPERTY_BH_C, DF_BH_C], [PROPERTY_LB_BC, DF_LB_BC], [PROPERTY_EL_BG, DF_EL_BG], [PROPERTY_EL_C, DF_EL_C]];
 const _setCssPropertiesFrom = P => {
   const _style = document.body.style;
-  _style.backgroundColor = P.BG_BODY;
   CUSTOM_CSS_PROPERTY_CONFIGS.forEach(_ref => {
     let [propName, dfValue] = _ref;
     _style.setProperty('--' + propName, P[propName] || dfValue);
@@ -74,7 +83,7 @@ const HP_THEME = {
   SAND: P_SAND,
   SAND_L: P_SAND_L
 };
-const DF_THEME_ID = 'GREY';
+const DF_THEME_ID = "GREY";
 const _crThemeConfig = themeName => HP_THEME[themeName] || HP_THEME[DF_THEME_ID];
 const _setTheme = themeName => {
   _setCssPropertiesFrom(_crThemeConfig(themeName));
