d6fd9426153655cffe7d373157ca96efa3d3dd64
Yevhenii Molodyi <eugmolody@gmail.com>
2023-11-02 12:59:23 +0200

    Add function getColorBlack from styleFn to OptionCheckBoxStack function component

--- js/components/header/OptionCheckBoxStack.js
+++ js/components/header/OptionCheckBoxStack.js
@@ -4,6 +4,7 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 exports.__esModule = true;
 exports.default = void 0;
 var _uiApi = require("../uiApi");
+var _styleFn = require("../styleFn");
 var _getFnByPropName = _interopRequireDefault(require("../../utils/getFnByPropName"));
 var _RowCheckBox = _interopRequireDefault(require("../dialogs/rows/RowCheckBox1"));
 var _jsxRuntime = require("react/jsx-runtime");
@@ -12,17 +13,18 @@ const OptionCheckBoxStack = _ref => {
     data,
     configs
   } = _ref;
-  const _hMode = (fnName, mode) => (0, _getFnByPropName.default)(data, fnName)(mode);
+  const _hMode = (fnName, mode) => (0, _getFnByPropName.default)(data, fnName)(mode),
+    _colorBlack = (0, _styleFn.getColorBlack)();
   return configs.map(_ref2 => {
     let [caption, id] = _ref2;
     return /*#__PURE__*/(0, _jsxRuntime.jsx)(_RowCheckBox.default, {
       caption: caption,
       initialValue: (0, _getFnByPropName.default)(data, id, false)(),
+      color: _colorBlack,
       onCheck: (0, _uiApi.bindTo)(_hMode, id, true),
       onUnCheck: (0, _uiApi.bindTo)(_hMode, id, false)
     }, caption);
   });
 };
-var _default = OptionCheckBoxStack;
-exports.default = _default;
+var _default = exports.default = OptionCheckBoxStack;
 //# sourceMappingURL=OptionCheckBoxStack.js.map
--- src/components/header/OptionCheckBoxStack.jsx
+++ src/components/header/OptionCheckBoxStack.jsx
@@ -1,4 +1,5 @@
 import { bindTo } from '../uiApi';
+import { getColorBlack } from '../styleFn';
 
 import getFnByPropName from '../../utils/getFnByPropName';
 import RowCheckBox1 from '../dialogs/rows/RowCheckBox1';
@@ -10,12 +11,14 @@ const OptionCheckBoxStack = ({
  const _hMode = (
    fnName,
    mode
- ) => getFnByPropName(data, fnName)(mode);
+ ) => getFnByPropName(data, fnName)(mode)
+ , _colorBlack = getColorBlack();
  return configs.map(([caption, id]) => (
     <RowCheckBox1
       key={caption}
       caption={caption}
       initialValue={getFnByPropName(data, id, false)()}
+      color={_colorBlack}
       onCheck={bindTo(_hMode, id, true)}
       onUnCheck={bindTo(_hMode, id, false)}
     />
