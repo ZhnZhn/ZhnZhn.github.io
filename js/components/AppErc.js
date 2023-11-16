02073b80d9af97b574c18c04ab85ae26457d16e1
Yevhenii Molodyi <eugmolody@gmail.com>
2023-11-16 11:31:25 +0200

    Improve details

--- js/components/AppErc.js
+++ js/components/AppErc.js
@@ -17,7 +17,7 @@ var _CompContainer = _interopRequireDefault(require("./zhn-containers/CompContai
 var _DialogContainer = _interopRequireDefault(require("./dialogs/DialogContainer"));
 var _checkBuild = _interopRequireDefault(require("./checkBuild"));
 var _jsxRuntime = require("react/jsx-runtime");
-const BUILD_DATE = '09-11-2023',
+const BUILD_DATE = '16-11-2023',
   CL_COMP_CONTAINER = "component-container";
 const showSettings = (0, _uiApi.bindTo)(_ComponentActions.ComponentActions.showSettings, _ChartStore.default.exportSettingFn());
 const AppErc = () => {
