91ac2a7c1144403186f675881641e799e1fb62f6
Yevhenii Molodyi <eugmolody@gmail.com>
2023-11-20 17:20:20 +0200

    Replace if-else statement by tenary operators in ToggleButton function component

--- js/components/zhn-search/ToggleButton.js
+++ js/components/zhn-search/ToggleButton.js
@@ -1,51 +1,35 @@
 "use strict";
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
 exports.__esModule = true;
 exports.default = void 0;
-
 var _ArrowCell = _interopRequireDefault(require("./ArrowCell"));
-
 var _jsxRuntime = require("react/jsx-runtime");
-
 const CL_SPINNER = 'zhn-search__spinner',
-      CL_SPINNER_FAILED = 'zhn-select__spinner--failed',
-      S_ARROW_SHOW = {
-  borderColor: '#1b75bb transparent transparent'
+  CL_SPINNER_FAILED = 'zhn-select__spinner--failed',
+  S_ARROW_SHOW = {
+    borderColor: '#1b75bb transparent transparent'
+  },
+  _LOADING_ELEMENT = /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+    className: CL_SPINNER,
+    "data-loader": "circle"
+  }),
+  _LOADING_FAILED_ELEMENT = /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+    className: CL_SPINNER_FAILED,
+    "data-loader": "circle-failed"
+  });
+const ToggleButton = _ref => {
+  let {
+    isLoading,
+    isLoadingFailed,
+    options,
+    isOptions,
+    toggleOptions
+  } = _ref;
+  return isLoading ? _LOADING_ELEMENT : isLoadingFailed ? _LOADING_FAILED_ELEMENT : options && options.length > 0 ? /*#__PURE__*/(0, _jsxRuntime.jsx)(_ArrowCell.default, {
+    arrowStyle: isOptions ? S_ARROW_SHOW : null,
+    onClick: toggleOptions
+  }) : null;
 };
-
-const _loadingEl = /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-  className: CL_SPINNER,
-  "data-loader": "circle"
-});
-
-const _loadingFailedEl = /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-  className: CL_SPINNER_FAILED,
-  "data-loader": "circle-failed"
-});
-
-const ToggleButton = ({
-  isLoading,
-  isLoadingFailed,
-  options,
-  isOptions,
-  toggleOptions
-}) => {
-  if (isLoading) {
-    return _loadingEl;
-  } else if (isLoadingFailed) {
-    return _loadingFailedEl;
-  } else if (options && options.length > 0) {
-    return /*#__PURE__*/(0, _jsxRuntime.jsx)(_ArrowCell.default, {
-      arrowStyle: isOptions ? S_ARROW_SHOW : null,
-      onClick: toggleOptions
-    });
-  }
-
-  return null;
-};
-
-var _default = ToggleButton;
-exports.default = _default;
+var _default = exports.default = ToggleButton;
 //# sourceMappingURL=ToggleButton.js.map
--- src/components/zhn-search/ToggleButton.jsx
+++ src/components/zhn-search/ToggleButton.jsx
@@ -4,34 +4,31 @@ const CL_SPINNER = 'zhn-search__spinner'
 , CL_SPINNER_FAILED = 'zhn-select__spinner--failed'
 , S_ARROW_SHOW = {
   borderColor: '#1b75bb transparent transparent'
-};
-
-const _loadingEl = (<span
+}
+, _LOADING_ELEMENT = (<span
   className={CL_SPINNER}
   data-loader="circle"
-/>);
-const _loadingFailedEl = (<span
+/>)
+, _LOADING_FAILED_ELEMENT = (<span
   className={CL_SPINNER_FAILED}
   data-loader="circle-failed"
 />);
 
 const ToggleButton = ({
-  isLoading, isLoadingFailed,
-  options, isOptions, toggleOptions
-}) => {
-  if (isLoading) {
-    return _loadingEl;
-  } else if (isLoadingFailed) {
-    return _loadingFailedEl;
-  } else if (options && options.length > 0) {
-    return (
-      <ArrowCell
-        arrowStyle={isOptions ? S_ARROW_SHOW : null}
-        onClick={toggleOptions}
-      />
-    );
-   }
-   return null;
-};
+  isLoading,
+  isLoadingFailed,
+  options,
+  isOptions,
+  toggleOptions
+}) => isLoading
+  ? _LOADING_ELEMENT
+  : isLoadingFailed
+    ? _LOADING_FAILED_ELEMENT
+    : options && options.length > 0
+      ? (<ArrowCell
+            arrowStyle={isOptions ? S_ARROW_SHOW : null}
+            onClick={toggleOptions}
+         />)
+      : null;
 
 export default ToggleButton
