bda28337f7288cd3862790f92614b5b477c44200
Yevhenii Molodyi <eugmolody@gmail.com>
2024-01-17 11:27:45 +0200

    Edit RouterAdapter

--- js/flux/logic/LoadConfig.js
+++ js/flux/logic/LoadConfig.js
@@ -7,46 +7,8 @@ var _LoadType = require("../../constants/LoadType");
 var _loadNdlCommodityTrade = require("./loadNdlCommodityTrade");
 var _LoadImpl = _interopRequireDefault(require("./LoadImpl"));
 const LoadConfig = {
-  [_LoadType.LT_Q]: _LoadImpl.default.Ndl,
-  [_LoadType.LT_QCT]: _loadNdlCommodityTrade.loadNdlCommodityTrade,
-  [_LoadType.LT_DBN]: _LoadImpl.default.DbNomics,
-  [_LoadType.LT_EI]: _LoadImpl.default.Ei,
-  [_LoadType.LT_EMB]: _LoadImpl.default.Ember,
-  [_LoadType.LT_IRE]: _LoadImpl.default.Irena,
-  [_LoadType.LT_AL]: _LoadImpl.default.AlphaVantage,
-  [_LoadType.LT_IEX]: _LoadImpl.default.Iex,
-  [_LoadType.LT_FMP]: _LoadImpl.default.Fmp,
-  [_LoadType.LT_TW]: _LoadImpl.default.Tw,
-  [_LoadType.LT_EU_STAT]: _LoadImpl.default.EuroStat,
-  [_LoadType.LT_FS]: _LoadImpl.default.Insee,
-  [_LoadType.LT_UKS]: _LoadImpl.default.StatUk,
-  [_LoadType.LT_NST]: _LoadImpl.default.StatNorway,
-  [_LoadType.LT_NST_2]: _LoadImpl.default.StatNorway2,
-  [_LoadType.LT_SWS]: _LoadImpl.default.StatSweden,
-  [_LoadType.LT_SFL]: _LoadImpl.default.StatFinland,
-  [_LoadType.LT_SDN]: _LoadImpl.default.StatDenmark,
-  [_LoadType.LT_SIR]: _LoadImpl.default.StatIreland,
-  [_LoadType.LT_BEA]: _LoadImpl.default.Bea,
-  [_LoadType.LT_BLS]: _LoadImpl.default.Bls,
-  [_LoadType.LT_EIA]: _LoadImpl.default.Eia,
-  [_LoadType.LT_INTR]: _LoadImpl.default.Intrinio,
-  [_LoadType.LT_CRC]: _LoadImpl.default.Crc,
-  [_LoadType.LT_CG]: _LoadImpl.default.Cg,
-  [_LoadType.LT_CM]: _LoadImpl.default.Cm,
-  [_LoadType.LT_CP]: _LoadImpl.default.Cp,
-  [_LoadType.LT_CL]: _LoadImpl.default.Cl,
-  [_LoadType.LT_BN]: _LoadImpl.default.Bn,
-  [_LoadType.LT_CB]: _LoadImpl.default.Cb,
-  [_LoadType.LT_BT]: _LoadImpl.default.Bt,
-  [_LoadType.LT_BF]: _LoadImpl.default.Bf,
-  [_LoadType.LT_KR]: _LoadImpl.default.Kr,
-  [_LoadType.LT_KC]: _LoadImpl.default.Kc,
-  [_LoadType.LT_GT]: _LoadImpl.default.Gt,
-  [_LoadType.LT_UN]: _LoadImpl.default.UnComtrade,
-  [_LoadType.LT_FAO]: _LoadImpl.default.FaoStat,
-  [_LoadType.LT_WB]: _LoadImpl.default.WorldBank,
-  [_LoadType.LT_WATCH_LIST]: _LoadImpl.default.Ndl,
-  [_LoadType.LT_WL]: _LoadImpl.default.Ndl
+  ..._LoadImpl.default,
+  [_LoadType.LT_QCT]: _loadNdlCommodityTrade.loadNdlCommodityTrade
 };
 var _default = exports.default = LoadConfig;
 //# sourceMappingURL=LoadConfig.js.map
--- src/flux/logic/LoadConfig.js
+++ src/flux/logic/LoadConfig.js
@@ -1,97 +1,10 @@
-import {
-  LT_Q,
-  LT_QCT,
-  LT_DBN,
-  LT_EI,
-  LT_EMB,
-  LT_IRE,
-  LT_AL,
-  LT_IEX,
-  LT_FMP,
-  LT_TW,
-  LT_EU_STAT,
-  LT_FS,
-  LT_UKS,
-  LT_NST,
-  LT_NST_2,
-  LT_SWS,
-  LT_SFL,
-  LT_SDN,
-  LT_SIR,
-  LT_BEA,
-  LT_BLS,
-  LT_EIA,
-  LT_INTR,
-  LT_CRC,
-  LT_CG,
-  LT_CM,
-  LT_CP,
-  LT_CL,
-  LT_BN,
-  LT_CB,
-  LT_BT,
-  LT_BF,
-  LT_KR,
-  LT_KC,
-  LT_GT,
-  LT_UN,
-  LT_FAO,
-  LT_WB,
-  LT_WATCH_LIST,
-  LT_WL
-} from '../../constants/LoadType';
+import { LT_QCT } from '../../constants/LoadType';
 import { loadNdlCommodityTrade } from './loadNdlCommodityTrade';
 import LoadImpl from './LoadImpl';
 
 const LoadConfig = {
-  [LT_Q]: LoadImpl.Ndl,
-  [LT_QCT]: loadNdlCommodityTrade,
-
-
-  [LT_DBN]: LoadImpl.DbNomics,
-  [LT_EI]: LoadImpl.Ei,
-  [LT_EMB]: LoadImpl.Ember,
-  [LT_IRE]: LoadImpl.Irena,
-
-  [LT_AL]: LoadImpl.AlphaVantage,
-  [LT_IEX]: LoadImpl.Iex,
-  [LT_FMP]: LoadImpl.Fmp,
-  [LT_TW]: LoadImpl.Tw,
-
-  [LT_EU_STAT]: LoadImpl.EuroStat,
-  [LT_FS]: LoadImpl.Insee,
-  [LT_UKS]: LoadImpl.StatUk,
-  [LT_NST]: LoadImpl.StatNorway,
-  [LT_NST_2]: LoadImpl.StatNorway2,
-  [LT_SWS]: LoadImpl.StatSweden,
-  [LT_SFL]: LoadImpl.StatFinland,
-  [LT_SDN]: LoadImpl.StatDenmark,
-  [LT_SIR]: LoadImpl.StatIreland,
-
-  [LT_BEA]: LoadImpl.Bea,
-  [LT_BLS]: LoadImpl.Bls,
-  [LT_EIA]: LoadImpl.Eia,
-  [LT_INTR]: LoadImpl.Intrinio,
-
-  [LT_CRC]: LoadImpl.Crc,
-  [LT_CG]: LoadImpl.Cg,
-  [LT_CM]: LoadImpl.Cm,
-  [LT_CP]: LoadImpl.Cp,
-  [LT_CL]: LoadImpl.Cl,
-  [LT_BN]: LoadImpl.Bn,
-  [LT_CB]: LoadImpl.Cb,
-  [LT_BT]: LoadImpl.Bt,
-  [LT_BF]: LoadImpl.Bf,
-  [LT_KR]: LoadImpl.Kr,
-  [LT_KC]: LoadImpl.Kc,
-  [LT_GT]: LoadImpl.Gt,
-
-  [LT_UN]: LoadImpl.UnComtrade,
-  [LT_FAO]: LoadImpl.FaoStat,
-  [LT_WB]: LoadImpl.WorldBank,
-
-  [LT_WATCH_LIST]: LoadImpl.Ndl,
-  [LT_WL]: LoadImpl.Ndl
+  ...LoadImpl,
+  [LT_QCT]: loadNdlCommodityTrade  
 };
 
 export default LoadConfig
