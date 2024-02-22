65bf9ce108d6b8f9621b4ad1e654be2428b57e6e
Yevhenii Molodyi <eugmolody@gmail.com>
2024-02-22 18:11:49 +0200

    Add CONFIG_PROP_NAMES to AlphaPerfItem function component

--- js/components/items/AlphaPerfItem.js
+++ js/components/items/AlphaPerfItem.js
@@ -5,32 +5,21 @@ exports.AlphaPerfItem = void 0;
 var _TableItem = require("./TableItem");
 var _jsxRuntime = require("react/jsx-runtime");
 const S_TH_MORE = {
-  paddingLeft: 12,
-  textAlign: 'left'
-};
+    paddingLeft: 12,
+    textAlign: 'left'
+  },
+  CONFIG_PROP_NAMES = ['m', 'g', 'l'];
 const AlphaPerfItem = _ref => {
   let {
     config,
     onCloseItem
   } = _ref;
-  return /*#__PURE__*/(0, _jsxRuntime.jsxs)(_jsxRuntime.Fragment, {
-    children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_TableItem.TableItem, {
-      isInitialClose: true,
-      thMoreStyle: S_TH_MORE,
-      config: config.m,
-      onCloseItem: onCloseItem
-    }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_TableItem.TableItem, {
-      isInitialClose: true,
-      thMoreStyle: S_TH_MORE,
-      config: config.g,
-      onCloseItem: onCloseItem
-    }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_TableItem.TableItem, {
-      isInitialClose: true,
-      thMoreStyle: S_TH_MORE,
-      config: config.l,
-      onCloseItem: onCloseItem
-    })]
-  });
+  return CONFIG_PROP_NAMES.map(pn => /*#__PURE__*/(0, _jsxRuntime.jsx)(_TableItem.TableItem, {
+    isInitialClose: true,
+    thMoreStyle: S_TH_MORE,
+    config: config[pn],
+    onCloseItem: onCloseItem
+  }, pn));
 };
 exports.AlphaPerfItem = AlphaPerfItem;
 //# sourceMappingURL=AlphaPerfItem.js.map
--- src/components/items/AlphaPerfItem.jsx
+++ src/components/items/AlphaPerfItem.jsx
@@ -3,30 +3,19 @@ import { TableItem } from './TableItem';
 const S_TH_MORE = {
   paddingLeft: 12,
   textAlign: 'left'
-};
+}
+, CONFIG_PROP_NAMES = ['m', 'g', 'l'];
 
 export const AlphaPerfItem = ({
   config,
   onCloseItem
-}) => (
-  <>
-    <TableItem
-      isInitialClose={true}
-      thMoreStyle={S_TH_MORE}
-      config={config.m}
-      onCloseItem={onCloseItem}
-    />
-    <TableItem
-      isInitialClose={true}
-      thMoreStyle={S_TH_MORE}
-      config={config.g}
-      onCloseItem={onCloseItem}
-    />
-    <TableItem
-      isInitialClose={true}
-      thMoreStyle={S_TH_MORE}
-      config={config.l}
-      onCloseItem={onCloseItem}
-    />
-  </>
-)
+}) => CONFIG_PROP_NAMES.map(pn => (
+  <TableItem
+    key={pn}
+    isInitialClose={true}
+    thMoreStyle={S_TH_MORE}
+    config={config[pn]}
+    onCloseItem={onCloseItem}
+  />
+))
+  
