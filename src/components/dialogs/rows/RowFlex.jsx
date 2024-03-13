5ad904485253c1f1eaaea233597bc18ee2e61c35
Yevhenii Molodyi <eugmolody@gmail.com>
2024-03-13 12:30:41 +0200

    Add RowFlexEnd named export to RowFlex

--- src/components/dialogs/rows/RowFlex.jsx
+++ src/components/dialogs/rows/RowFlex.jsx
@@ -14,3 +14,19 @@ export const RowFlex = ({
     {children}
   </div>
 )
+
+const S_ROW_FLEX_END = {
+  display: 'flex',
+  justifyContent: 'flex-end',
+  flexWrap: 'wrap',
+  margin: '8px 4px 10px 0'
+}
+
+export const RowFlexEnd = ({
+  style,
+  children
+}) => (
+  <div style={crStyle2(S_ROW_FLEX_END, style)}>
+    {children}
+  </div>
+)
