65441b09df5f9925c240e028e227b25b07b8c2b7
Yevhenii Molodyi <eugmolody@gmail.com>
2024-03-13 12:38:10 +0200

    Add function component factory _fRowFlex to dialogs RowFlex

--- js/components/dialogs/rows/RowFlex.js
+++ js/components/dialogs/rows/RowFlex.js
@@ -4,37 +4,27 @@ exports.__esModule = true;
 exports.S_ROW_FLEX = exports.RowFlexEnd = exports.RowFlex = void 0;
 var _styleFn = require("../../styleFn");
 var _jsxRuntime = require("react/jsx-runtime");
-const S_ROW_FLEX = exports.S_ROW_FLEX = {
-  display: 'flex',
-  alignItems: 'center',
-  margin: 5
-};
-const RowFlex = _ref => {
+const _fRowFlex = initialStyle => _ref => {
   let {
     style,
     children
   } = _ref;
   return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-    style: (0, _styleFn.crStyle2)(S_ROW_FLEX, style),
+    style: (0, _styleFn.crStyle2)(initialStyle, style),
     children: children
   });
 };
-exports.RowFlex = RowFlex;
+const S_ROW_FLEX = exports.S_ROW_FLEX = {
+  display: 'flex',
+  alignItems: 'center',
+  margin: 5
+};
+const RowFlex = exports.RowFlex = _fRowFlex(S_ROW_FLEX);
 const S_ROW_FLEX_END = {
   display: 'flex',
   justifyContent: 'flex-end',
   flexWrap: 'wrap',
   margin: '8px 4px 10px 0'
 };
-const RowFlexEnd = _ref2 => {
-  let {
-    style,
-    children
-  } = _ref2;
-  return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-    style: (0, _styleFn.crStyle2)(S_ROW_FLEX_END, style),
-    children: children
-  });
-};
-exports.RowFlexEnd = RowFlexEnd;
+const RowFlexEnd = exports.RowFlexEnd = _fRowFlex(S_ROW_FLEX_END);
 //# sourceMappingURL=RowFlex.js.map
--- src/components/dialogs/rows/RowFlex.jsx
+++ src/components/dialogs/rows/RowFlex.jsx
@@ -1,32 +1,27 @@
 import { crStyle2 } from '../../styleFn';
 
+const _fRowFlex = (
+  initialStyle
+) => ({
+  style,
+  children
+}) => (
+  <div style={crStyle2(initialStyle, style)}>
+    {children}
+  </div>
+);
+
 export const S_ROW_FLEX = {
   display: 'flex',
   alignItems: 'center',
   margin: 5
-};
-
-export const RowFlex = ({
-  style,
-  children
-}) => (
-  <div style={crStyle2(S_ROW_FLEX, style)}>
-    {children}
-  </div>
-)
+}
+export const RowFlex = _fRowFlex(S_ROW_FLEX)
 
 const S_ROW_FLEX_END = {
   display: 'flex',
   justifyContent: 'flex-end',
   flexWrap: 'wrap',
   margin: '8px 4px 10px 0'
-}
-
-export const RowFlexEnd = ({
-  style,
-  children
-}) => (
-  <div style={crStyle2(S_ROW_FLEX_END, style)}>
-    {children}
-  </div>
-)
+};
+export const RowFlexEnd = _fRowFlex(S_ROW_FLEX_END)
