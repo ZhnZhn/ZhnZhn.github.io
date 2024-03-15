36684d7baaf0c1db34cc3eb26d8c256a85bfb4e4
Yevhenii Molodyi <eugmolody@gmail.com>
2024-03-15 11:23:18 +0200

    Add crAriaLabelProp to BtSvgCircle

--- js/components/zhn/BtSvgCircle.js
+++ js/components/zhn/BtSvgCircle.js
@@ -3,31 +3,27 @@
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 exports.__esModule = true;
 exports.SvgPlus = exports.SvgMinus = void 0;
+var _a11yFn = require("../a11yFn");
 var _Button = _interopRequireDefault(require("./Button"));
 var _Svg = _interopRequireDefault(require("./svg/Svg"));
 var _jsxRuntime = require("react/jsx-runtime");
 const CL_BT_SVG_CIRCLE = "bt-svg-circle",
-  _fBtCircleSvg = pathElement => _ref => {
-    let {
-      style,
-      onClick
-    } = _ref;
-    return /*#__PURE__*/(0, _jsxRuntime.jsx)(_Button.default, {
-      className: CL_BT_SVG_CIRCLE,
-      style: style,
-      onClick: onClick,
-      children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_Svg.default, {
-        w: "20",
-        strokeWidth: "2",
-        strokeLinecap: "round",
-        children: pathElement
-      })
-    });
-  };
+  _fBtCircleSvg = (pathElement, dfAriaLabel) => props => /*#__PURE__*/(0, _jsxRuntime.jsx)(_Button.default, {
+    ...(0, _a11yFn.crAriaLabelProp)(props, dfAriaLabel),
+    className: CL_BT_SVG_CIRCLE,
+    style: props.style,
+    onClick: props.onClick,
+    children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_Svg.default, {
+      w: "20",
+      strokeWidth: "2",
+      strokeLinecap: "round",
+      children: pathElement
+    })
+  });
 const SvgMinus = exports.SvgMinus = _fBtCircleSvg( /*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
   d: "M 4,10 L 16,10"
-}));
+}), "Subtract");
 const SvgPlus = exports.SvgPlus = _fBtCircleSvg( /*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
   d: "M 10,4 L 10,16 M 4,10 L 16,10"
-}));
+}), "Add");
 //# sourceMappingURL=BtSvgCircle.js.map
--- src/components/zhn/BtSvgCircle.jsx
+++ src/components/zhn/BtSvgCircle.jsx
@@ -1,15 +1,18 @@
+import { crAriaLabelProp } from '../a11yFn';
+
 import Button from './Button';
 import Svg from './svg/Svg';
 
 const CL_BT_SVG_CIRCLE = "bt-svg-circle"
-, _fBtCircleSvg = (pathElement) => ({
-  style,
-  onClick
-}) => (
+, _fBtCircleSvg = (
+  pathElement,
+  dfAriaLabel
+) => (props) => (
   <Button
+    {...crAriaLabelProp(props, dfAriaLabel)}
     className={CL_BT_SVG_CIRCLE}
-    style={style}
-    onClick={onClick}
+    style={props.style}
+    onClick={props.onClick}
   >
      <Svg
        w="20"
@@ -22,8 +25,10 @@ const CL_BT_SVG_CIRCLE = "bt-svg-circle"
 );
 
 export const SvgMinus = _fBtCircleSvg(
-  <path d="M 4,10 L 16,10" />
+  <path d="M 4,10 L 16,10" />,
+  "Subtract"
 )
 export const SvgPlus = _fBtCircleSvg(
-  <path d="M 10,4 L 10,16 M 4,10 L 16,10" />
+  <path d="M 10,4 L 10,16 M 4,10 L 16,10" />,
+  "Add"
 )
