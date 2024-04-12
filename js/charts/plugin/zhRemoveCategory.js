6ff729b7d0bcf162596024f1ad11b6e7463ba02f
Yevhenii Molodyi <eugmolody@gmail.com>
2024-04-12 11:01:57 +0300

    Improve Highcharts custom plugin method zhRemoveCategory

--- js/charts/plugin/zhRemoveCategory.js
+++ js/charts/plugin/zhRemoveCategory.js
@@ -2,36 +2,29 @@
 
 exports.__esModule = true;
 exports.default = void 0;
-
-//refresh chart config
-const _updateSeriaAsync = seria => {
-  setTimeout(() => seria && seria.update(seria.options), 500);
-};
-
+var _pluginFn = require("./pluginFn");
 const zhRemoveCategory = function (id) {
-  try {
-    const _c = this.xAxis[0].categories;
-
-    if (_c) {
-      const _newC = _c.filter(str => str !== id),
-            _newData = this.options.series[0].data.filter(p => p.c !== id && p.name !== id && p.id !== id);
-
-      if (_newC.length < _c.length) {
-        const _seria = this.series[0];
-        this.xAxis[0].setCategories(_newC, false);
-
-        _seria.update({
-          data: _newData
-        });
-
-        _updateSeriaAsync(_seria);
-      }
+  const userOptions = this.userOptions,
+    {
+      xAxis,
+      series
+    } = userOptions,
+    _categories = xAxis.categories;
+  if (_categories) {
+    const _updatedCategories = _categories.filter(str => str !== id);
+    if (_updatedCategories.length < _categories.length) {
+      (0, _pluginFn.tryUpdate)(this, {
+        xAxis: {
+          ...xAxis,
+          categories: _updatedCategories
+        },
+        series: [{
+          ...series[0],
+          data: series[0].data.filter(p => p.c !== id && p.name !== id && p.id !== id)
+        }]
+      });
     }
-  } catch (err) {
-    console.log(err.message);
   }
 };
-
-var _default = zhRemoveCategory;
-exports.default = _default;
+var _default = exports.default = zhRemoveCategory;
 //# sourceMappingURL=zhRemoveCategory.js.map
--- src/charts/plugin/zhRemoveCategory.js
+++ src/charts/plugin/zhRemoveCategory.js
@@ -1,26 +1,29 @@
+import { tryUpdate } from './pluginFn';
 
-//refresh chart config
-const _updateSeriaAsync = (seria) => {
-  setTimeout(() => seria && seria.update(seria.options), 500)
-};
-
-const zhRemoveCategory = function(id){
-  try {
-    const _c = this.xAxis[0].categories;
-    if (_c) {
-      const _newC = _c.filter(str => str !== id)
-      , _newData = this.options.series[0].data.filter(p => p.c !== id && p.name !== id && p.id !== id)
-      if (_newC.length < _c.length) {
-        const _seria = this.series[0];
-        this.xAxis[0].setCategories(_newC, false)
-        _seria.update({ data: _newData })
-        _updateSeriaAsync(_seria)
-      }
+const zhRemoveCategory = function(id) {
+  const userOptions = this.userOptions
+  , { xAxis, series } = userOptions
+  , _categories = xAxis.categories;
+  if (_categories) {
+    const _updatedCategories = _categories
+      .filter(str => str !== id);
+    if (_updatedCategories.length < _categories.length) {
+      tryUpdate(this, {
+        xAxis: {
+          ...xAxis,
+          categories: _updatedCategories
+        },
+        series: [{
+          ...series[0],
+          data: series[0].data
+            .filter(p => p.c !== id
+               && p.name !== id
+               && p.id !== id
+            )
+        }]
+      })
     }
-  } catch (err) {
-    console.log(err.message)
   }
 };
-
 
 export default zhRemoveCategory
