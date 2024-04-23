e9350feed13c5f044ab88765f4ed8b42bce3a773
Yevhenii Molodyi <eugmolody@gmail.com>
2024-04-23 17:03:32 +0300

    Add WTO toLineAdapter

---
+++ src/adapters/wto/toLineAdapter.js
@@ -0,0 +1,65 @@
+import {
+  isNumber,
+  isStr,
+  assign,
+  getCaption,
+  joinBy,
+  ymdToUTC
+} from '../AdapterFn';
+import { compareByDate } from '../compareByFn';
+import crAdapterType1 from '../crAdapterType1';
+
+const crTitle = (
+  option,
+  json
+) => {
+  const {
+    items,
+    dfT
+   } = option
+  , _reporting = getCaption(items[0])
+  , _product = getCaption(items[1])
+  , item = json.Dataset[0] || {};
+  return {
+    title: joinBy(": ", _reporting, dfT),
+    subtitle: joinBy(": ", _product, item.Unit)
+  };
+};
+
+const trOption = (
+  option,
+  json
+) => {
+   assign(option, crTitle(option, json))
+}
+
+const _getPeriodCode = (
+  periodCode
+) => periodCode === "A"
+  ? ""
+  : isStr(periodCode)
+      ? "-" + periodCode.replace("M", "")
+      : "-NN";
+
+const crData = (
+  json
+) => json.Dataset.reduce((data, item) => {
+  const {
+    Value,
+    Year
+  } = item;
+  if (isNumber(Value) && isNumber(Year)) {
+    data.push([
+      ymdToUTC(''+ Year + _getPeriodCode(item.PeriodCode)),
+      Value
+    ])
+  }
+  return data;
+}, []).sort(compareByDate);
+
+const toLineAdapter = crAdapterType1({
+  crData,
+  trOption
+});
+
+export default toLineAdapter
