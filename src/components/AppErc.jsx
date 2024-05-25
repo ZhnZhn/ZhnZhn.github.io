9eece8b4a822aa96c796c12fb160a23212065bd6
Yevhenii Molodyi <eugmolody@gmail.com>
2024-05-25 19:09:51 +0300

    Improve details

--- src/components/AppErc.jsx
+++ src/components/AppErc.jsx
@@ -26,7 +26,7 @@ import DialogContainer from './dialogs/DialogContainer';
 
 import checkBuild from './checkBuild';
 
-const BUILD_DATE = '23-05-2024'
+const BUILD_DATE = '25-05-2024'
 , CL_COMP_CONTAINER = "component-container";
 
 const showSettings = bindTo(
