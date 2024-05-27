5c307daafa6be8f5f94b15f99874b4ce1174ac77
Yevhenii Molodyi <eugmolody@gmail.com>
2024-05-27 12:52:14 +0300

    Add toSplineAdapter to FAOSTAT data adapter

---
+++ js/adapters/faostat/toSplineAdapter.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+exports.__esModule = true;
+exports.default = void 0;
+var _pipe = _interopRequireDefault(require("../../utils/pipe"));
+var _configBuilderFn = require("../../charts/configBuilderFn");
+var _Tooltip = require("../../charts/Tooltip");
+var _fnAdapter = require("./fnAdapter");
+const toSplineAdapter = {
+  crKey: _fnAdapter.crDfItemKey,
+  toConfig(json, option) {
+    const _id = (0, _fnAdapter.crDfItemKey)(option),
+      _title = (0, _fnAdapter.crTitle)(json, option),
+      _subtitle = (0, _fnAdapter.crSubtitle)(json, option),
+      _points = (0, _fnAdapter.toDataPoints)(json, option);
+    return {
+      config: (0, _pipe.default)((0, _configBuilderFn.crAreaConfig)(), (0, _configBuilderFn.fAddCaption)(_title, _subtitle), (0, _configBuilderFn.fAddPointsToConfig)(_points), (0, _configBuilderFn.fAddMinMax)(_points, option), (0, _configBuilderFn.fAddTooltip)(_Tooltip.tooltipValueDmy), (0, _configBuilderFn.fAdd)({
+        info: (0, _fnAdapter.toInfo)(json, _title, _subtitle),
+        valueMoving: (0, _fnAdapter.crValueMoving)(_points),
+        zhConfig: (0, _fnAdapter.crZhConfig)(_id, option)
+      }), _configBuilderFn.toConfig)
+    };
+  },
+  toSeries(json, option) {
+    const {
+        data
+      } = json,
+      _data = (0, _fnAdapter.crSeriaData)(data, option),
+      {
+        itemCaption
+      } = option;
+    return (0, _pipe.default)((0, _configBuilderFn.crSeriaConfig)(), (0, _configBuilderFn.fAdd)({
+      data: _data,
+      minY: (0, _fnAdapter.findMinY)(_data),
+      name: itemCaption,
+      itemCaption
+    }));
+  }
+};
+var _default = exports.default = toSplineAdapter;
+//# sourceMappingURL=toSplineAdapter.js.map
---
+++ src/adapters/faostat/toSplineAdapter.js
@@ -0,0 +1,71 @@
+import pipe from '../../utils/pipe';
+
+import {
+  crAreaConfig,
+  fAddCaption,
+  fAddPointsToConfig,
+  fAddMinMax,
+  fAddTooltip,
+  fAdd,
+  toConfig,
+  crSeriaConfig
+} from '../../charts/configBuilderFn';
+import {
+  tooltipValueDmy
+} from '../../charts/Tooltip';
+
+import {
+  crDfItemKey,
+  crSubtitle,
+  crTitle,
+  toDataPoints,
+  crZhConfig,
+  toInfo,
+  crValueMoving,
+  crSeriaData,
+  findMinY
+} from './fnAdapter';
+
+const toSplineAdapter = {
+  crKey: crDfItemKey,
+
+  toConfig(json, option){
+    const _id = crDfItemKey(option)
+    , _title = crTitle(json, option)
+    , _subtitle = crSubtitle(json, option)
+    , _points = toDataPoints(json, option);
+
+    return {
+      config: pipe(
+        crAreaConfig(),
+        fAddCaption(_title, _subtitle),
+        fAddPointsToConfig(_points),
+        fAddMinMax(_points, option),
+        fAddTooltip(tooltipValueDmy),
+        fAdd({
+          info: toInfo(json, _title, _subtitle),
+          valueMoving: crValueMoving(_points),
+          zhConfig: crZhConfig(_id, option)
+        }),
+        toConfig
+      )
+    };
+  },
+
+  toSeries(json, option){
+    const { data } = json
+    , _data = crSeriaData(data, option)
+    , { itemCaption } = option;
+    return pipe(
+      crSeriaConfig(),
+      fAdd({
+        data: _data,
+        minY: findMinY(_data),
+        name: itemCaption,
+        itemCaption
+      })
+    );
+  }
+};
+
+export default toSplineAdapter
