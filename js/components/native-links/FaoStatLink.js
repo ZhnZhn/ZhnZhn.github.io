03a31743bd641c7e5fde904c80f3a4ef37bb0bdc
Yevhenii Molodyi <eugmolody@gmail.com>
2024-06-12 16:10:43 +0300

    Add isStr function from uiApi to FaoStatLink function component

--- js/components/native-links/FaoStatLink.js
+++ js/components/native-links/FaoStatLink.js
@@ -3,20 +3,18 @@
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 exports.__esModule = true;
 exports.default = void 0;
+var _uiApi = require("../uiApi");
 var _Link = _interopRequireDefault(require("./Link"));
 var _jsxRuntime = require("react/jsx-runtime");
 const DATA_URL = 'https://www.fao.org/faostat/en/#data/';
-const _isStr = v => typeof str === 'string';
 const FaoStatLink = _ref => {
   let {
     item
   } = _ref;
   return /*#__PURE__*/(0, _jsxRuntime.jsx)(_Link.default, {
-    isHttp: true,
-    href: _isStr(item) ? DATA_URL + item : DATA_URL,
+    href: (0, _uiApi.isStr)(item) ? DATA_URL + item : DATA_URL,
     caption: "FAOSTAT Link"
   });
 };
-var _default = FaoStatLink;
-exports.default = _default;
+var _default = exports.default = FaoStatLink;
 //# sourceMappingURL=FaoStatLink.js.map
--- src/components/native-links/FaoStatLink.jsx
+++ src/components/native-links/FaoStatLink.jsx
@@ -1,16 +1,15 @@
+import { isStr } from '../uiApi';
 import Link from './Link';
 
 const DATA_URL = 'https://www.fao.org/faostat/en/#data/';
-const _isStr = v => typeof str === 'string';
 
 const FaoStatLink = ({
   item
 }) => (
   <Link
-    isHttp={true}
-    href={_isStr(item)
-       ? DATA_URL + item
-       : DATA_URL
+    href={isStr(item)
+      ? DATA_URL + item
+      : DATA_URL
     }
     caption="FAOSTAT Link"
   />
