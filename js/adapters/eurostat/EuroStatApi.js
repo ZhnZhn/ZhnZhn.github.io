a171d9d1ac3418be8e2da7858a55ef9e9e8c5552
Yevhenii Molodyi <eugmolody@gmail.com>
2024-06-13 16:11:07 +0300

    Improve EuroStatApi

--- js/adapters/eurostat/EuroStatApi.js
+++ js/adapters/eurostat/EuroStatApi.js
@@ -5,25 +5,21 @@ exports.__esModule = true;
 exports.default = void 0;
 var _AdapterFn = require("../AdapterFn");
 var _api = _interopRequireDefault(require("./api/api"));
-const RES_ERR_STATUS = [400],
-  MSG_400 = '400: Bad request.\nDataset contains no data. One or more filtering elements (query parameters) are probably not valid.\nMaybe try to request this data set with older date or another country.',
-  _crDetailMsg = errLabel => errLabel + "\n\nIf you use For Date input field in Dialog\ntry to use more late date.",
-  _addPropTo = option => {
-    option.resErrStatus = [...RES_ERR_STATUS];
-    option._rt = option._rt || option.dfRt;
-  };
+const HTTP_STATUS_400 = 400,
+  MSG_HTTP_STATUS_400 = '400: Bad request.\nDataset contains no data. One or more filtering elements (query parameters) are probably not valid.\nMaybe try to request this data set with older date or another country.',
+  _crDetailMsg = errLabel => errLabel + "\n\nIf you use For Date input field in Dialog\ntry to use more late date.";
 const EuroStatApi = {
   getRequestUrl(option) {
-    _addPropTo(option);
+    option.resErrStatus = [HTTP_STATUS_400];
     return option.url ? option.url : option.url = _api.default.crUrlN(option);
   },
   checkResponse(json, option, status) {
-    if (status === 400) {
-      throw (0, _AdapterFn.crError)('', MSG_400);
+    if (status === HTTP_STATUS_400) {
+      throw (0, _AdapterFn.crError)('', MSG_HTTP_STATUS_400);
     }
     const {
       error
-    } = json;
+    } = json || {};
     if (error) {
       const {
           label
--- src/adapters/eurostat/EuroStatApi.js
+++ src/adapters/eurostat/EuroStatApi.js
@@ -1,31 +1,26 @@
 import { crError } from '../AdapterFn';
 import api from './api/api';
 
-const RES_ERR_STATUS = [400]
-, MSG_400 = '400: Bad request.\nDataset contains no data. One or more filtering elements (query parameters) are probably not valid.\nMaybe try to request this data set with older date or another country.'
-
-, _crDetailMsg = errLabel =>
-    `${errLabel}\n\nIf you use For Date input field in Dialog\ntry to use more late date.`
-, _addPropTo = option => {
-  option.resErrStatus = [...RES_ERR_STATUS]
-  option._rt = option._rt || option.dfRt;
-};
+const HTTP_STATUS_400 = 400
+, MSG_HTTP_STATUS_400 = '400: Bad request.\nDataset contains no data. One or more filtering elements (query parameters) are probably not valid.\nMaybe try to request this data set with older date or another country.'
+, _crDetailMsg = (
+  errLabel
+) => `${errLabel}\n\nIf you use For Date input field in Dialog\ntry to use more late date.`;
 
 const EuroStatApi = {
-
   getRequestUrl(option){
-    _addPropTo(option)
+    option.resErrStatus = [HTTP_STATUS_400]
     return option.url
       ? option.url
       : (option.url = api.crUrlN(option));
   },
 
   checkResponse(json, option, status) {
-    if (status === 400) {
-      throw crError('', MSG_400);
+    if (status === HTTP_STATUS_400) {
+      throw crError('', MSG_HTTP_STATUS_400);
     }
-    const { error } = json;
-    if (error){
+    const { error } = json || {};
+    if (error) {
       const { label } = error
       , _msgErr = label
           ? _crDetailMsg(label)
@@ -33,7 +28,6 @@ const EuroStatApi = {
       throw crError('', _msgErr);
     }
   }
-
 };
 
 export default EuroStatApi
