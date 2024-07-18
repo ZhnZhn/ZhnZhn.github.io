fa412f014c897ac37a46a9e2fdda6f0806afbbde
Yevhenii Molodyi <eugmolody@gmail.com>
2024-07-18 17:49:40 +0300

    Add CHT_BAR_TREE_MAP to constants ChartType

--- js/constants/ChartType.js
+++ js/constants/ChartType.js
@@ -1,23 +1,24 @@
 "use strict";
 
 exports.__esModule = true;
-exports.CHT_YEARLY = exports.CHT_TREE_MAP_CLUSTER = exports.CHT_TREE_MAP_2_CLUSTER = exports.CHT_TREE_MAP_2 = exports.CHT_TREE_MAP = exports.CHT_SPLINE = exports.CHT_SCATTER_UP = exports.CHT_SCATTER_DOWN = exports.CHT_SCATTER = exports.CHT_MAP = exports.CHT_LINE = exports.CHT_DOT_SET = exports.CHT_COLUMN_SET = exports.CHT_COLUMN_CLUSTER = exports.CHT_COLUMN = exports.CHT_BAR_WITH_LABELS = exports.CHT_BAR_SET = exports.CHT_BAR_CLUSTER = exports.CHT_BAR = exports.CHT_AREA_YEARLY = exports.CHT_AREA = void 0;
+exports.CHT_YEARLY = exports.CHT_TREE_MAP_CLUSTER = exports.CHT_TREE_MAP_2_CLUSTER = exports.CHT_TREE_MAP_2 = exports.CHT_TREE_MAP = exports.CHT_SPLINE = exports.CHT_SCATTER_UP = exports.CHT_SCATTER_DOWN = exports.CHT_SCATTER = exports.CHT_MAP = exports.CHT_LINE = exports.CHT_DOT_SET = exports.CHT_COLUMN_SET = exports.CHT_COLUMN_CLUSTER = exports.CHT_COLUMN = exports.CHT_BAR_WITH_LABELS = exports.CHT_BAR_TREE_MAP = exports.CHT_BAR_SET = exports.CHT_BAR_CLUSTER = exports.CHT_BAR = exports.CHT_AREA_YEARLY = exports.CHT_AREA = void 0;
 const CHT_AREA = exports.CHT_AREA = 'AREA',
   CHT_SPLINE = exports.CHT_SPLINE = 'SPLINE',
   CHT_LINE = exports.CHT_LINE = 'LINE',
   CHT_COLUMN = exports.CHT_COLUMN = 'COLUMN';
-const _crClusterType = chartType => chartType + "_CLUSTER",
-  _crSetType = chartType => chartType + "_SET";
+const _crClusterType = chartType => `${chartType}_CLUSTER`,
+  _crSetType = chartType => `${chartType}_SET`;
 const CHT_BAR = exports.CHT_BAR = "BAR",
   CHT_BAR_CLUSTER = exports.CHT_BAR_CLUSTER = _crClusterType(CHT_BAR),
   CHT_BAR_SET = exports.CHT_BAR_SET = _crSetType(CHT_BAR),
-  CHT_BAR_WITH_LABELS = exports.CHT_BAR_WITH_LABELS = CHT_BAR + "_WITH_LABELS",
+  CHT_BAR_WITH_LABELS = exports.CHT_BAR_WITH_LABELS = `${CHT_BAR}_WITH_LABELS`,
   CHT_COLUMN_SET = exports.CHT_COLUMN_SET = _crSetType(CHT_COLUMN),
   CHT_COLUMN_CLUSTER = exports.CHT_COLUMN_CLUSTER = _crClusterType(CHT_COLUMN),
   CHT_TREE_MAP = exports.CHT_TREE_MAP = "TREE_MAP",
   CHT_TREE_MAP_CLUSTER = exports.CHT_TREE_MAP_CLUSTER = _crClusterType(CHT_TREE_MAP),
-  CHT_TREE_MAP_2 = exports.CHT_TREE_MAP_2 = CHT_TREE_MAP + "_2",
-  CHT_TREE_MAP_2_CLUSTER = exports.CHT_TREE_MAP_2_CLUSTER = _crClusterType(CHT_TREE_MAP_2);
+  CHT_TREE_MAP_2 = exports.CHT_TREE_MAP_2 = `${CHT_TREE_MAP}_2`,
+  CHT_TREE_MAP_2_CLUSTER = exports.CHT_TREE_MAP_2_CLUSTER = _crClusterType(CHT_TREE_MAP_2),
+  CHT_BAR_TREE_MAP = exports.CHT_BAR_TREE_MAP = `BAR_${CHT_TREE_MAP}`;
 const CHT_MAP = exports.CHT_MAP = 'MAP',
   CHT_DOT_SET = exports.CHT_DOT_SET = 'DOT_SET';
 const CHT_YEARLY = exports.CHT_YEARLY = 'YEARLY';
--- src/constants/ChartType.js
+++ src/constants/ChartType.js
@@ -16,6 +16,7 @@ export const CHT_BAR = "BAR"
 , CHT_TREE_MAP_CLUSTER = _crClusterType(CHT_TREE_MAP)
 , CHT_TREE_MAP_2 = `${CHT_TREE_MAP}_2`
 , CHT_TREE_MAP_2_CLUSTER = _crClusterType(CHT_TREE_MAP_2)
+, CHT_BAR_TREE_MAP = `BAR_${CHT_TREE_MAP}`
 
 export const CHT_MAP = 'MAP'
 , CHT_DOT_SET = 'DOT_SET'
