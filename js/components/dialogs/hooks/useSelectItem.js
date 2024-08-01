4fd8d53ff9ba5ce22e793686739ee29f97205ddf
Yevhenii Molodyi <eugmolody@gmail.com>
2024-08-01 16:40:38 +0300

    Add filters logic to dialogs useSelectItem

--- js/components/dialogs/hooks/useSelectItem.js
+++ js/components/dialogs/hooks/useSelectItem.js
@@ -3,9 +3,12 @@
 exports.__esModule = true;
 exports.default = void 0;
 var _uiApi = require("../../uiApi");
+const _isArr = Array.isArray;
 const TABLE_ID = 'table';
 const useSelectItem = setChartConfigFromItem => {
   const _refItems = (0, _uiApi.useRef)([]),
+    _refFilterId = (0, _uiApi.useRef)(),
+    [filters, setFilters] = (0, _uiApi.useState)(),
     _hSelect = (0, _uiApi.useCallback)((id, index, item) => {
       (0, _uiApi.getRefValue)(_refItems)[index] = item;
       if (item) {
@@ -13,9 +16,23 @@ const useSelectItem = setChartConfigFromItem => {
         if (id === TABLE_ID) {
           setChartConfigFromItem(item);
         }
+        setFilters(prevFilters => {
+          if (_isArr(item.not)) {
+            (0, _uiApi.setRefValue)(_refFilterId, id);
+            return item.not;
+          } else {
+            if (_isArr(prevFilters)) {
+              return prevFilters.length === 0 ? void 0 : id === (0, _uiApi.getRefValue)(_refFilterId) ? [] : prevFilters;
+            } else {
+              return prevFilters;
+            }
+          }
+        });
+      } else {
+        setFilters();
       }
     }, [setChartConfigFromItem]);
-  return [_refItems, _hSelect];
+  return [_refItems, _hSelect, filters];
 };
 var _default = exports.default = useSelectItem;
 //# sourceMappingURL=useSelectItem.js.map
--- src/components/dialogs/hooks/useSelectItem.js
+++ src/components/dialogs/hooks/useSelectItem.js
@@ -1,15 +1,22 @@
 import {
   useRef,
+  useState,
   useCallback,
-  getRefValue
+  getRefValue,
+  setRefValue
 } from '../../uiApi';
 
+const _isArr = Array.isArray;
 const TABLE_ID = 'table';
 
 const useSelectItem = (
   setChartConfigFromItem
 ) => {
   const _refItems = useRef([])
+
+  , _refFilterId = useRef()
+  , [filters, setFilters] = useState()
+
   , _hSelect = useCallback((id, index, item) => {
      getRefValue(_refItems)[index] = item
      if (item) {
@@ -17,11 +24,31 @@ const useSelectItem = (
        if (id === TABLE_ID) {
          setChartConfigFromItem(item)
        }
+       setFilters(prevFilters => {
+         if (_isArr(item.not)) {
+           setRefValue(_refFilterId, id)
+           return item.not;
+         } else {
+           if (_isArr(prevFilters)) {
+             return prevFilters.length === 0
+               ? void 0
+               : id === getRefValue(_refFilterId)
+                  ? []
+                  : prevFilters;
+           } else {
+             return prevFilters;
+           }
+         }
+       })
+     } else {
+       setFilters()
      }
   }, [setChartConfigFromItem]);
+
   return [
     _refItems,
-    _hSelect
+    _hSelect,
+    filters
   ];
 };
 
