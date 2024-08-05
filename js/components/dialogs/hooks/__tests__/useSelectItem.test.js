9136b5558070a7256c3a8c28b80d185bd246df92
Yevhenii Molodyi <eugmolody@gmail.com>
2024-08-05 15:10:33 +0300

    Add useSelectItem.test.js

---
+++ js/components/dialogs/hooks/__tests__/useSelectItem.test.js
@@ -0,0 +1,99 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+var _react = require("@testing-library/react");
+var _useSelectItem = _interopRequireDefault(require("../useSelectItem"));
+const _getRefItems = result => result.current[0],
+  _getSelectItem = result => result.current[1],
+  _getTupleFilter = result => result.current[2];
+describe("useSelectItem", () => {
+  test("should return tuple with refItems, hSelect, tupleFilter", () => {
+    const setChartConfigFromItem = jest.fn(),
+      {
+        result
+      } = (0, _react.renderHook)(() => (0, _useSelectItem.default)(setChartConfigFromItem));
+    const _refItems = _getRefItems(result),
+      _hSelect = _getSelectItem(result),
+      _tupleFilter = _getTupleFilter(result);
+    expect(_refItems.current).toEqual([]);
+    expect(typeof _hSelect).toBe("function");
+    expect(_tupleFilter).toEqual([]);
+
+    // set input1 to _items1
+    const id1 = "id1",
+      _filters1 = ["A"],
+      _item1 = {
+        c: "A",
+        v: "a",
+        not: _filters1
+      };
+    (0, _react.act)(() => _hSelect(id1, 0, _item1));
+    expect(_getRefItems(result).current).toEqual([{
+      ..._item1,
+      id: id1
+    }]);
+    expect(_getTupleFilter(result)).toEqual([id1, _filters1]);
+
+    //clear input1
+    (0, _react.act)(() => _hSelect(id1, 0, void 0));
+    expect(_getRefItems(result).current).toEqual([void 0]);
+    expect(_getTupleFilter(result)).toEqual([]);
+
+    // set input1 to _items2
+    const _filters2 = ["B"],
+      _item2 = {
+        c: "A",
+        v: "a",
+        not: _filters2
+      };
+    (0, _react.act)(() => _hSelect(id1, 0, _item2));
+    expect(_getRefItems(result).current).toEqual([{
+      ..._item2,
+      id: id1
+    }]);
+    expect(_getTupleFilter(result)).toEqual([id1, _filters2]);
+
+    // set input1 to _item1
+    (0, _react.act)(() => _hSelect(id1, 0, _item1));
+    expect(_getRefItems(result).current).toEqual([{
+      ..._item1,
+      id: id1
+    }]);
+    expect(_getTupleFilter(result)).toEqual([id1, _filters1]);
+
+    //clear input1
+    (0, _react.act)(() => _hSelect(id1, 0, void 0));
+    expect(_getRefItems(result).current).toEqual([void 0]);
+    expect(_getTupleFilter(result)).toEqual([]);
+
+    //clear input1
+    (0, _react.act)(() => _hSelect(id1, 0, void 0));
+    expect(_getRefItems(result).current).toEqual([void 0]);
+    expect(_getTupleFilter(result)).toEqual([]);
+
+    // set input2 to _itemTable
+    const TABLE_ID = "table",
+      _itemTable = {
+        c: "Table",
+        v: "t"
+      };
+    (0, _react.act)(() => _hSelect(TABLE_ID, 1, _itemTable));
+    expect(_getRefItems(result).current).toEqual([void 0, {
+      ..._itemTable,
+      id: TABLE_ID
+    }]);
+    expect(_getTupleFilter(result)).toEqual([]);
+    expect(setChartConfigFromItem).toBeCalledTimes(1);
+    expect(setChartConfigFromItem).toBeCalledWith({
+      ..._itemTable,
+      id: TABLE_ID
+    });
+
+    //clear input2
+    (0, _react.act)(() => _hSelect(TABLE_ID, 1, void 0));
+    expect(_getRefItems(result).current).toEqual([void 0, void 0]);
+    expect(_getTupleFilter(result)).toEqual([]);
+    expect(setChartConfigFromItem).toBeCalledTimes(1);
+  });
+});
+//# sourceMappingURL=useSelectItem.test.js.map
