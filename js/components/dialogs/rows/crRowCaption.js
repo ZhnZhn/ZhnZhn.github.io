8edcae4da1fb61545071f6370717f323b36c3b02
Yevhenii Molodyi <eugmolody@gmail.com>
2024-08-07 15:49:41 +0300

    Add functon _crCaptionTuple to crRowCaption

--- js/components/dialogs/rows/crRowCaption.js
+++ js/components/dialogs/rows/crRowCaption.js
@@ -3,10 +3,11 @@
 exports.__esModule = true;
 exports.default = void 0;
 const MAX_WORD_LENGTH = 11,
-  MAX_LONG_LENGTH = 30;
+  MAX_LONG_LENGTH = 30,
+  _isOneLongWord = caption => caption.length > MAX_WORD_LENGTH && caption.indexOf(' ') === -1,
+  _crCaptionTuple = (caption, toIndex, endToken) => [caption.slice(0, toIndex) + endToken, caption];
 
 // [caption, title]
-const crRowCaption = caption => typeof caption !== 'string' ? [] : caption.length > MAX_WORD_LENGTH && caption.indexOf(' ') === -1 ? [caption.slice(0, MAX_WORD_LENGTH) + '.', caption] : caption.length > MAX_LONG_LENGTH ? [caption.slice(0, MAX_LONG_LENGTH) + '...', caption] : [caption];
-var _default = crRowCaption;
-exports.default = _default;
+const crRowCaption = caption => typeof caption !== 'string' ? [] : _isOneLongWord(caption) ? _crCaptionTuple(caption, MAX_WORD_LENGTH, '.') : caption.length > MAX_LONG_LENGTH ? _crCaptionTuple(caption, MAX_LONG_LENGTH, '...') : [caption];
+var _default = exports.default = crRowCaption;
 //# sourceMappingURL=crRowCaption.js.map
--- src/components/dialogs/rows/crRowCaption.js
+++ src/components/dialogs/rows/crRowCaption.js
@@ -1,23 +1,24 @@
 const MAX_WORD_LENGTH = 11
-, MAX_LONG_LENGTH = 30;
+, MAX_LONG_LENGTH = 30
+, _isOneLongWord = caption => caption.length > MAX_WORD_LENGTH && caption.indexOf(' ') === -1
+, _crCaptionTuple = (
+  caption,
+  toIndex,
+  endToken
+) => [
+  caption.slice(0, toIndex) + endToken,
+  caption
+];
 
 // [caption, title]
 const crRowCaption = (
   caption
 ) => typeof caption !== 'string'
   ? []
-  : caption.length > MAX_WORD_LENGTH && caption.indexOf(' ') === -1
-      ? [
-          caption.slice(0, MAX_WORD_LENGTH) + '.',
-          caption
-        ]
+  : _isOneLongWord(caption)
+      ? _crCaptionTuple(caption, MAX_WORD_LENGTH, '.')
       : caption.length > MAX_LONG_LENGTH
-         ? [
-             caption.slice(0, MAX_LONG_LENGTH) + '...',
-             caption
-           ]
-         : [
-             caption
-           ];
+         ? _crCaptionTuple(caption, MAX_LONG_LENGTH, '...')           
+         : [ caption ];
 
 export default crRowCaption
