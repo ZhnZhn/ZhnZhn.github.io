1351aa3ad470fa8b4a5e649b28c2bb5a819b4f70
Yevhenii Molodyi <eugmolody@gmail.com>
2024-09-10 17:40:35 +0300

    Add toLineAdapter to ECB data adapter

---
+++ js/adapters/ecb/toLineAdapter.js
@@ -0,0 +1,42 @@
+"use strict";
+
+exports.__esModule = true;
+exports.default = void 0;
+var _crAdapterType = require("../crAdapterType1");
+var _AdapterFn = require("../AdapterFn");
+var _compareByFn = require("../compareByFn");
+var _fnAdapter = require("./fnAdapter");
+const ITEM_URL = `https://data.${_fnAdapter.ECB_EUROPA_EU}/data/datasets`;
+const crData = (json, option) => {
+  const _observations = (0, _fnAdapter.getSeriesObservertions)(json),
+    _values = (0, _fnAdapter.getObservationValues)(json);
+  return (0, _AdapterFn.getObjectKeys)(_observations).reduce((data, prName) => {
+    const _dateMls = (0, _AdapterFn.ymdToUTC)((_values[prName] || {}).id),
+      _value = (_observations[prName] || [])[0];
+    if ((0, _AdapterFn.isNumber)(_dateMls) && (0, _AdapterFn.isNumber)(_value)) {
+      data.push([_dateMls, _value]);
+    }
+    return data;
+  }, []).sort(_compareByFn.compareByDate);
+};
+const addToConfig = (config, json, option) => {
+  (0, _AdapterFn.addToConfigInfo)(config, option);
+  (0, _AdapterFn.addToConfigDfLink)(config, "ECB Data Portal", `${ITEM_URL}/${option.dfR}/${option.dfR}.${(0, _fnAdapter.crItemId)(option)}`);
+  return config;
+};
+const trOption = option => {
+  const {
+    dfSubt
+  } = option;
+  if (dfSubt) {
+    option.subtitle = (0, _AdapterFn.joinBy)(', ', option.subtitle, dfSubt);
+  }
+};
+const toLineAdapter = (0, _crAdapterType.crAdapterType1)({
+  crData,
+  crConfOption: (0, _crAdapterType.fCrConfOptionExchangeRate)("EUR"),
+  addToConfig,
+  trOption
+});
+var _default = exports.default = toLineAdapter;
+//# sourceMappingURL=toLineAdapter.js.map
---
+++ src/adapters/ecb/toLineAdapter.js
@@ -0,0 +1,69 @@
+import {
+  fCrConfOptionExchangeRate,
+  crAdapterType1
+} from '../crAdapterType1';
+import {
+  isNumber,
+  getObjectKeys,
+  ymdToUTC,
+  addToConfigInfo,
+  addToConfigDfLink,
+  joinBy
+} from '../AdapterFn';
+import { compareByDate } from '../compareByFn';
+import {
+  ECB_EUROPA_EU,
+  crItemId,
+  getSeriesObservertions,
+  getObservationValues
+} from './fnAdapter';
+
+const ITEM_URL = `https://data.${ECB_EUROPA_EU}/data/datasets`
+
+const crData = (
+  json,
+  option
+) => {
+  const _observations = getSeriesObservertions(json)
+  , _values = getObservationValues(json);
+  return getObjectKeys(_observations)
+    .reduce((data, prName) => {
+       const _dateMls = ymdToUTC((_values[prName] || {}).id)
+       , _value = (_observations[prName] || [])[0];
+       if (isNumber(_dateMls) && isNumber(_value)) {
+         data.push([_dateMls, _value])
+       }
+       return data;
+    }, []).sort(compareByDate);
+};
+
+const addToConfig = (
+  config,
+  json,
+  option
+) => {
+  addToConfigInfo(config, option)
+  addToConfigDfLink(config,
+    "ECB Data Portal",
+    `${ITEM_URL}/${option.dfR}/${option.dfR}.${crItemId(option)}`
+  )
+  return config;
+}
+
+const trOption = option => {
+  const {
+    dfSubt
+  } = option;
+  if (dfSubt) {
+    option.subtitle = joinBy(', ', option.subtitle, dfSubt)
+  }
+};
+
+const toLineAdapter = crAdapterType1({
+  crData,
+  crConfOption: fCrConfOptionExchangeRate("EUR"),
+  addToConfig,
+  trOption
+});
+
+export default toLineAdapter
