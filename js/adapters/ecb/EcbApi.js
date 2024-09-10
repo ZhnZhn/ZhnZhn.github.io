eaa25195e72184ac5ae985a9474a773ec4463c62
Yevhenii Molodyi <eugmolody@gmail.com>
2024-09-10 17:42:04 +0300

    Add category route to EcbApi

--- js/adapters/ecb/EcbApi.js
+++ js/adapters/ecb/EcbApi.js
@@ -3,11 +3,13 @@
 exports.__esModule = true;
 exports.default = void 0;
 var _AdapterFn = require("../AdapterFn");
+var _CategoryFn = require("../CategoryFn");
 var _fnAdapter = require("./fnAdapter");
 const API_URL = `https://data-api.${_fnAdapter.ECB_EUROPA_EU}/service/data`;
 const EcbApi = {
   getRequestUrl(option) {
-    return `${option.proxy}${API_URL}/${option.dfR}/${(0, _fnAdapter.crItemId)(option)}?format=jsondata&detail=dataonly&startPeriod=${option.fromDate}`;
+    const _queryDate = (0, _CategoryFn.isCategory)(option.seriaType) ? `startPeriod=${option.time}&endPeriod=${option.time}` : `startPeriod=${option.fromDate}`;
+    return `${option.proxy}${API_URL}/${option.dfR}/${(0, _fnAdapter.crItemId)(option)}?format=jsondata&detail=dataonly&${_queryDate}`;
   },
   checkResponse(json) {
     if (!(0, _AdapterFn.isObj)((0, _fnAdapter.getSeriesObservertions)(json))) {
--- src/adapters/ecb/EcbApi.js
+++ src/adapters/ecb/EcbApi.js
@@ -3,6 +3,9 @@ import {
   crError
 } from '../AdapterFn';
 import {
+  isCategory
+} from '../CategoryFn';
+import {
   ECB_EUROPA_EU,
   crItemId,
   getSeriesObservertions
@@ -12,7 +15,10 @@ const API_URL = `https://data-api.${ECB_EUROPA_EU}/service/data`;
 
 const EcbApi = {
   getRequestUrl(option){
-    return `${option.proxy}${API_URL}/${option.dfR}/${crItemId(option)}?format=jsondata&detail=dataonly&startPeriod=${option.fromDate}`;
+    const _queryDate = isCategory(option.seriaType)
+      ? `startPeriod=${option.time}&endPeriod=${option.time}`
+      : `startPeriod=${option.fromDate}`;
+    return `${option.proxy}${API_URL}/${option.dfR}/${crItemId(option)}?format=jsondata&detail=dataonly&${_queryDate}`;
   },
   checkResponse(json){
     if (!isObj(getSeriesObservertions(json))) {
