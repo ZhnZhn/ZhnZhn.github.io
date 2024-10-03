c5d6dfd75eade7cf5b9aca19bf455aa23c3f731d
Yevhenii Molodyi <eugmolody@gmail.com>
2024-10-03 14:25:07 +0300

    Add function crYearlyData from JsonStatFn to stat-json toYearly

--- js/adapters/stat-json/toYearly.js
+++ js/adapters/stat-json/toYearly.js
@@ -5,25 +5,18 @@ exports.__esModule = true;
 exports.default = void 0;
 var _pipe = _interopRequireDefault(require("../../utils/pipe"));
 var _configBuilderFn = require("../../charts/configBuilderFn");
+var _JsonStatFn = require("../JsonStatFn");
 var _toYearsByMonths = _interopRequireDefault(require("../toYearsByMonths"));
 var _fnAdapter = require("./fnAdapter");
-var _fnUtil = require("./fnUtil");
-const _isArr = Array.isArray;
-const _toData = (values, times) => {
-  const _values = _isArr(values) ? values : [values];
-  const data = times.map((time, i) => [(0, _fnUtil.toYMD)(time), _values[i].value]);
-  return data.reverse();
-};
 const crYearlyConfig = (json, option) => {
   const {
       title = '',
       subtitle
     } = option,
-    [ds, values, times] = (0, _fnAdapter.crDsValuesTimes)(json, option),
-    data = _toData(values, times);
+    data = (0, _JsonStatFn.crYearlyData)(json).reverse();
   return (0, _pipe.default)((0, _toYearsByMonths.default)(data, option), (0, _configBuilderFn.fAdd)('chart', {
     spacingTop: 25
-  }), (0, _configBuilderFn.fAddCaption)(title, subtitle), (0, _configBuilderFn.fAdd)('info', (0, _fnAdapter.crInfo)(ds, option)), (0, _configBuilderFn.fAdd)('zhConfig', (0, _fnAdapter.crZhConfig)(option)), _configBuilderFn.toConfig);
+  }), (0, _configBuilderFn.fAddCaption)(title, subtitle), (0, _configBuilderFn.fAdd)('info', (0, _fnAdapter.crInfo)(option, json)), (0, _configBuilderFn.fAdd)('zhConfig', (0, _fnAdapter.crZhConfig)(option)), _configBuilderFn.toConfig);
 };
 var _default = exports.default = crYearlyConfig;
 //# sourceMappingURL=toYearly.js.map
--- src/adapters/stat-json/toYearly.js
+++ src/adapters/stat-json/toYearly.js
@@ -4,31 +4,13 @@ import {
   fAdd,
   toConfig
 } from '../../charts/configBuilderFn';
+import { crYearlyData } from '../JsonStatFn';
 import crYearlyConfigImpl from '../toYearsByMonths';
 
 import {
-  crDsValuesTimes,
   crInfo,
-  crZhConfig,
+  crZhConfig
 } from './fnAdapter';
-import { toYMD } from './fnUtil';
-
-const _isArr = Array.isArray;
-
-const _toData = (
-  values,
-  times
-) => {
-  const _values = _isArr(values)
-    ? values
-    : [values];
-  const data = times.map((time, i) => ([
-    toYMD(time),
-    _values[i].value
-  ]))
-
-  return data.reverse();
-};
 
 const crYearlyConfig = (
   json,
@@ -38,21 +20,15 @@ const crYearlyConfig = (
     title='',
     subtitle
   } = option
-  , [
-    ds,
-    values,
-    times
-  ] = crDsValuesTimes(json, option)
-  , data = _toData(values, times);
-
-   return pipe(
-     crYearlyConfigImpl(data, option),
-     fAdd('chart', { spacingTop: 25 }),
-     fAddCaption(title, subtitle),
-     fAdd('info', crInfo(ds, option)),
-     fAdd('zhConfig', crZhConfig(option)),
-     toConfig
-   );
+  , data = crYearlyData(json).reverse();
+  return pipe(
+    crYearlyConfigImpl(data, option),
+    fAdd('chart', { spacingTop: 25 }),
+    fAddCaption(title, subtitle),
+    fAdd('info', crInfo(option, json)),
+    fAdd('zhConfig', crZhConfig(option)),
+    toConfig
+  );
 };
 
 export default crYearlyConfig
