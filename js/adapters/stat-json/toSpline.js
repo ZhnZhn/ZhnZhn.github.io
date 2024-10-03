0bba0d4e4a6e2f4b700a31be1760af52daa36b4d
Yevhenii Molodyi <eugmolody@gmail.com>
2024-10-03 14:24:03 +0300

    Add function crSplineData from JsonStatFn to stat-json toSpline

--- js/adapters/stat-json/toSpline.js
+++ js/adapters/stat-json/toSpline.js
@@ -5,44 +5,16 @@ exports.__esModule = true;
 exports.default = void 0;
 var _ChartType = require("../../constants/ChartType");
 var _crConfigType = _interopRequireDefault(require("../../charts/crConfigType1"));
-var _fnUtil = require("./fnUtil");
+var _JsonStatFn = require("../JsonStatFn");
 var _fnAdapter = require("./fnAdapter");
 var _toYearly = _interopRequireDefault(require("./toYearly"));
-const _isArr = Array.isArray;
-const _filterLeadingNulls = data => {
-  const _len = data.length;
-  let i = 0;
-  for (i; i < _len; i++) {
-    if (data[i][1] !== null) break;
-  }
-  let j = _len - 1;
-  for (j; j > -1; j--) {
-    if (data[j][1] !== null) break;
-  }
-  return data.slice(i, j + 1);
-};
 const _isReverse = data => data.length > 2 && data[0][0] > data[1][0];
-const _checkOrder = data => _isReverse(data) ? data.reverse() : data;
+const _checkTimeOrder = data => _isReverse(data) ? data.reverse() : data;
 const _isPerJanuary = label => (label || '').indexOf('per 1 January') !== -1;
-const _fCrDataPoint = (values, hasPerJanuary) => (time, i) => {
-  const _pIndex = time.length - 1,
-    isP = time[_pIndex] === '*',
-    _time = isP ? time.slice(0, _pIndex) : time,
-    x = (0, _fnUtil.toUTC)(_time, hasPerJanuary),
-    y = values[i] ? values[i].value : null;
-  return isP ? [x, y, 'p'] : [x, y];
-};
-const _postProcessData = (0, _fnUtil.compose)(_filterLeadingNulls, _checkOrder);
-const _toData = (values, times, hasPerJanuary) => {
-  const _values = _isArr(values) ? values : [values],
-    _crPoint = _fCrDataPoint(_values, hasPerJanuary);
-  return _isArr(times) ? _postProcessData(times.map(_crPoint)) : [];
-};
+const _crData = json => _checkTimeOrder((0, _JsonStatFn.crSplineData)(json, _isPerJanuary((0, _JsonStatFn.getDatasetLabel)(json))));
 const _crSplineConfig = (json, option) => {
-  const [ds, values, times] = (0, _fnAdapter.crDsValuesTimes)(json, option),
-    _hasPerJanuary = _isPerJanuary(ds.label),
-    data = _toData(values, times, _hasPerJanuary),
-    confOption = (0, _fnAdapter.crConfOption)(ds, option);
+  const data = _crData(json),
+    confOption = (0, _fnAdapter.crConfOption)(option, json);
   return (0, _crConfigType.default)({
     option,
     data,
--- src/adapters/stat-json/toSpline.js
+++ src/adapters/stat-json/toSpline.js
@@ -8,89 +8,35 @@ import {
 import crConfigType1 from '../../charts/crConfigType1';
 
 import {
-  toUTC,
-  compose
-} from './fnUtil';
-import {
-  crDsValuesTimes,
-  crConfOption
-} from './fnAdapter';
+  getDatasetLabel,
+  crSplineData
+} from '../JsonStatFn';
 
+import { crConfOption } from './fnAdapter';
 import crYearlyConfig from './toYearly';
-
-const _isArr = Array.isArray;
-
-const _filterLeadingNulls = data => {
-  const _len = data.length;
-  let i = 0;
-  for(i; i<_len; i++){
-    if (data[i][1] !== null) break;
-  }
-  let j = _len - 1;
-  for(j; j>-1; j--){
-    if (data[j][1] !== null) break;
-  }
-  return data.slice(i, j+1);
-};
 
 const _isReverse = data => data.length > 2
   && data[0][0] > data[1][0];
-const _checkOrder = data => _isReverse(data)
+const _checkTimeOrder = data => _isReverse(data)
   ? data.reverse()
   : data;
 
 const _isPerJanuary = (label) => (label || '')
   .indexOf('per 1 January') !== -1;
 
-const _fCrDataPoint = (
-  values,
-  hasPerJanuary
-) => (time, i) => {
-  const _pIndex = time.length - 1
-  , isP = time[_pIndex] === '*'
-  , _time = isP
-     ? time.slice(0, _pIndex)
-     : time
-  , x = toUTC(_time, hasPerJanuary)
-  , y = values[i]
-     ? values[i].value
-     : null;
-  return isP
-     ? [x, y, 'p']
-     : [x, y];
-};
-
-const _postProcessData = compose(
-  _filterLeadingNulls,
-  _checkOrder
-);
-
-const _toData = (
-  values,
-  times,
-  hasPerJanuary
-) => {
-  const _values = _isArr(values)
-    ? values
-    : [values]
-  , _crPoint = _fCrDataPoint(_values, hasPerJanuary);
-  return _isArr(times)
-    ? _postProcessData(times.map(_crPoint))
-    : [];
-};
+const _crData = (
+  json
+) => _checkTimeOrder(crSplineData(
+  json,
+  _isPerJanuary(getDatasetLabel(json))
+));
 
 const _crSplineConfig = (
   json,
   option
 ) => {
-  const [
-    ds,
-    values,
-    times
-  ] = crDsValuesTimes(json, option)
-  , _hasPerJanuary = _isPerJanuary(ds.label)
-  , data = _toData(values, times, _hasPerJanuary)
-  , confOption = crConfOption(ds, option);
+  const data = _crData(json)
+  , confOption = crConfOption(option, json);
 
   return crConfigType1({
     option,
