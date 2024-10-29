eebd188eafd84700bba97cd90e66d497e9b9f15a
Yevhenii Molodyi <eugmolody@gmail.com>
2024-10-29 14:09:06 +0200

    Add function getByPropsFrom from AdapterFn to BEA fnAdapter

--- js/adapters/bea/fnAdapter.js
+++ js/adapters/bea/fnAdapter.js
@@ -1,41 +1,40 @@
 "use strict";
 
 exports.__esModule = true;
-exports.crData = exports.crConfOption = void 0;
+exports.getResultsData = exports.getResults = exports.getResError = exports.getFrequency = exports.crData = exports.crConfOption = exports.BEA_DATA_URL = void 0;
 var _AdapterFn = require("../AdapterFn");
-const _getResults = json => json.BEAAPI.Results,
-  _getData = Results => (0, _AdapterFn.isArr)(Results) ? Results[0].Data : Results.Data,
-  _getInfo = Results => (0, _AdapterFn.isArr)(Results) ? Results[0] : Results;
-const _crName = info => {
+const BEA_DATA_URL = exports.BEA_DATA_URL = "https://apps.bea.gov";
+const INDUSTRY_FACTSHEET_URL = `${BEA_DATA_URL}/industry/factsheet/factsheet.html`;
+const getFrequency = item => (0, _AdapterFn.getCaption)(item, "").indexOf("(A,Q)") === -1 ? "A" : "Q";
+exports.getFrequency = getFrequency;
+const _getBeaapi = json => (json || {}).BEAAPI || {};
+const getResults = json => _getBeaapi(json).Results,
+  getResError = json => _getBeaapi(json).Error,
+  getResultsData = Results => (0, _AdapterFn.getByPropsFrom)(Results, ...((0, _AdapterFn.isArr)(Results) ? [0, "Data"] : ["Data"]));
+exports.getResultsData = getResultsData;
+exports.getResError = getResError;
+exports.getResults = getResults;
+const _getResultsInfo = Results => (0, _AdapterFn.isArr)(Results) ? Results[0] : Results;
+const _crName = info => (0, _AdapterFn.joinBy)(": ", info.Statistic, (info.UTCProductionTime || "").replace("T", " "));
+const _crDescr = info => (info.Notes || []).map(note => {
   const {
-      Statistic = '',
-      UTCProductionTime = ''
-    } = info,
-    t = UTCProductionTime.replace('T', ' ');
-  return `${Statistic}: ${t}`;
-};
-const _crDescr = info => {
-  const {
-      Notes = []
-    } = info,
-    arr = Notes.map(note => {
-      const {
-        NoteRef = '',
-        NoteText = ''
-      } = note;
-      return `<P>${NoteRef}: ${NoteText}</P><BR/>`;
-    });
-  return arr.join('');
-};
+    NoteRef = '',
+    NoteText = ''
+  } = note || {};
+  return `<p>${NoteRef}: ${NoteText}</p><br/>`;
+}).join('');
 const _crInfo = Results => {
-  const _info = _getInfo(Results);
+  const _info = _getResultsInfo(Results) || {};
   return {
     name: _crName(_info),
     description: _crDescr(_info)
   };
 };
+const _crDfLinkCondig = (ValueName, industryItem) => ValueName === "Industry" && getFrequency(industryItem) === "Q" ? (0, _AdapterFn.crDfLink)(`BEA ${(0, _AdapterFn.getCaption)(industryItem)}`, `${INDUSTRY_FACTSHEET_URL}#${(0, _AdapterFn.getValue)(industryItem)}`) : void 0;
 const _crZhConfig = _ref => {
   let {
+    ValueName,
+    items,
     _itemKey,
     itemCaption,
     dataSource
@@ -44,53 +43,46 @@ const _crZhConfig = _ref => {
     id: _itemKey,
     key: _itemKey,
     itemCaption,
-    dataSource
+    dataSource,
+    ..._crDfLinkCondig(ValueName, items[0])
   };
 };
 const MD = {
-  DF: '-12-31',
-  'I': '-03-31',
-  'II': '-06-30',
-  'III': '-09-30'
+  DF: "-12-31",
+  I: "-03-31",
+  II: "-06-30",
+  III: "-09-30"
 };
 const _crUTC = item => {
-  const {
-      Frequency,
-      Year,
-      Quarter
-    } = item,
-    md = Frequency === 'Q' ? MD[Quarter] || MD.DF : MD.DF;
-  return (0, _AdapterFn.ymdToUTC)(Year + md);
+  const md = item.Frequency === "Q" ? MD[item.Quarter] || MD.DF : MD.DF;
+  return (0, _AdapterFn.ymdToUTC)(item.Year + md);
 };
 const crData = (json, option) => {
-  const Results = _getResults(json),
+  const Results = getResults(json),
     {
       dfFilterName,
       items
     } = option,
-    two = (items[1] || {}).value,
-    d = [],
-    isFilter = dfFilterName ? true : false,
-    data = _getData(Results) || [];
-  data.forEach(item => {
-    const v = parseFloat(item.DataValue),
-      y = (0, _AdapterFn.isNaN)(v) ? null : v;
-    if (!(isFilter && item[dfFilterName] !== two)) {
-      d.push({
-        x: _crUTC(item),
-        y: y
-      });
+    two = (0, _AdapterFn.getValue)(items[1]),
+    isFilter = dfFilterName ? true : false;
+  return (getResultsData(Results) || []).reduce((data, item) => {
+    if ((0, _AdapterFn.isObj)(item)) {
+      const v = parseFloat(item.DataValue),
+        dateMls = _crUTC(item);
+      if ((0, _AdapterFn.isNumber)(dateMls) && !(isFilter && item[dfFilterName] !== two)) {
+        data.push({
+          x: dateMls,
+          y: (0, _AdapterFn.isNaN)(v) ? null : v
+        });
+      }
     }
-  });
-  return d;
+    return data;
+  }, []);
 };
 exports.crData = crData;
-const crConfOption = (option, json) => {
-  const Results = _getResults(json);
-  return {
-    zhConfig: _crZhConfig(option),
-    info: _crInfo(Results)
-  };
-};
+const crConfOption = (option, json) => ({
+  zhConfig: _crZhConfig(option),
+  info: _crInfo(getResults(json))
+});
 exports.crConfOption = crConfOption;
 //# sourceMappingURL=fnAdapter.js.map
--- src/adapters/bea/fnAdapter.js
+++ src/adapters/bea/fnAdapter.js
@@ -1,105 +1,126 @@
 import {
   isNaN,
   isArr,
+  isObj,
+  isNumber,
+  getByPropsFrom,
+  getValue,
+  getCaption,
+  crDfLink,
+  joinBy,
   ymdToUTC
 } from '../AdapterFn';
 
-const _getResults = json => json.BEAAPI.Results
-, _getData = Results => isArr(Results)
-    ? Results[0].Data
-    : Results.Data
-, _getInfo = Results => isArr(Results)
-    ? Results[0]
-    : Results;
+export const BEA_DATA_URL = "https://apps.bea.gov";
+const INDUSTRY_FACTSHEET_URL = `${BEA_DATA_URL}/industry/factsheet/factsheet.html`
 
-const _crName = info => {
-  const {
-    Statistic='',
-    UTCProductionTime=''
-  } = info
-  , t = UTCProductionTime.replace('T', ' ');
-  return `${Statistic}: ${t}`;
-};
+export const getFrequency = (
+  item
+) => getCaption(item, "").indexOf("(A,Q)") === -1
+  ? "A"
+  : "Q"
 
-const _crDescr = info => {
-  const { Notes=[] } = info
-  , arr = Notes.map(note => {
-      const { NoteRef='', NoteText='' } = note;
-      return `<P>${NoteRef}: ${NoteText}</P><BR/>`;
-  });
-  return arr.join('');
-};
+const _getBeaapi = json => (json || {}).BEAAPI || {};
+
+export const getResults = json => _getBeaapi(json).Results
+, getResError = json => _getBeaapi(json).Error
+, getResultsData = Results => getByPropsFrom(Results,
+  ...isArr(Results)
+    ? [0, "Data"]
+    : ["Data"]
+)
+
+const _getResultsInfo = Results => isArr(Results)
+  ? Results[0]
+  : Results;
+
+const _crName = info => joinBy(": ",
+  info.Statistic,
+  (info.UTCProductionTime || "").replace("T", " ")
+);
+
+const _crDescr = info => (info.Notes || [])
+  .map(note => {
+    const { NoteRef='', NoteText='' } = note || {};
+    return `<p>${NoteRef}: ${NoteText}</p><br/>`;
+  }).join('');
+
+
 const _crInfo = (Results) => {
-  const _info = _getInfo(Results);
+  const _info = _getResultsInfo(Results) || {};
   return {
     name: _crName(_info),
     description: _crDescr(_info)
   }
 };
 
+const _crDfLinkCondig = (
+  ValueName,
+  industryItem
+) => ValueName === "Industry" && getFrequency(industryItem) === "Q"
+  ? crDfLink(`BEA ${getCaption(industryItem)}`, `${INDUSTRY_FACTSHEET_URL}#${getValue(industryItem)}`)
+  : void 0;
+
 const _crZhConfig = ({
+  ValueName,
+  items,
   _itemKey,
   itemCaption,
   dataSource
 }) => ({
-  id: _itemKey,
-  key: _itemKey,
-  itemCaption,
-  dataSource
-});
+   id: _itemKey,
+   key: _itemKey,
+   itemCaption,
+   dataSource,
+   ..._crDfLinkCondig(ValueName, items[0])
+})
 
 const MD = {
-  DF: '-12-31',
-  'I': '-03-31',
-  'II': '-06-30',
-  'III': '-09-30'
+  DF: "-12-31",
+  I: "-03-31",
+  II: "-06-30",
+  III: "-09-30"
 };
 
 const _crUTC = (item) => {
-  const {
-    Frequency,
-    Year,
-    Quarter
-  } = item
-  , md = Frequency === 'Q'
-     ? MD[Quarter] || MD.DF
-     : MD.DF;
-  return ymdToUTC(Year + md);
+  const md = item.Frequency === "Q"
+    ? MD[item.Quarter] || MD.DF
+    : MD.DF;
+  return ymdToUTC(item.Year + md);
 };
-
 
 export const crData = (
   json,
   option
 ) => {
-  const Results = _getResults(json)
-  , { dfFilterName, items } = option
-  , two = (items[1] || {}).value
-  , d = []
-  , isFilter = dfFilterName ? true : false
-  , data = _getData(Results) || [];
+  const Results = getResults(json)
+  , {
+    dfFilterName,
+    items
+  } = option
+  , two = getValue(items[1])
+  , isFilter = dfFilterName ? true : false;
 
-  data.forEach(item => {
-    const v = parseFloat(item.DataValue)
-        , y = isNaN(v) ? null : v;
-    if ( !(isFilter && item[dfFilterName] !== two) ) {
-      d.push({
-        x: _crUTC(item),
-        y: y
-      })
-    }
-  })
-
-  return d;
+  return  (getResultsData(Results) || [])
+    .reduce((data, item) => {
+       if (isObj(item)) {
+         const v = parseFloat(item.DataValue)
+         , dateMls =  _crUTC(item);
+         if ( isNumber(dateMls) && !(isFilter && item[dfFilterName] !== two) ) {
+           data.push({
+             x: dateMls,
+             y: isNaN(v) ? null : v
+           })
+         }
+       }
+       return data;
+   }, []);
 }
 
 export const crConfOption = (
   option,
   json
-) => {
-  const Results = _getResults(json);
-  return {
-    zhConfig: _crZhConfig(option),
-    info: _crInfo(Results)
-  };
-}
+) => ({
+  zhConfig: _crZhConfig(option),
+  info: _crInfo(getResults(json))
+})
