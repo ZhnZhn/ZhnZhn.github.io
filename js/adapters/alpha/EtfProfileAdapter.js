e1cb3ff1a5c8a030e401be8d0be02c9f1ea4c1d3
Yevhenii Molodyi <eugmolody@gmail.com>
2024-11-07 10:30:02 +0200

    Improve Alpha Vantage EtfProfileAdapter

--- js/adapters/alpha/EtfProfileAdapter.js
+++ js/adapters/alpha/EtfProfileAdapter.js
@@ -7,6 +7,9 @@ var _crAdapterCategory = _interopRequireDefault(require("../crAdapterCategory"))
 var _AdapterFn = require("../AdapterFn");
 var _CategoryFn = require("../CategoryFn");
 var _compareByFn = require("../compareByFn");
+const DOMESTIC_EQUITIES = "domestic_equities";
+const FOREIGN_EQUITIES = "foreign_equities";
+const FOREIGN_EQUITIES_SHORT_FORM = "For. Eq.";
 const _getByProps = (json, propName) => (json || {})[propName] || "n/a";
 const crItemCaption = (_ref, json) => {
   let {
@@ -16,14 +19,21 @@ const crItemCaption = (_ref, json) => {
 };
 const crData = (json, option) => {
   const {
-      holdings
+      holdings,
+      asset_allocation
     } = json || {},
-    data = (0, _compareByFn.sortDescCategory)(holdings.map(item => {
-      return (0, _CategoryFn.crCategoryPoint)(parseFloat(item.weight), item.symbol);
-    }, []));
+    data = (holdings || {}).map(item => (0, _CategoryFn.crCategoryPoint)(parseFloat(item.weight), item.symbol), []);
+  (0, _AdapterFn.getObjectKeys)(asset_allocation).forEach(key => {
+    if (key !== DOMESTIC_EQUITIES) {
+      const value = parseFloat(asset_allocation[key]);
+      if ((0, _AdapterFn.isPositiveNumber)(value)) {
+        data.push((0, _CategoryFn.crCategoryPoint)(value, key === FOREIGN_EQUITIES ? FOREIGN_EQUITIES_SHORT_FORM : (0, _AdapterFn.toUpperCaseFirst)(key)));
+      }
+    }
+  });
   option.subtitle = `${option.itemCaption}, Net Expense Ratio ${_getByProps(json, "net_expense_ratio")}`;
   option.title = `Portfolio Turnover ${_getByProps(json, "portfolio_turnover")}, Dividend Yield ${_getByProps(json, "dividend_yield")}`;
-  return data;
+  return (0, _compareByFn.sortDescCategory)(data);
 };
 const EtfProfileAdapter = (0, _crAdapterCategory.default)(crData, crItemCaption);
 var _default = exports.default = EtfProfileAdapter;
--- src/adapters/alpha/EtfProfileAdapter.js
+++ src/adapters/alpha/EtfProfileAdapter.js
@@ -1,33 +1,56 @@
 import crAdapterCategory from '../crAdapterCategory';
 import {
+  isPositiveNumber,
   numberFormat,
-  crShortItemCaption
+  toUpperCaseFirst,
+  crShortItemCaption,
+  getObjectKeys
 } from '../AdapterFn';
 import { crCategoryPoint } from '../CategoryFn';
 import { sortDescCategory } from '../compareByFn';
 
+const DOMESTIC_EQUITIES = "domestic_equities";
+const FOREIGN_EQUITIES = "foreign_equities";
+const FOREIGN_EQUITIES_SHORT_FORM = "For. Eq.";
+
 const _getByProps = (
   json,
   propName
-) => (json|| {})[propName] || "n/a";
+) => (json || {})[propName] || "n/a";
 
 const crItemCaption = ({
   itemCaption
 }, json) => `${crShortItemCaption(itemCaption)} ${numberFormat(_getByProps(json, "net_assets"), "")}`;
 
 const crData = (json, option) => {
-  const { holdings } = json || {}
-  , data = sortDescCategory(holdings.map(item => {
-    return crCategoryPoint(
+  const {
+    holdings,
+    asset_allocation
+  } = json || {}
+  , data = (holdings || {}).map(item => crCategoryPoint(
       parseFloat(item.weight),
       item.symbol
-    );
-  }, []));
+  ), []);
+
+  getObjectKeys(asset_allocation)
+    .forEach(key => {
+       if (key !== DOMESTIC_EQUITIES) {
+         const value = parseFloat(asset_allocation[key]);
+         if (isPositiveNumber(value)) {
+           data.push(crCategoryPoint(
+             value,
+             key === FOREIGN_EQUITIES
+               ? FOREIGN_EQUITIES_SHORT_FORM
+               : toUpperCaseFirst(key)
+           ))
+         }
+       }
+    })
 
   option.subtitle = `${option.itemCaption}, Net Expense Ratio ${_getByProps(json, "net_expense_ratio")}`
   option.title = `Portfolio Turnover ${_getByProps(json, "portfolio_turnover")}, Dividend Yield ${_getByProps(json, "dividend_yield")}`
 
-  return data;
+  return sortDescCategory(data);
 }
 const EtfProfileAdapter = crAdapterCategory(
   crData,
