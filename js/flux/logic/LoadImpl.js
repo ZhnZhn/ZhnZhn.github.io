eef26d39ef7463f07c0f2369be7336d99fe8da52
Yevhenii Molodyi <eugmolody@gmail.com>
2024-11-18 12:08:01 +0200

    Add function addLoadImpl named export to flux logic LoadImpl

--- js/flux/logic/LoadImpl.js
+++ js/flux/logic/LoadImpl.js
@@ -2,16 +2,22 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 exports.__esModule = true;
-exports.getLoadImpl = void 0;
+exports.getLoadImpl = exports.addLoadImpl = void 0;
 var _RouterAdapter = _interopRequireDefault(require("../../adapters/RouterAdapter"));
 var _loadItem = _interopRequireDefault(require("./loadItem"));
 const _hmLoadImpl = Object.create(null);
+const addLoadImpl = (key, adapter) => {
+  if (!_hmLoadImpl[key]) {
+    _hmLoadImpl[key] = (0, _loadItem.default)(adapter);
+  }
+};
+exports.addLoadImpl = addLoadImpl;
+const getLoadImpl = loadId => _hmLoadImpl[loadId];
+exports.getLoadImpl = getLoadImpl;
 const _initHmLoadImpl = () => {
   for (let key in _RouterAdapter.default) {
-    _hmLoadImpl[key] = (0, _loadItem.default)(_RouterAdapter.default[key]);
+    addLoadImpl(key, _RouterAdapter.default[key]);
   }
 };
 _initHmLoadImpl();
-const getLoadImpl = loadId => _hmLoadImpl[loadId];
-exports.getLoadImpl = getLoadImpl;
 //# sourceMappingURL=LoadImpl.js.map
--- src/flux/logic/LoadImpl.js
+++ src/flux/logic/LoadImpl.js
@@ -2,11 +2,19 @@ import Adapter from '../../adapters/RouterAdapter';
 import fLoadItem from './loadItem';
 
 const _hmLoadImpl = Object.create(null);
+export const addLoadImpl = (
+  key,
+  adapter
+) => {
+  if (!_hmLoadImpl[key]) {
+    _hmLoadImpl[key] = fLoadItem(adapter)
+  }
+}
+export const getLoadImpl = loadId => _hmLoadImpl[loadId]
+
 const _initHmLoadImpl = () => {
   for (let key in Adapter) {
-    _hmLoadImpl[key] = fLoadItem(Adapter[key])
+    addLoadImpl(key, Adapter[key])
   }
 };
 _initHmLoadImpl()
-
-export const getLoadImpl = loadId => _hmLoadImpl[loadId]
