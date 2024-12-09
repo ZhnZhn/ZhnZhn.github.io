d342e85f8499aa2492dfbf394c8751a5a10e0edd
Yevhenii Molodyi <eugmolody@gmail.com>
2024-12-09 16:22:19 +0200

    Add NDL toLineAdapter

---
+++ js/adapters/ndl/toLineAdapter.js
@@ -0,0 +1,26 @@
+"use strict";
+
+exports.__esModule = true;
+exports.toLineAdapter = void 0;
+var _AdapterFn = require("../AdapterFn");
+var _compareByFn = require("../compareByFn");
+var _crAdapterType = require("../crAdapterType1");
+var _NdlFn = require("./NdlFn");
+const crData = (json, option) => {
+  const points = (0, _NdlFn.getData)(json).sort(_compareByFn.compareByDate),
+    data = [];
+  for (const point of points) {
+    const value = point[1];
+    if ((0, _AdapterFn.isNumber)(value)) {
+      data.push([(0, _AdapterFn.ymdToUTC)(point[0]), value]);
+    }
+  }
+  return data;
+};
+const toLineAdapter = exports.toLineAdapter = (0, _crAdapterType.crAdapterType1)({
+  crData,
+  crConfOption: option => ({
+    zhConfig: (0, _NdlFn.crZhConfig)(option)
+  })
+});
+//# sourceMappingURL=toLineAdapter.js.map
---
+++ src/adapters/ndl/toLineAdapter.js
@@ -0,0 +1,35 @@
+import {
+  isNumber,
+  ymdToUTC
+} from "../AdapterFn";
+import { compareByDate } from "../compareByFn";
+import { crAdapterType1 } from "../crAdapterType1";
+
+import {
+  getData,
+  crZhConfig
+} from "./NdlFn";
+
+const crData = (
+  json,
+  option
+) => {
+  const points = getData(json).sort(compareByDate)
+  , data = [];
+
+  for (const point of points) {
+    const value = point[1];
+    if (isNumber(value)) {
+      data.push([ymdToUTC(point[0]), value]);
+    }
+  }
+
+  return data;
+};
+
+export const toLineAdapter = crAdapterType1({
+  crData,
+  crConfOption: option => ({
+    zhConfig: crZhConfig(option)
+  })
+})
