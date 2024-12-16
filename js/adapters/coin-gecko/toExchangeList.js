8fd6ff49287c59f034f17ec36f12a434b8acfed9
Yevhenii Molodyi <eugmolody@gmail.com>
2024-12-16 15:18:31 +0200

    Add function crStyleCenter from toTableFn to CoinGecko toExchangeList adapter

--- js/adapters/coin-gecko/toExchangeList.js
+++ js/adapters/coin-gecko/toExchangeList.js
@@ -6,36 +6,32 @@ var _toTableFn = require("../toTableFn");
 var _fnAdapter = require("./fnAdapter");
 const _crNumberCenterProps = () => ({
     toN: [],
-    style: {
-      textAlign: 'center'
-    }
+    ...(0, _toTableFn.crStyleCenter)()
   }),
-  PN_TRADE_VOLUME_24H_BTC = 'trade_volume_24h_btc',
+  PN_TRADE_VOLUME_24H_BTC = "trade_volume_24h_btc",
   PN_TRADE_VOLUME_24H_BTC_NORM = `${PN_TRADE_VOLUME_24H_BTC}_normalized`,
   HEADERS = [{
-    ...(0, _toTableFn.crNameProps)('Trust Rank', 'trust_score_rank'),
+    ...(0, _toTableFn.crNameProps)("Trust Rank", "trust_score_rank"),
     ..._crNumberCenterProps()
-  }, (0, _toTableFn.crNameProps)('Name'), {
-    ...(0, _toTableFn.crNameProps)('24h BTC', PN_TRADE_VOLUME_24H_BTC),
+  }, (0, _toTableFn.crNameProps)("Name"), {
+    ...(0, _toTableFn.crNameProps)("24h BTC", PN_TRADE_VOLUME_24H_BTC),
     ...(0, _toTableFn.crNumberProps)(0)
   }, {
-    ...(0, _toTableFn.crNameProps)('24h BTC Norm.', PN_TRADE_VOLUME_24H_BTC_NORM),
+    ...(0, _toTableFn.crNameProps)("24h BTC Norm.", PN_TRADE_VOLUME_24H_BTC_NORM),
     ...(0, _toTableFn.crNumberProps)(0)
   }, {
-    ...(0, _toTableFn.crNameProps)('Trust Score', 'trust_score'),
+    ...(0, _toTableFn.crNameProps)("Trust Score", "trust_score"),
     ..._crNumberCenterProps()
   }, {
-    ...(0, _toTableFn.crNameProps)('Year Estb.', 'year_established', true),
-    style: {
-      textAlign: 'center'
-    }
-  }, (0, _toTableFn.crNameProps)('Country', true), {
-    ...(0, _toTableFn.crNameProps)('Link', 'url', true),
+    ...(0, _toTableFn.crNameProps)("Year Estb.", "year_established", true),
+    ...(0, _toTableFn.crStyleCenter)()
+  }, (0, _toTableFn.crNameProps)("Country", true), {
+    ...(0, _toTableFn.crNameProps)("Link", "url", true),
     isHref: true
   }];
 const toExchangeList = {
   crKey(option) {
-    option.key = (0, _fnAdapter.crPageConfig)(option).join('_');
+    option.key = (0, _fnAdapter.crPageConfig)(option).join("_");
     return option.key;
   },
   toConfig(json, option) {
--- src/adapters/coin-gecko/toExchangeList.js
+++ src/adapters/coin-gecko/toExchangeList.js
@@ -1,47 +1,48 @@
 import {
+  crStyleCenter,
   crNameProps,
   crNumberProps,
   crTableRows,
   crTableConfig
-} from '../toTableFn';
+} from "../toTableFn";
 import {
   crPageConfig,
   getYmdhmUTC
-} from './fnAdapter';
+} from "./fnAdapter";
 
 const _crNumberCenterProps = () => ({
   toN: [],
-  style: { textAlign: 'center' }
+  ...crStyleCenter()
 })
-, PN_TRADE_VOLUME_24H_BTC = 'trade_volume_24h_btc'
+, PN_TRADE_VOLUME_24H_BTC = "trade_volume_24h_btc"
 , PN_TRADE_VOLUME_24H_BTC_NORM = `${PN_TRADE_VOLUME_24H_BTC}_normalized`
 , HEADERS = [{
-  ...crNameProps('Trust Rank', 'trust_score_rank'),
+  ...crNameProps("Trust Rank", "trust_score_rank"),
   ..._crNumberCenterProps()
 }
-, crNameProps('Name')
+, crNameProps("Name")
 , {
-  ...crNameProps('24h BTC', PN_TRADE_VOLUME_24H_BTC),
+  ...crNameProps("24h BTC", PN_TRADE_VOLUME_24H_BTC),
   ...crNumberProps(0)
 },{
-  ...crNameProps('24h BTC Norm.', PN_TRADE_VOLUME_24H_BTC_NORM),
+  ...crNameProps("24h BTC Norm.", PN_TRADE_VOLUME_24H_BTC_NORM),
   ...crNumberProps(0)
 },{
-  ...crNameProps('Trust Score', 'trust_score'),
+  ...crNameProps("Trust Score", "trust_score"),
   ..._crNumberCenterProps()
 },{
-  ...crNameProps('Year Estb.', 'year_established', true),
-  style: { textAlign: 'center' }
+  ...crNameProps("Year Estb.", "year_established", true),
+  ...crStyleCenter()
 }
-, crNameProps('Country', true)
+, crNameProps("Country", true)
 , {
-  ...crNameProps('Link', 'url', true),
+  ...crNameProps("Link", "url", true),
   isHref: true
 }];
 
 const toExchangeList = {
   crKey(option){
-    option.key = crPageConfig(option).join('_');
+    option.key = crPageConfig(option).join("_");
     return option.key;
   },
 
