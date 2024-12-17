526c30bdf3f4756197a76d6fe1125125a2fc37d9
Yevhenii Molodyi <eugmolody@gmail.com>
2024-12-17 10:58:50 +0200

    Add crTableFlatHeaders from toTableFn to CoinGecko toMarketCapList data adapter

--- js/adapters/coin-gecko/toMarketCapList.js
+++ js/adapters/coin-gecko/toMarketCapList.js
@@ -12,28 +12,29 @@ const _crPriceChangeItem = (name, pnSuffix, isHide) => ({
 });
 const _crStyleItem = (name, pn, options) => ({
   ...(0, _toTableFn.crNameProps)(name, pn),
+  ...(0, _toTableFn.crStyleBold)(),
   toN: [],
-  style: {
-    fontWeight: 'bold'
-  },
   ...options
 });
-const _getTableHeaders = (0, _AdapterFn.fCrLazyValue)(() => [{
-  ...(0, _toTableFn.crNameProps)('Rank', 'market_cap_rank'),
-  toN: [],
-  style: {
-    textAlign: 'center'
-  }
-}, (0, _toTableFn.crNameProps)('Name'), {
-  ...(0, _toTableFn.crNameProps)('Symbol', true),
-  style: {
-    textTransform: 'uppercase',
-    fontWeight: 'bold'
-  }
-}, _crPriceChangeItem('1h %', '1h_in_currency'), _crPriceChangeItem('24h %', '24h'), _crPriceChangeItem('7d %', '7d_in_currency'), _crPriceChangeItem('30d %', '30d_in_currency', true), _crPriceChangeItem('1y %', '1y_in_currency', true), _crStyleItem('Price', 'current_price'), _crStyleItem('MarketCap', 'market_cap', {
-  isF: true
-}), (0, _toTableFn.crNameProps)('Updated UTC', 'last_updated', true)]);
-const _toDate = rowDate => (rowDate || '').replace('T', ' ').split('.')[0];
+const _getTableHeaders = (0, _AdapterFn.fCrLazyValue)(() => {
+  const headers = [{
+    ...(0, _toTableFn.crStyleCenter)(),
+    ...(0, _toTableFn.crNameProps)("Rank", "market_cap_rank"),
+    toN: []
+  }, (0, _toTableFn.crNameProps)("Name"), {
+    ...(0, _toTableFn.crNameProps)("Symbol", true),
+    ...(0, _toTableFn.crStyleBold)({
+      textTransform: "uppercase"
+    })
+  }, {
+    caption: "Price Change %",
+    items: [_crPriceChangeItem("1h %", "1h_in_currency"), _crPriceChangeItem("24h %", "24h"), _crPriceChangeItem("7d %", "7d_in_currency"), _crPriceChangeItem("30d %", "30d_in_currency", true), _crPriceChangeItem("1y %", "1y_in_currency", true)]
+  }, _crStyleItem("Price", "current_price"), _crStyleItem("MarketCap", "market_cap", {
+    isF: true
+  }), (0, _toTableFn.crNameProps)("Updated UTC", "last_updated", true)];
+  return [headers, (0, _toTableFn.crTableFlatHeaders)(headers)];
+});
+const _toDate = rowDate => (rowDate || "").replace("T", " ").split(".")[0];
 const _transformDate = json => json.map(item => {
   item.last_updated = _toDate(item.last_updated);
   return item;
@@ -47,17 +48,14 @@ const toMarketCapList = {
     return option.key;
   },
   toConfig(json, option) {
-    const {
-        key,
-        title
-      } = option,
-      _json = _transformDate(json),
-      headers = _getTableHeaders(),
-      rows = (0, _toTableFn.crTableRows)(headers, _json),
+    const _json = _transformDate(json),
+      [headers, flatHeaders] = _getTableHeaders(),
+      rows = (0, _toTableFn.crTableRows)(flatHeaders, _json),
       config = (0, _toTableFn.crTableConfig)({
-        id: key,
-        title,
+        id: option.key,
+        title: option.title,
         headers,
+        flatHeaders,
         rows,
         dataSource: _crDataSource(rows)
       });
--- src/adapters/coin-gecko/toMarketCapList.js
+++ src/adapters/coin-gecko/toMarketCapList.js
@@ -1,11 +1,14 @@
-import { fCrLazyValue } from '../AdapterFn';
+import { fCrLazyValue } from "../AdapterFn";
 
 import {
+  crStyleBold,
+  crStyleCenter,
   crNameProps,
+  crTableFlatHeaders,
   crTableRows,
   crTableConfig
-} from '../toTableFn';
-import { crPageConfig } from './fnAdapter';
+} from "../toTableFn";
+import { crPageConfig } from "./fnAdapter";
 
 const _crPriceChangeItem = (
   name,
@@ -22,43 +25,45 @@ const _crPriceChangeItem = (
 });
 const _crStyleItem = (name, pn, options) => ({
   ...crNameProps(name, pn),
+  ...crStyleBold(),
   toN: [],
-  style: {
-    fontWeight: 'bold'
-  },
   ...options
 });
 
-const _getTableHeaders = fCrLazyValue(() => [
-  {
-    ...crNameProps('Rank', 'market_cap_rank'),
-    toN: [],
-    style: { textAlign: 'center' }
-  },
-  crNameProps('Name'),
-  {
-    ...crNameProps('Symbol', true),
-    style: {
-      textTransform: 'uppercase',
-      fontWeight: 'bold'
-    }
-  },
+const _getTableHeaders = fCrLazyValue(() => {
+  const headers = [
+    {
+      ...crStyleCenter(),
+      ...crNameProps("Rank", "market_cap_rank"),
+      toN: []
+    },
+    crNameProps("Name"),
+    {
+      ...crNameProps("Symbol", true),
+      ...crStyleBold({ textTransform: "uppercase" })
+    },
+    { caption: "Price Change %", items: [
+       _crPriceChangeItem("1h %", "1h_in_currency"),
+       _crPriceChangeItem("24h %", "24h"),
+       _crPriceChangeItem("7d %", "7d_in_currency"),
+       _crPriceChangeItem("30d %", "30d_in_currency", true),
+       _crPriceChangeItem("1y %", "1y_in_currency", true)
+     ]
+    },
+    _crStyleItem("Price", "current_price"),
+    _crStyleItem("MarketCap", "market_cap", { isF: true }),
+    crNameProps("Updated UTC", "last_updated", true)
+  ];
+  return [
+    headers,
+    crTableFlatHeaders(headers)
+  ];
+});
 
-  _crPriceChangeItem('1h %', '1h_in_currency'),
-  _crPriceChangeItem('24h %', '24h'),
-  _crPriceChangeItem('7d %', '7d_in_currency'),
-  _crPriceChangeItem('30d %', '30d_in_currency', true),
-  _crPriceChangeItem('1y %', '1y_in_currency', true),
 
-  _crStyleItem('Price', 'current_price'),
-  _crStyleItem('MarketCap', 'market_cap', {isF: true}),
-  crNameProps('Updated UTC', 'last_updated', true)
-]);
-
-
-const _toDate = rowDate => (rowDate || '')
-  .replace('T', ' ')
-  .split('.')[0];
+const _toDate = rowDate => (rowDate || "")
+  .replace("T", " ")
+  .split(".")[0];
 
 const _transformDate = json => json
  .map(item => {
@@ -78,13 +83,16 @@ const toMarketCapList = {
   },
 
   toConfig(json, option){
-    const { key, title } = option
-    , _json = _transformDate(json)
-    , headers = _getTableHeaders()
-    , rows = crTableRows(headers, _json)
+    const _json = _transformDate(json)
+    , [
+      headers,
+      flatHeaders
+    ] = _getTableHeaders()
+    , rows = crTableRows(flatHeaders, _json)
     , config = crTableConfig({
-      id: key, title,
-      headers, rows,
+      id: option.key,
+      title: option.title,
+      headers, flatHeaders, rows,
       dataSource: _crDataSource(rows)
     });
     return { config };
