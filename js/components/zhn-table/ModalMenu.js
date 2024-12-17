01958462828950919d8616661f2504d21f0d04c3
Yevhenii Molodyi <eugmolody@gmail.com>
2024-12-17 10:35:05 +0200

    Add OpenClose function component to zhn-table ModalMenu function component

--- js/components/zhn-table/ModalMenu.js
+++ js/components/zhn-table/ModalMenu.js
@@ -3,6 +3,9 @@
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 exports.__esModule = true;
 exports.default = void 0;
+var _uiApi = require("../uiApi");
+var _styleFn = require("../styleFn");
+var _OpenClose = _interopRequireDefault(require("../zhn/OpenClose"));
 var _ModalPopup = _interopRequireDefault(require("../zhn-moleculs/ModalPopup"));
 var _RowCheckBox = _interopRequireDefault(require("../dialogs/rows/RowCheckBox3"));
 var _Style = require("./Style");
@@ -15,21 +18,46 @@ const S_ROW = {
     borderColor: (0, _Style.getColorBlack)(),
     marginTop: 2,
     marginBottom: 2
+  }),
+  S_OPEN_CLOSE = {
+    lineHeight: 1.5
+  };
+const _crRowCheckBoxElement = (h, color, onToggle) => /*#__PURE__*/(0, _jsxRuntime.jsx)(_RowCheckBox.default, {
+  style: S_ROW,
+  color: color,
+  caption: h.name,
+  value: !h.isHide,
+  onToggle: onToggle
+}, h.name);
+const MenuPart = _ref => {
+  let {
+    item,
+    color,
+    onToggle
+  } = _ref;
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)(_OpenClose.default, {
+    className: _styleFn.CL_OPEN_CLOSE_BLACK,
+    childStyle: S_OPEN_CLOSE,
+    caption: item.caption,
+    children: (0, _uiApi.safeMap)(item.items, h => _crRowCheckBoxElement(h, color, () => onToggle(h.id)))
   });
-const _renderHeaders = (headers, onToggle) => {
+};
+const MenuItems = _ref2 => {
+  let {
+    headers,
+    onToggle
+  } = _ref2;
   /*eslint-disable no-unused-vars*/
   const [rank, ...restHeader] = headers
     /*eslint-enable no-unused-vars*/,
     _colorBlack = (0, _Style.getColorBlack)();
-  return restHeader.map((h, index) => /*#__PURE__*/(0, _jsxRuntime.jsx)(_RowCheckBox.default, {
-    style: S_ROW,
+  return (0, _uiApi.safeMap)(restHeader, (h, index) => (0, _uiApi.isArr)(h.items) ? /*#__PURE__*/(0, _jsxRuntime.jsx)(MenuPart, {
+    item: h,
     color: _colorBlack,
-    caption: h.name,
-    value: !h.isHide,
-    onToggle: () => onToggle(index + 1)
-  }, h.name));
+    onToggle: onToggle
+  }, h.caption) : _crRowCheckBoxElement(h, _colorBlack, () => onToggle(h.id || index + 1)));
 };
-const ModalMenu = _ref => {
+const ModalMenu = _ref3 => {
   let {
     isShow,
     isGridLine,
@@ -38,7 +66,7 @@ const ModalMenu = _ref => {
     onToggleGrid,
     onToggle,
     onClose
-  } = _ref;
+  } = _ref3;
   return /*#__PURE__*/(0, _jsxRuntime.jsxs)(_ModalPopup.default, {
     style: style,
     isShow: isShow,
@@ -51,7 +79,10 @@ const ModalMenu = _ref => {
       onToggle: onToggleGrid
     }), /*#__PURE__*/(0, _jsxRuntime.jsx)("hr", {
       style: _crHrStyle()
-    }), _renderHeaders(headers, onToggle)]
+    }), /*#__PURE__*/(0, _jsxRuntime.jsx)(MenuItems, {
+      headers: headers,
+      onToggle: onToggle
+    })]
   });
 };
 var _default = exports.default = ModalMenu;
--- src/components/zhn-table/ModalMenu.jsx
+++ src/components/zhn-table/ModalMenu.jsx
@@ -1,6 +1,17 @@
-import ModalPopup from '../zhn-moleculs/ModalPopup';
-import RowCheckBox3 from '../dialogs/rows/RowCheckBox3';
-import { getColorBlack } from './Style';
+import {
+  isArr,
+  safeMap
+} from "../uiApi";
+
+import {
+  CL_OPEN_CLOSE_BLACK
+} from "../styleFn";
+
+import OpenClose from "../zhn/OpenClose";
+import ModalPopup from "../zhn-moleculs/ModalPopup";
+import RowCheckBox3 from "../dialogs/rows/RowCheckBox3";
+
+import { getColorBlack } from "./Style";
 
 const S_ROW = {
   paddingLeft: 0,
@@ -10,27 +21,65 @@ const S_ROW = {
   borderColor: getColorBlack(),
   marginTop: 2,
   marginBottom: 2
-});
+})
+, S_OPEN_CLOSE = {
+  lineHeight: 1.5
+};
 
-const _renderHeaders = (
+const _crRowCheckBoxElement = (
+  h,
+  color,
+  onToggle
+) => (
+  <RowCheckBox3
+    key={h.name}
+    style={S_ROW}
+    color={color}
+    caption={h.name}
+    value={!h.isHide}
+    onToggle={onToggle}
+  />
+)
+
+const MenuPart = ({
+  item,
+  color,
+  onToggle
+}) => (
+  <OpenClose
+    className={CL_OPEN_CLOSE_BLACK}
+    childStyle={S_OPEN_CLOSE}
+    caption={item.caption}
+  >
+    {safeMap(
+      item.items,
+      h => _crRowCheckBoxElement(h, color, () => onToggle(h.id))
+    )}
+  </OpenClose>
+);
+
+
+const MenuItems = ({
   headers,
   onToggle
-) => {
+}) => {
   /*eslint-disable no-unused-vars*/
   const [rank, ...restHeader] = headers
   /*eslint-enable no-unused-vars*/
   , _colorBlack = getColorBlack();
-  return restHeader.map((h, index) => (
-    <RowCheckBox3
-      key={h.name}
-      style={S_ROW}
-      color={_colorBlack}
-      caption={h.name}
-      value={!h.isHide}
-      onToggle={() => onToggle(index+1)}
-    />
-  ))
-}
+  return safeMap(restHeader, (h, index) => isArr(h.items)
+    ? (<MenuPart
+         key={h.caption}
+         item={h}
+         color={_colorBlack}
+         onToggle={onToggle}
+       />)
+    : _crRowCheckBoxElement(
+         h, _colorBlack,
+         () => onToggle(h.id || (index+1))
+      )
+  );
+};
 
 const ModalMenu = ({
   isShow,
@@ -54,9 +103,11 @@ const ModalMenu = ({
        onToggle={onToggleGrid}
      />
      <hr style={_crHrStyle()} />
-     {_renderHeaders(headers, onToggle)}
+     <MenuItems
+       headers={headers}
+       onToggle={onToggle}
+     />
   </ModalPopup>
 );
-
 
 export default ModalMenu
