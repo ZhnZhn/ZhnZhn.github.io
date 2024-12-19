2d46876f79b7fbddcfd547be8d0c8d3a8cec15ae
Yevhenii Molodyi <eugmolody@gmail.com>
2024-12-19 11:03:52 +0200

    Add unit tests for isCategory function from adapters CategoryFn

--- js/adapters/__tests__/CategoryFn.test.js
+++ js/adapters/__tests__/CategoryFn.test.js
@@ -1,25 +1,63 @@
 "use strict";
 
+var _ChartType = require("../../constants/ChartType");
 var _CategoryFn = require("../CategoryFn");
-
-describe('arrangeSeriaByCategories', () => {
+describe("isCategory", () => {
+  const fn = _CategoryFn.isCategory;
+  test("should return boolean from string", () => {
+    expect(fn(_ChartType.CHT_BAR_SET)).toBe(true);
+    expect(fn(_ChartType.CHT_BAR_CLUSTER)).toBe(true);
+    expect(fn(_ChartType.CHT_COLUMN_SET)).toBe(true);
+    expect(fn(_ChartType.CHT_COLUMN_CLUSTER)).toBe(true);
+    expect(fn(_ChartType.CHT_TREE_MAP)).toBe(true);
+    expect(fn(_ChartType.CHT_TREE_MAP_CLUSTER)).toBe(true);
+    expect(fn(_ChartType.CHT_SPLINE)).toBe(false);
+    expect(fn(_ChartType.CHT_LINE)).toBe(false);
+    expect(fn(_ChartType.CHT_AREA)).toBe(false);
+    expect(fn(_ChartType.CHT_COLUMN)).toBe(false);
+  });
+  test("should return boolean from object with property seriaType", () => {
+    expect(fn({
+      seriaType: _ChartType.CHT_COLUMN_SET
+    })).toBe(true);
+    expect(fn({
+      seriaType: _ChartType.CHT_COLUMN_CLUSTER
+    })).toBe(true);
+    expect(fn({
+      seriaType: _ChartType.CHT_SPLINE
+    })).toBe(false);
+    expect(fn({
+      seriaType: _ChartType.CHT_LINE
+    })).toBe(false);
+  });
+  test("should return false for edge cases", () => {
+    expect(fn()).toBe(false);
+    expect(fn({})).toBe(false);
+    expect(fn([])).toBe(false);
+    expect(fn(null)).toBe(false);
+    expect(fn(0)).toBe(false);
+    expect(fn(NaN)).toBe(false);
+    expect(fn(true)).toBe(false);
+  });
+});
+describe("arrangeSeriaByCategories", () => {
   const fn = _CategoryFn.arrangeSeriaByCategories;
-  test('should arrange seria data by categories', () => {
-    const categories = ['a1', 'a2', 'a3'],
-          seria = {
-      data: [{
-        'c': 'a3'
-      }, {
-        c: 'a4'
-      }, {
-        c: 'a1'
-      }]
-    };
+  test("should arrange seria data by categories", () => {
+    const categories = ["a1", "a2", "a3"],
+      seria = {
+        data: [{
+          "c": "a3"
+        }, {
+          c: "a4"
+        }, {
+          c: "a1"
+        }]
+      };
     expect(fn(seria, categories)).toEqual({
       data: [{
-        c: 'a1'
+        c: "a1"
       }, null, {
-        c: 'a3'
+        c: "a3"
       }]
     });
   });
--- src/adapters/__tests__/CategoryFn.test.js
+++ src/adapters/__tests__/CategoryFn.test.js
@@ -1,17 +1,68 @@
 import {
-  arrangeSeriaByCategories
-} from '../CategoryFn';
+  CHT_SPLINE,
+  CHT_LINE,
+  CHT_AREA,
+  CHT_COLUMN,
 
-describe('arrangeSeriaByCategories', () =>{
+  CHT_BAR_SET,
+  CHT_BAR_CLUSTER,
+  CHT_COLUMN_SET,
+  CHT_COLUMN_CLUSTER,
+  CHT_TREE_MAP,
+  CHT_TREE_MAP_CLUSTER
+} from "../../constants/ChartType";
+import {
+  isCategory,
+  arrangeSeriaByCategories
+} from "../CategoryFn";
+
+describe("isCategory", () => {
+  const fn = isCategory;
+  test("should return boolean from string", () => {
+    expect(fn(CHT_BAR_SET)).toBe(true)
+    expect(fn(CHT_BAR_CLUSTER)).toBe(true)
+    expect(fn(CHT_COLUMN_SET)).toBe(true)
+    expect(fn(CHT_COLUMN_CLUSTER)).toBe(true)
+    expect(fn(CHT_TREE_MAP)).toBe(true)
+    expect(fn(CHT_TREE_MAP_CLUSTER)).toBe(true)
+
+    expect(fn(CHT_SPLINE)).toBe(false)
+    expect(fn(CHT_LINE)).toBe(false)
+    expect(fn(CHT_AREA)).toBe(false)
+    expect(fn(CHT_COLUMN)).toBe(false)
+  })
+
+  test("should return boolean from object with property seriaType", () => {
+    expect(fn({seriaType: CHT_COLUMN_SET})).toBe(true)
+    expect(fn({seriaType: CHT_COLUMN_CLUSTER})).toBe(true)
+
+    expect(fn({seriaType: CHT_SPLINE})).toBe(false)
+    expect(fn({seriaType: CHT_LINE})).toBe(false)
+  })
+
+  test("should return false for edge cases", () => {
+    expect(fn()).toBe(false)
+
+    expect(fn({})).toBe(false)
+    expect(fn([])).toBe(false)
+
+    expect(fn(null)).toBe(false)
+    expect(fn(0)).toBe(false)
+    expect(fn(NaN)).toBe(false)
+    expect(fn(true)).toBe(false)
+  })
+})
+
+describe("arrangeSeriaByCategories", () => {
   const fn = arrangeSeriaByCategories;
-  test('should arrange seria data by categories', ()=>{
-    const categories = ['a1','a2','a3']
+  test("should arrange seria data by categories", () => {
+    const categories = ["a1","a2","a3"]
     , seria = { data: [
-      {'c': 'a3'}, {c: 'a4'}, {c: 'a1'}
+      {"c": "a3"}, {c: "a4"}, {c: "a1"}
     ]}
     expect(fn(seria, categories)).toEqual({
       data: [
-        {c: 'a1'}, null, {c: 'a3'}
+        {c: "a1"}, null, {c: "a3"}
       ]
     })
   })
