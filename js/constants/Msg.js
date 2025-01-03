d15fbe9db476311693b6fbdc1093ded6226013a4
Yevhenii Molodyi <eugmolody@gmail.com>
2025-01-03 17:59:49 +0200

    Add utils function joinBy from arrFn to constants Msg

--- js/constants/Msg.js
+++ js/constants/Msg.js
@@ -2,43 +2,40 @@
 
 exports.__esModule = true;
 exports.withoutProxy = exports.withoutApiKey = exports.setAlertMsg = exports.YMD_DATE_OR_EMPTY = exports.NOT_VALID_FORMAT = exports.NOT_SELECTED = exports.MSG_OFFLINE = exports.ERR_TOO_MANY_REQUEST = exports.ERR_PREMIUM_WITHOUT_KEY = exports.ERR_NETWORK = exports.ERR_LOADING_IN_PROGRESS = exports.ERR_FEATURE_WITHOUT_KEY = exports.ERR_ALREADY_EXIST = exports.ERR_10 = void 0;
-const WITHOUT_API_KEY = 'Without Api Key';
-const _crForReqToken = providerName => ['For this type of', providerName, 'request'].filter(Boolean).join(' ');
-const AND_THEN_ENTER_KEY = 'and then enter your API key in dialog SETTINGS [s].';
+var _arrFn = require("../utils/arrFn");
+const WITHOUT_API_KEY = "Without Api Key";
+const _crForReqToken = providerName => (0, _arrFn.joinBy)(" ", "For this type of", providerName, "request");
+const AND_THEN_ENTER_KEY = "and then enter your API key in dialog SETTINGS [s].";
 const _crMsg = (caption, descr) => ({
   caption,
   descr
 });
 const setAlertMsg = (option, msg) => {
-  const {
-    caption,
-    descr
-  } = msg;
-  option.alertCaption = caption;
-  option.alertDescr = descr;
+  option.alertCaption = msg.caption;
+  option.alertDescr = msg.descr;
 };
 exports.setAlertMsg = setAlertMsg;
-const NDL = 'Nasdaq Data Link';
+const NDL = "Nasdaq Data Link";
 const NOT_SELECTED = item => `${item} is not selected`;
 exports.NOT_SELECTED = NOT_SELECTED;
 const NOT_VALID_FORMAT = item => `${item} is not in valid format`;
 exports.NOT_VALID_FORMAT = NOT_VALID_FORMAT;
 const YMD_DATE_OR_EMPTY = exports.YMD_DATE_OR_EMPTY = "YYYY-MM-DD format must be OR Empty";
-const ERR_ALREADY_EXIST = exports.ERR_ALREADY_EXIST = _crMsg('Check Error', 'The chart for this code has already existed in a container. Please, close it and load again.');
-const ERR_LOADING_IN_PROGRESS = exports.ERR_LOADING_IN_PROGRESS = _crMsg('Loading In Progress Error', 'Loading the chart for this item-code in progress.\nIt seems several clicks on button Load repeatedly happend.');
+const ERR_ALREADY_EXIST = exports.ERR_ALREADY_EXIST = _crMsg("Check Error", "The chart for this code has already existed in a container. Please, close it and load again.");
+const ERR_LOADING_IN_PROGRESS = exports.ERR_LOADING_IN_PROGRESS = _crMsg("Loading In Progress Error", "Loading the chart for this item-code in progress.\nIt seems several clicks on button Load repeatedly happend.");
 const ERR_PREMIUM_WITHOUT_KEY = exports.ERR_PREMIUM_WITHOUT_KEY = _crMsg(WITHOUT_API_KEY, `403 : Forbidden\n\nYou have attempted to view a premium database in anonymous mode, i.e., without providing a ${NDL} key. Please register for a free ${NDL} account, and then enter your API in dialog SETTINGS [s].`);
-const TOKEN_API_KEY_REQUIRED = 'API key is required';
-const TOKEN_PLEASE_REGISTER_FOR_FREE = 'Please register for a free';
+const TOKEN_API_KEY_REQUIRED = "API key is required";
+const TOKEN_PLEASE_REGISTER_FOR_FREE = "Please register for a free";
 const ERR_FEATURE_WITHOUT_KEY = exports.ERR_FEATURE_WITHOUT_KEY = _crMsg(WITHOUT_API_KEY, `${_crForReqToken()} a ${NDL} ${TOKEN_API_KEY_REQUIRED}. ${TOKEN_PLEASE_REGISTER_FOR_FREE} ${NDL} account, ${AND_THEN_ENTER_KEY}`);
 const withoutApiKey = providerName => _crMsg(WITHOUT_API_KEY, `${_crForReqToken()} a ${providerName} ${TOKEN_API_KEY_REQUIRED}. ${TOKEN_PLEASE_REGISTER_FOR_FREE} ${providerName} account, more top button A in dialog, ${AND_THEN_ENTER_KEY}`);
 exports.withoutApiKey = withoutApiKey;
-const withoutProxy = providerName => _crMsg('Without Proxy Server', `${_crForReqToken(providerName)} local HTTP proxy server is required. Could be set in dialog SETTINGS [s].`);
+const withoutProxy = providerName => _crMsg("Without Proxy Server", `${_crForReqToken(providerName)} local HTTP proxy server is required. Could be set in dialog SETTINGS [s].`);
 exports.withoutProxy = withoutProxy;
-const ERR_TOO_MANY_REQUEST = exports.ERR_TOO_MANY_REQUEST = _crMsg('Http Code 429', 'Too many request in a given amount of time (rate limiting)');
-const ERR_NETWORK = exports.ERR_NETWORK = _crMsg('Network Error', 'Network error is encountered. Failed to fetch. Maybe you are offline or a DNS lookup failure or a data provider does not respond.');
+const ERR_TOO_MANY_REQUEST = exports.ERR_TOO_MANY_REQUEST = _crMsg("Http Code 429", "Too many request in a given amount of time (rate limiting)");
+const ERR_NETWORK = exports.ERR_NETWORK = _crMsg("Network Error", "Network error is encountered. Failed to fetch. Maybe you are offline or a DNS lookup failure or a data provider does not respond.");
 const ERR_10 = exports.ERR_10 = {
-  ..._crMsg('Not Allowed to Add', "This type of request isn't allowed to be added to selected chart. It can be loaded in his own chart or view item."),
-  token: 'ERR_10'
+  ..._crMsg("Not Allowed to Add", "This type of request isn't allowed to be added to selected chart. It can be loaded in his own chart or view item."),
+  token: "ERR_10"
 };
-const MSG_OFFLINE = exports.MSG_OFFLINE = 'It seems you are offline';
+const MSG_OFFLINE = exports.MSG_OFFLINE = "It seems you are offline";
 //# sourceMappingURL=Msg.js.map
--- src/constants/Msg.js
+++ src/constants/Msg.js
@@ -1,10 +1,12 @@
-const WITHOUT_API_KEY = 'Without Api Key';
+import { joinBy } from "../utils/arrFn";
 
-const _crForReqToken = (providerName) => [
-   'For this type of', providerName, 'request'
-].filter(Boolean).join(' ');
+const WITHOUT_API_KEY = "Without Api Key";
 
-const AND_THEN_ENTER_KEY = 'and then enter your API key in dialog SETTINGS [s].';
+const _crForReqToken = (
+  providerName
+) => joinBy(" ", "For this type of", providerName, "request");
+
+const AND_THEN_ENTER_KEY = "and then enter your API key in dialog SETTINGS [s].";
 
 const _crMsg = (
   caption,
@@ -18,12 +20,11 @@ export const setAlertMsg = (
   option,
   msg
 ) => {
-  const {caption, descr} = msg;
-  option.alertCaption = caption;
-  option.alertDescr = descr;
+  option.alertCaption = msg.caption;
+  option.alertDescr = msg.descr;
 }
 
-const NDL = 'Nasdaq Data Link'
+const NDL = "Nasdaq Data Link"
 
 export const NOT_SELECTED = (
   item
@@ -34,19 +35,19 @@ export const NOT_VALID_FORMAT = (
 export const YMD_DATE_OR_EMPTY = "YYYY-MM-DD format must be OR Empty"
 
 export const ERR_ALREADY_EXIST = _crMsg(
-  'Check Error',
-  'The chart for this code has already existed in a container. Please, close it and load again.'
+  "Check Error",
+  "The chart for this code has already existed in a container. Please, close it and load again."
 )
 export const ERR_LOADING_IN_PROGRESS = _crMsg(
-  'Loading In Progress Error',
-  'Loading the chart for this item-code in progress.\nIt seems several clicks on button Load repeatedly happend.'
+  "Loading In Progress Error",
+  "Loading the chart for this item-code in progress.\nIt seems several clicks on button Load repeatedly happend."
 )
 export const ERR_PREMIUM_WITHOUT_KEY = _crMsg(
   WITHOUT_API_KEY,
   `403 : Forbidden\n\nYou have attempted to view a premium database in anonymous mode, i.e., without providing a ${NDL} key. Please register for a free ${NDL} account, and then enter your API in dialog SETTINGS [s].`
 )
-const TOKEN_API_KEY_REQUIRED = 'API key is required';
-const TOKEN_PLEASE_REGISTER_FOR_FREE = 'Please register for a free';
+const TOKEN_API_KEY_REQUIRED = "API key is required";
+const TOKEN_PLEASE_REGISTER_FOR_FREE = "Please register for a free";
 export const ERR_FEATURE_WITHOUT_KEY = _crMsg(
   WITHOUT_API_KEY,
   `${_crForReqToken()} a ${NDL} ${TOKEN_API_KEY_REQUIRED}. ${TOKEN_PLEASE_REGISTER_FOR_FREE} ${NDL} account, ${AND_THEN_ENTER_KEY}`
@@ -56,23 +57,23 @@ export const withoutApiKey = (providerName) => _crMsg(
   `${_crForReqToken()} a ${providerName} ${TOKEN_API_KEY_REQUIRED}. ${TOKEN_PLEASE_REGISTER_FOR_FREE} ${providerName} account, more top button A in dialog, ${AND_THEN_ENTER_KEY}`
 )
 export const withoutProxy = (providerName) => _crMsg(
-  'Without Proxy Server',
+  "Without Proxy Server",
   `${_crForReqToken(providerName)} local HTTP proxy server is required. Could be set in dialog SETTINGS [s].`
 )
 export const ERR_TOO_MANY_REQUEST = _crMsg(
-  'Http Code 429',
-  'Too many request in a given amount of time (rate limiting)'
+  "Http Code 429",
+  "Too many request in a given amount of time (rate limiting)"
 )
 export const ERR_NETWORK = _crMsg(
-  'Network Error',
-  'Network error is encountered. Failed to fetch. Maybe you are offline or a DNS lookup failure or a data provider does not respond.'
+  "Network Error",
+  "Network error is encountered. Failed to fetch. Maybe you are offline or a DNS lookup failure or a data provider does not respond."
 )
 export const ERR_10 = {
   ..._crMsg(
-    'Not Allowed to Add',
+    "Not Allowed to Add",
     "This type of request isn't allowed to be added to selected chart. It can be loaded in his own chart or view item.",
   ),
-  token: 'ERR_10'
+  token: "ERR_10"
 }
 
-export const MSG_OFFLINE = 'It seems you are offline'
+export const MSG_OFFLINE = "It seems you are offline"
