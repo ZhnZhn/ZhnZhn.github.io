175935e86d8489dd9406c28ed555a95dfcc456c0
Yevhenii Molodyi <eugmolody@gmail.com>
2025-01-03 17:49:41 +0200

    Remove deprecated routes from flux logic RouterFnValue

--- js/flux/logic/RouterFnValue.js
+++ js/flux/logic/RouterFnValue.js
@@ -5,11 +5,7 @@ exports.getCrValue = void 0;
 var _crRouter = require("../../utils/crRouter");
 const getCrValue = exports.getCrValue = (0, _crRouter.crGetRoute)({
   ROne: one => one,
-  RTwo: (one, two) => `${two}`,
-  ROneTwo: (one, two) => `${one}/${two}`,
   RPrefixOne: (prefix, one) => `${prefix}/${one}`,
-  RPrefixOneTwo: (prefix, one, two) => `${prefix}/${one}_${two}`,
-  RPrefixOneEmptyTwo: (prefix, one, two) => `${prefix}/${two}`,
-  RPrefixTwoOne: (prefix, one, two) => `${prefix}/${two}_${one}`
+  RPrefixOneTwo: (prefix, one, two) => `${prefix}/${one}_${two}`
 });
 //# sourceMappingURL=RouterFnValue.js.map
--- src/flux/logic/RouterFnValue.js
+++ src/flux/logic/RouterFnValue.js
@@ -2,10 +2,6 @@ import { crGetRoute } from "../../utils/crRouter";
 
 export const getCrValue = crGetRoute({
   ROne : (one) => one,
-  RTwo : (one, two) => `${two}`,
-  ROneTwo : (one, two) => `${one}/${two}`,
   RPrefixOne : (prefix, one) => `${prefix}/${one}`,
-  RPrefixOneTwo : (prefix, one, two) => `${prefix}/${one}_${two}`,
-  RPrefixOneEmptyTwo : (prefix, one, two) => `${prefix}/${two}`,
-  RPrefixTwoOne : (prefix, one, two) => `${prefix}/${two}_${one}`
+  RPrefixOneTwo : (prefix, one, two) => `${prefix}/${one}_${two}`
 })
