c2dc8cde29bc68fbc2989c4dded487fa8dcb3f10
Yevhenii Molodyi <eugmolody@gmail.com>
2025-01-08 12:49:38 +0200

    Add function factory fCheckResponse from AdapterFn to TwApi

--- js/adapters/twelve/TwApi.js
+++ js/adapters/twelve/TwApi.js
@@ -5,7 +5,6 @@ exports.default = void 0;
 var _AdapterFn = require("../AdapterFn");
 const URL = 'https://api.twelvedata.com',
   QUERY_TAIL = 'dp=2&order=ASC&timezone=UTC';
-const _isArr = Array.isArray;
 const TwApi = {
   getRequestUrl(option) {
     const {
@@ -17,18 +16,11 @@ const TwApi = {
       }),
       interval = (0, _AdapterFn.getValue)(items[1]),
       outputsize = (0, _AdapterFn.getValue)(items[2]);
+    option.apiKey = null;
     option.itemCaption = symbol;
     return `${URL}/time_series?symbol=${symbol}&apikey=${apiKey}&interval=${interval}&outputsize=${outputsize}&${QUERY_TAIL}`;
   },
-  checkResponse(json, option) {
-    const {
-      values,
-      message
-    } = json || {};
-    if (!_isArr(values)) {
-      throw (0, _AdapterFn.crError)('', message);
-    }
-  }
+  checkResponse: (0, _AdapterFn.fCheckResponse)(json => (json || {}).values, json => (json || {}).message)
 };
 var _default = exports.default = TwApi;
 //# sourceMappingURL=TwApi.js.map
--- src/adapters/twelve/TwApi.js
+++ src/adapters/twelve/TwApi.js
@@ -1,28 +1,31 @@
 import {
   getValue,
-  crError
+  fCheckResponse
 } from '../AdapterFn';
 
 const URL = 'https://api.twelvedata.com'
 , QUERY_TAIL = 'dp=2&order=ASC&timezone=UTC';
 
-const _isArr = Array.isArray;
-
 const TwApi = {
   getRequestUrl(option){
-    const { apiKey, items } = option
+    const {
+      apiKey,
+      items
+    } = option
     , symbol = getValue(items[0], {isUpper: true})
     , interval = getValue(items[1])
     , outputsize = getValue(items[2]);
+
+    option.apiKey = null
     option.itemCaption = symbol
+
     return `${URL}/time_series?symbol=${symbol}&apikey=${apiKey}&interval=${interval}&outputsize=${outputsize}&${QUERY_TAIL}`;
   },
-  checkResponse(json, option){
-    const { values, message } = json || {};
-    if (!_isArr(values)) {
-      throw crError('', message);
-    }
-  }
+
+  checkResponse: fCheckResponse(
+    json => (json || {}).values,
+    json => (json || {}).message
+  )
 };
 
 export default TwApi
