50c55c97ac26ad6a148db4659ed2390f37924d89
Yevhenii Molodyi <eugmolody@gmail.com>
2025-02-11 15:58:08 +0200

    Replace ColumnRangeDialog class component by function component

--- js/components/dialogs-modal/ColumnRangeDialog.js
+++ js/components/dialogs-modal/ColumnRangeDialog.js
@@ -4,6 +4,8 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 exports.__esModule = true;
 exports.default = void 0;
 var _uiApi = require("../uiApi");
+var _styleFn = require("../styleFn");
+var _memoIsShow = _interopRequireDefault(require("../hoc/memoIsShow"));
 var _seriaFns = require("../../charts/seriaFns");
 var _ModalDialog = _interopRequireDefault(require("../zhn-moleculs/ModalDialog"));
 var _FlatButton = _interopRequireDefault(require("../zhn-m/FlatButton"));
@@ -14,11 +16,8 @@ const S_TEXT = {
     padding: '16px 0 0 16px',
     fontWeight: 600
   },
-  S_ROW = {
+  S_PL_8 = {
     paddingLeft: 8
-  },
-  S_INLINE = {
-    display: 'inline-block'
   },
   S_CAPTION_1 = {
     width: 60
@@ -65,142 +64,124 @@ const _crSeriaOptions = pointWidth => ({
     pointWidth
   }
 });
-class ColumnRangeDialog extends _uiApi.Component {
-  constructor(props) {
-    super(props);
-    this._commandButtons = [/*#__PURE__*/(0, _jsxRuntime.jsx)(_FlatButton.default, {
+const ColumnRangeDialog = (0, _memoIsShow.default)(_ref => {
+  let {
+    isShow,
+    data,
+    onClose
+  } = _ref;
+  const _refPointWidth = (0, _uiApi.useRef)(DF_POIN_WIDTH),
+    _refR1 = (0, _uiApi.useRef)(DF_R1),
+    _refR2 = (0, _uiApi.useRef)(DF_R2),
+    _refColor = (0, _uiApi.useRef)(),
+    _refFromIndex = (0, _uiApi.useRef)(),
+    _refToIndex = (0, _uiApi.useRef)(),
+    _refInputWidth = (0, _uiApi.useRef)(),
+    _refInputR1 = (0, _uiApi.useRef)(),
+    _refInputR2 = (0, _uiApi.useRef)(),
+    [_heWidth, _heRadius1, _heRadius2, _heColor] = (0, _uiApi.useMemo)(() => [_fHeValue(_refPointWidth, -1, 7), _fHeValue(_refR1, -1, 9), _fHeValue(_refR2, -1, 9), color => {
+      (0, _uiApi.setRefValue)(_refColor, color);
+    }], []),
+    _hAdd = (0, _uiApi.useMemo)(() => () => {
+      const {
+          chart
+        } = data,
+        _series = chart.series,
+        _s1 = _series[(0, _uiApi.getRefValue)(_refFromIndex)],
+        _s2 = _series[(0, _uiApi.getRefValue)(_refToIndex)],
+        _d = (0, _seriaFns.columnRange)(_s1.data, _s2.data);
+      _heWidth((0, _uiApi.getInputValue)(_refInputWidth));
+      _heRadius1((0, _uiApi.getInputValue)(_refInputR1));
+      _heRadius2((0, _uiApi.getInputValue)(_refInputR2));
+      _setRadius((0, _uiApi.getRefValue)(_refR1), _s1);
+      _setRadius((0, _uiApi.getRefValue)(_refR2), _s2);
+      chart.zhAddSeriaToYAxis({
+        data: _d,
+        color: (0, _uiApi.getRefValue)(_refColor),
+        yIndex: 0
+      }, _crSeriaOptions((0, _uiApi.getRefValue)(_refPointWidth)));
+      chart.zhDataLabels(true);
+      onClose();
+    }, [data, onClose, _heWidth, _heRadius1, _heRadius2]),
+    _commandButtons = (0, _uiApi.useMemo)(() => [/*#__PURE__*/(0, _jsxRuntime.jsx)(_FlatButton.default, {
       caption: "Yes, Connect",
       isPrimary: true,
-      onClick: this._hAdd
-    }, "yes")];
-    this._refPointWidth = (0, _uiApi.createRef)(DF_POIN_WIDTH);
-    this._refR1 = (0, _uiApi.createRef)(DF_R1);
-    this._refR2 = (0, _uiApi.createRef)(DF_R2);
-    this._heWidth = _fHeValue(this._refPointWidth, -1, 7);
-    this._heRadius1 = _fHeValue(this._refR1, -1, 9);
-    this._heRadius2 = _fHeValue(this._refR2, -1, 9);
-    this._refColor = (0, _uiApi.createRef)();
-    this._refFromIndex = (0, _uiApi.createRef)();
-    this._refToIndex = (0, _uiApi.createRef)();
-    this._refW = (0, _uiApi.createRef)();
-    this._refR1 = (0, _uiApi.createRef)();
-    this._refR2 = (0, _uiApi.createRef)();
-  }
-  shouldComponentUpdate(nextProps, nextState) {
-    if (nextProps !== this.props && nextProps.isShow === this.props.isShow) {
-      return false;
-    }
-    return true;
-  }
-  _hAdd = () => {
-    const {
-        data,
-        onClose
-      } = this.props,
-      {
-        chart
-      } = data,
-      _series = chart.series,
-      _s1 = _series[(0, _uiApi.getRefValue)(this._refFromIndex)],
-      _s2 = _series[(0, _uiApi.getRefValue)(this._refToIndex)],
-      _d = (0, _seriaFns.columnRange)(_s1.data, _s2.data);
-    this._heWidth((0, _uiApi.getInputValue)(this._refW));
-    this._heRadius1((0, _uiApi.getInputValue)(this._refR1));
-    this._heRadius2((0, _uiApi.getInputValue)(this._refR2));
-    _setRadius((0, _uiApi.getRefValue)(this._refR1), _s1);
-    _setRadius((0, _uiApi.getRefValue)(this._refR2), _s2);
-    chart.zhAddSeriaToYAxis({
-      data: _d,
-      color: (0, _uiApi.getRefValue)(this._refColor),
-      yIndex: 0
-    }, _crSeriaOptions((0, _uiApi.getRefValue)(this._refPointWidth)));
-    chart.zhDataLabels(true);
-    onClose();
-  };
-  _heColor = color => {
-    (0, _uiApi.setRefValue)(this._refColor, color);
-  };
-  render() {
-    const {
-        isShow,
-        data,
-        onClose
-      } = this.props,
-      {
-        chart
-      } = data,
-      {
-        series
-      } = chart,
-      [n1, n2, fromIndex, toIndex] = _getNames(series),
-      c1 = _getColor(series, fromIndex),
-      c2 = _getColor(series, toIndex);
-    (0, _uiApi.setRefValue)(this._refFromIndex, fromIndex);
-    (0, _uiApi.setRefValue)(this._refToIndex, toIndex);
-    (0, _uiApi.setRefValue)(this._refColor, c1);
-    return /*#__PURE__*/(0, _jsxRuntime.jsxs)(_ModalDialog.default, {
-      caption: "Add ColumnRange",
-      isShow: isShow,
-      commandButtons: this._commandButtons,
-      onClose: onClose,
-      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-        style: S_TEXT,
-        children: "Connect dots series by column range?"
-      }), /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
-        style: S_ROW,
-        children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_RowInputColor.default, {
-          style: S_INLINE,
-          captionStyle: S_CAPTION_1,
-          caption: "Color",
-          initValue: c1,
-          onEnter: this._heColor
-        }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_RowInputText.default, {
-          refEl: this._refW,
-          styleRoot: S_INLINE,
-          styleCaption: S_CAPTION_1,
-          styleInput: S_INPUT,
-          caption: "Width",
-          initValue: DF_POIN_WIDTH,
-          maxLength: 2,
-          type: "number",
-          min: 0,
-          max: 6,
-          step: 1
-        })]
-      }), /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
-        style: S_ROW,
-        children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_RowInputText.default, {
-          refEl: this._refR1,
-          styleRoot: S_INLINE,
-          styleCaption: {
-            ...S_CAPTION_2,
-            ...{
-              color: c1
-            }
-          },
-          styleInput: S_INPUT,
-          caption: `R ${n1}`,
-          initValue: DF_R1,
-          type: "number",
-          maxLength: 2
-        }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_RowInputText.default, {
-          refEl: this._refR2,
-          styleRoot: S_INLINE,
-          styleCaption: {
-            ...S_CAPTION_2,
-            ...{
-              color: c2
-            }
-          },
-          styleInput: S_INPUT,
-          caption: `R ${n2}`,
-          initValue: DF_R2,
-          type: "number",
-          maxLength: 2
-        })]
+      onClick: _hAdd
+    }, "yes")], [_hAdd]);
+  const {
+      chart
+    } = data,
+    {
+      series
+    } = chart,
+    [n1, n2, fromIndex, toIndex] = _getNames(series),
+    c1 = _getColor(series, fromIndex),
+    c2 = _getColor(series, toIndex);
+  (0, _uiApi.setRefValue)(_refFromIndex, fromIndex);
+  (0, _uiApi.setRefValue)(_refToIndex, toIndex);
+  (0, _uiApi.setRefValue)(_refColor, c1);
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)(_ModalDialog.default, {
+    caption: "Add ColumnRange",
+    isShow: isShow,
+    commandButtons: _commandButtons,
+    onClose: onClose,
+    children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+      style: S_TEXT,
+      children: "Connect dots series by column range?"
+    }), /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+      style: S_PL_8,
+      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_RowInputColor.default, {
+        style: _styleFn.S_INLINE,
+        captionStyle: S_CAPTION_1,
+        caption: "Color",
+        initValue: c1,
+        onEnter: _heColor
+      }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_RowInputText.default, {
+        refEl: _refInputWidth,
+        styleRoot: _styleFn.S_INLINE,
+        styleCaption: S_CAPTION_1,
+        styleInput: S_INPUT,
+        caption: "Width",
+        initValue: DF_POIN_WIDTH,
+        maxLength: 2,
+        type: "number",
+        min: 0,
+        max: 6,
+        step: 1
       })]
-    });
-  }
-}
+    }), /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+      style: S_PL_8,
+      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_RowInputText.default, {
+        refEl: _refInputR1,
+        styleRoot: _styleFn.S_INLINE,
+        styleCaption: {
+          ...S_CAPTION_2,
+          ...{
+            color: c1
+          }
+        },
+        styleInput: S_INPUT,
+        caption: `R ${n1}`,
+        initValue: DF_R1,
+        type: "number",
+        maxLength: 2
+      }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_RowInputText.default, {
+        refEl: _refInputR2,
+        styleRoot: _styleFn.S_INLINE,
+        styleCaption: {
+          ...S_CAPTION_2,
+          ...{
+            color: c2
+          }
+        },
+        styleInput: S_INPUT,
+        caption: `R ${n2}`,
+        initValue: DF_R2,
+        type: "number",
+        maxLength: 2
+      })]
+    })]
+  });
+});
 var _default = exports.default = ColumnRangeDialog;
 //# sourceMappingURL=ColumnRangeDialog.js.map
--- src/components/dialogs-modal/ColumnRangeDialog.jsx
+++ src/components/dialogs-modal/ColumnRangeDialog.jsx
@@ -1,10 +1,13 @@
 import {
-  Component,
-  createRef,
+  useRef,
+  useMemo,
   getInputValue,
   setRefValue,
   getRefValue
 } from '../uiApi';
+
+import { S_INLINE } from '../styleFn';
+import memoIsShow from '../hoc/memoIsShow';
 
 import { columnRange } from '../../charts/seriaFns';
 
@@ -17,8 +20,7 @@ const S_TEXT = {
   padding: '16px 0 0 16px',
   fontWeight: 600
 }
-, S_ROW = { paddingLeft: 8 }
-, S_INLINE = { display: 'inline-block' }
+, S_PL_8 = { paddingLeft: 8 }
 , S_CAPTION_1 = { width: 60 }
 , S_CAPTION_2 = { width: 100 }
 , S_INPUT = { width: 40 };
@@ -55,7 +57,7 @@ const _setRadius = (
   const { options } = seria;
   options.marker.radius = value
   seria.update(options, false)
-}
+};
 
 const _fHeValue = (
   ref,
@@ -66,7 +68,7 @@ const _fHeValue = (
   if ( _>min && _<max) {
     setRefValue(ref, v)
   }
-}
+};
 
 const _crSeriaOptions = (
   pointWidth
@@ -75,153 +77,142 @@ const _crSeriaOptions = (
   ...{ pointWidth }
 });
 
-class ColumnRangeDialog extends Component {
+const ColumnRangeDialog = memoIsShow(({
+  isShow,
+  data,
+  onClose
+}) => {
+  const _refPointWidth = useRef(DF_POIN_WIDTH)
+  , _refR1 = useRef(DF_R1)
+  , _refR2 = useRef(DF_R2)
 
-  constructor(props){
-    super(props)
+  , _refColor = useRef()
+  , _refFromIndex = useRef()
+  , _refToIndex = useRef()
 
-    this._commandButtons = [
-      <FlatButton
-         key="yes"
-         caption="Yes, Connect"
-         isPrimary={true}
-         onClick={this._hAdd}
-      />
-    ]
+  , _refInputWidth = useRef()
+  , _refInputR1 = useRef()
+  , _refInputR2 = useRef()
 
-    this._refPointWidth = createRef(DF_POIN_WIDTH)
-    this._refR1 = createRef(DF_R1)
-    this._refR2 = createRef(DF_R2)
-    this._heWidth = _fHeValue(this._refPointWidth, -1, 7)
-    this._heRadius1 = _fHeValue(this._refR1, -1, 9)
-    this._heRadius2 = _fHeValue(this._refR2, -1, 9)
-
-    this._refColor = createRef()
-    this._refFromIndex = createRef()
-    this._refToIndex = createRef()
-
-    this._refW = createRef()
-    this._refR1 = createRef()
-    this._refR2 = createRef()
-  }
-
-  shouldComponentUpdate(nextProps, nextState){
-    if (nextProps !== this.props
-        && nextProps.isShow === this.props.isShow) {
-      return false;
+  , [
+    _heWidth,
+    _heRadius1,
+    _heRadius2,
+    _heColor
+  ] = useMemo(() => [
+    _fHeValue(_refPointWidth, -1, 7),
+    _fHeValue(_refR1, -1, 9),
+    _fHeValue(_refR2, -1, 9),
+    (color) => {
+      setRefValue(_refColor, color)
     }
-    return true;
-  }
-
-  _hAdd = () => {
-    const {
-      data,
-      onClose
-    } = this.props
-    , { chart } = data
+  ], [])
+  , _hAdd = useMemo(() => () => {
+    const { chart } = data
     , _series = chart.series
-    , _s1 = _series[getRefValue(this._refFromIndex)]
-    , _s2 = _series[getRefValue(this._refToIndex)]
+    , _s1 = _series[getRefValue(_refFromIndex)]
+    , _s2 = _series[getRefValue(_refToIndex)]
     , _d = columnRange(_s1.data, _s2.data);
 
-     this._heWidth(getInputValue(this._refW))
-     this._heRadius1(getInputValue(this._refR1))
-     this._heRadius2(getInputValue(this._refR2))
+    _heWidth(getInputValue(_refInputWidth))
+    _heRadius1(getInputValue(_refInputR1))
+    _heRadius2(getInputValue(_refInputR2))
 
-     _setRadius(getRefValue(this._refR1), _s1)
-     _setRadius(getRefValue(this._refR2), _s2)
+    _setRadius(getRefValue(_refR1), _s1)
+    _setRadius(getRefValue(_refR2), _s2)    
 
-     chart.zhAddSeriaToYAxis({
-       data: _d,
-       color: getRefValue(this._refColor),
-       yIndex: 0,
-     }, _crSeriaOptions(getRefValue(this._refPointWidth)))
+    chart.zhAddSeriaToYAxis({
+      data: _d,
+      color: getRefValue(_refColor),
+      yIndex: 0,
+    }, _crSeriaOptions(getRefValue(_refPointWidth)))
 
-     chart.zhDataLabels(true)
-     onClose()
-  }
+    chart.zhDataLabels(true)
+    onClose()
+  }, [
+    data, onClose,
+    _heWidth, _heRadius1, _heRadius2
+  ])
+  , _commandButtons = useMemo(() => [
+     <FlatButton
+        key="yes"
+        caption="Yes, Connect"
+        isPrimary={true}
+        onClick={_hAdd}
+     />
+  ], [_hAdd]);
 
-  _heColor = (color) => {
-    setRefValue(this._refColor, color)
-  }
+  const { chart } = data
+  , { series } = chart
+  , [
+    n1,
+    n2,
+    fromIndex,
+    toIndex
+  ] = _getNames(series)
+  , c1 = _getColor(series, fromIndex)
+  , c2 = _getColor(series, toIndex);
 
-  render(){
-    const {
-      isShow,
-      data,
-      onClose
-    } = this.props
-    , { chart } = data
-    , { series } = chart
-    , [
-      n1,
-      n2,
-      fromIndex,
-      toIndex
-    ] = _getNames(series)
-    , c1 = _getColor(series, fromIndex)
-    , c2 = _getColor(series, toIndex);
+  setRefValue(_refFromIndex, fromIndex)
+  setRefValue(_refToIndex, toIndex)
+  setRefValue(_refColor, c1)
 
-    setRefValue(this._refFromIndex, fromIndex)
-    setRefValue(this._refToIndex, toIndex)
-    setRefValue(this._refColor, c1)
-    return(
-      <ModalDialog
-        caption="Add ColumnRange"
-        isShow={isShow}
-        commandButtons={this._commandButtons}
-        onClose={onClose}
-      >
-        <div style={S_TEXT}>
-          Connect dots series by column range?
-        </div>
-        <div style={S_ROW}>
-          <RowInputColor
-            style={S_INLINE}
-            captionStyle={S_CAPTION_1}
-            caption="Color"
-            initValue={c1}
-            onEnter={this._heColor}
-          />
-          <RowInputText
-             refEl={this._refW}
-             styleRoot={S_INLINE}
-             styleCaption={S_CAPTION_1}
-             styleInput={S_INPUT}
-             caption="Width"
-             initValue={DF_POIN_WIDTH}
-             maxLength={2}
-             type="number"
-             min={0}
-             max={6}
-             step={1}
-          />
-        </div>
-        <div style={S_ROW}>
-          <RowInputText
-             refEl={this._refR1}
-             styleRoot={S_INLINE}
-             styleCaption={{...S_CAPTION_2, ...{ color: c1 }}}
-             styleInput={S_INPUT}
-             caption={`R ${n1}`}
-             initValue={DF_R1}
-             type="number"
-             maxLength={2}
-          />
-          <RowInputText
-             refEl={this._refR2}
-             styleRoot={S_INLINE}
-             styleCaption={{...S_CAPTION_2, ...{ color: c2 }}}
-             styleInput={S_INPUT}
-             caption={`R ${n2}`}
-             initValue={DF_R2}
-             type="number"
-             maxLength={2}
-          />
-       </div>
-      </ModalDialog>
-    );
-  }
-}
+  return (
+    <ModalDialog
+      caption="Add ColumnRange"
+      isShow={isShow}
+      commandButtons={_commandButtons}
+      onClose={onClose}
+    >
+      <div style={S_TEXT}>
+        Connect dots series by column range?
+      </div>
+      <div style={S_PL_8}>
+        <RowInputColor
+          style={S_INLINE}
+          captionStyle={S_CAPTION_1}
+          caption="Color"
+          initValue={c1}
+          onEnter={_heColor}
+        />
+        <RowInputText
+           refEl={_refInputWidth}
+           styleRoot={S_INLINE}
+           styleCaption={S_CAPTION_1}
+           styleInput={S_INPUT}
+           caption="Width"
+           initValue={DF_POIN_WIDTH}
+           maxLength={2}
+           type="number"
+           min={0}
+           max={6}
+           step={1}
+        />
+      </div>
+      <div style={S_PL_8}>
+        <RowInputText
+           refEl={_refInputR1}
+           styleRoot={S_INLINE}
+           styleCaption={{...S_CAPTION_2, ...{ color: c1 }}}
+           styleInput={S_INPUT}
+           caption={`R ${n1}`}
+           initValue={DF_R1}
+           type="number"
+           maxLength={2}
+        />
+        <RowInputText
+           refEl={_refInputR2}
+           styleRoot={S_INLINE}
+           styleCaption={{...S_CAPTION_2, ...{ color: c2 }}}
+           styleInput={S_INPUT}
+           caption={`R ${n2}`}
+           initValue={DF_R2}
+           type="number"
+           maxLength={2}
+        />
+     </div>
+    </ModalDialog>
+  );
+})
 
 export default ColumnRangeDialog
