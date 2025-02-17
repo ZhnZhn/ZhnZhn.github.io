3290bd742f5c00dadfef425c606c840892448fd6
Yevhenii Molodyi <eugmolody@gmail.com>
2025-02-17 12:39:13 +0200

    Add function _crPairLimitQueryToken to BgApi

--- src/adapters/bitget/BgApi.js
+++ src/adapters/bitget/BgApi.js
@@ -2,16 +2,21 @@ import { crRouteDfObApi } from '../ApiFn';
 
 const API_URL = "https://api.bitget.com/api/v2/spot/market";
 
+const _crPairLimitQueryToken = (
+  pair,
+  limit
+) => `symbol=${pair}&limit=${limit}`;
+
 const _crDfUrl = (
   pair,
   timeframe,
   limit
-) => `${API_URL}/history-candles?symbol=${pair}&granularity=${timeframe}&endTime=${Date.now()}&limit=${limit}`;
+) => `${API_URL}/candles?${_crPairLimitQueryToken(pair, limit)}&granularity=${timeframe}`;
 
 const _crObUrl = (
   pair,
   limit
-) => `${API_URL}/orderbook?symbol=${pair}&type=step0&limit=${limit}`;
+) => `${API_URL}/orderbook?${_crPairLimitQueryToken(pair, limit)}&type=step0`;
 
 const _getData = (json) => (json || {}).data;
 
