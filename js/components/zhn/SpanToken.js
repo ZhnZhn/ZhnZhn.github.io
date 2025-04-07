2f02fb4af672e880e289647ebe70e5af5e166ab7
Yevhenii Molodyi <eugmolody@gmail.com>
2025-04-07 14:03:28 +0300

    Add function component SpanGap named export to components SpanToken namespace

--- js/components/zhn/SpanToken.js
+++ js/components/zhn/SpanToken.js
@@ -1,7 +1,7 @@
 "use strict";
 
 exports.__esModule = true;
-exports.SpanValue = exports.SpanLabel = exports.SpanInputLabel = exports.SpanDate = exports.SpanBoldBlack = exports.SpanBlack = exports.CL_SP_INPUT_LABEL = void 0;
+exports.SpanValue = exports.SpanLabel = exports.SpanInputLabel = exports.SpanGap = exports.SpanDate = exports.SpanBoldBlack = exports.SpanBold = exports.SpanBlack = exports.CL_SP_INPUT_LABEL = void 0;
 var _styleFn = require("../styleFn");
 var _jsxRuntime = require("react/jsx-runtime");
 const _fSpanToken = className => _ref => {
@@ -25,4 +25,17 @@ const CL_SP_INPUT_LABEL = exports.CL_SP_INPUT_LABEL = `${CL_SP_LABEL} sp-input`;
 const SpanInputLabel = exports.SpanInputLabel = _fSpanToken((0, _styleFn.crBoldCn)(CL_SP_INPUT_LABEL));
 const SpanBlack = exports.SpanBlack = _fSpanToken(_styleFn.CL_BLACK);
 const SpanBoldBlack = exports.SpanBoldBlack = _fSpanToken((0, _styleFn.crBoldCn)(_styleFn.CL_BLACK));
+const SpanBold = exports.SpanBold = _fSpanToken((0, _styleFn.crBoldCn)());
+const SpanGap = _ref2 => {
+  let {
+    width
+  } = _ref2;
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+    style: {
+      ..._styleFn.S_INLINE,
+      width
+    }
+  });
+};
+exports.SpanGap = SpanGap;
 //# sourceMappingURL=SpanToken.js.map
--- src/components/zhn/SpanToken.jsx
+++ src/components/zhn/SpanToken.jsx
@@ -1,5 +1,6 @@
 import {
   CL_BLACK,
+  S_INLINE,
   crCnNotSelected,
   crBoldCn
 } from '../styleFn';
@@ -28,3 +29,10 @@ export const SpanInputLabel = _fSpanToken(crBoldCn(CL_SP_INPUT_LABEL))
 
 export const SpanBlack = _fSpanToken(CL_BLACK)
 export const SpanBoldBlack = _fSpanToken(crBoldCn(CL_BLACK))
+export const SpanBold = _fSpanToken(crBoldCn())
+
+export const SpanGap = ({
+  width
+}) => (
+  <span style={{...S_INLINE, width}} />
+)
