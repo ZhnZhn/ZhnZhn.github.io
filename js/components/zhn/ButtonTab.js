048f6b9f72576c046c26f8fb01dcded3108916be
Yevhenii Molodyi <eugmolody@gmail.com>
2025-05-05 14:25:28 +0300

    Add function component IfTrue from uiApi to ButtonTab function component

--- js/components/zhn/ButtonTab.js
+++ js/components/zhn/ButtonTab.js
@@ -3,28 +3,36 @@
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 exports.__esModule = true;
 exports.default = void 0;
+var _uiApi = require("../uiApi");
 var _styleFn = require("../styleFn");
 var _Button = _interopRequireDefault(require("./Button"));
 var _jsxRuntime = require("react/jsx-runtime");
 const CL_BT_TAB = (0, _styleFn.crNotSelectedCn)("bt bt-tab"),
   CL_ARROW_DOWN = "arrow-down";
-const ButtonTab = _ref => {
-  let {
-    is = true,
-    isMenu,
-    className,
-    style,
-    caption,
-    onClick
-  } = _ref;
-  return is ? /*#__PURE__*/(0, _jsxRuntime.jsxs)(_Button.default, {
-    className: (0, _styleFn.crCn)(CL_BT_TAB, className),
-    style: style,
-    onClick: onClick,
-    children: [caption, isMenu && /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-      className: CL_ARROW_DOWN
+const ButtonTab = props => /*#__PURE__*/(0, _jsxRuntime.jsx)(_uiApi.IfTrue, {
+  v: props.is ?? true,
+  children: /*#__PURE__*/(0, _jsxRuntime.jsxs)(_Button.default, {
+    className: (0, _styleFn.crCn)(CL_BT_TAB, props.className),
+    style: props.style,
+    onClick: props.onClick,
+    children: [props.caption, /*#__PURE__*/(0, _jsxRuntime.jsx)(_uiApi.IfTrue, {
+      v: props.isMenu,
+      children: /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+        className: CL_ARROW_DOWN
+      })
     })]
-  }) : null;
-};
+  })
+});
+
+/*
+ButtonTab.propTypes = {
+ is: PropTypes.bool,
+ isMenu: PropTypes.bool,
+ className: PropTypes.string,
+ style: PropTypes.object,
+ caption: PropTypes.string,
+ onClick: PropTypes.func
+}
+*/
 var _default = exports.default = ButtonTab;
 //# sourceMappingURL=ButtonTab.js.map
--- src/components/zhn/ButtonTab.jsx
+++ src/components/zhn/ButtonTab.jsx
@@ -1,3 +1,4 @@
+import { IfTrue } from '../uiApi';
 import {
   crCn,
   crNotSelectedCn
@@ -8,22 +9,30 @@ import Button from './Button';
 const CL_BT_TAB = crNotSelectedCn("bt bt-tab")
 , CL_ARROW_DOWN = "arrow-down";
 
-const ButtonTab = ({
-  is=true,
-  isMenu,
-  className,
-  style,
-  caption,
-  onClick
-}) => is ? (
-  <Button
-    className={crCn(CL_BT_TAB, className)}
-    style={style}
-    onClick={onClick}
-  >
-     {caption}
-     {isMenu && <span className={CL_ARROW_DOWN} />}
-  </Button>
-) : null;
+const ButtonTab = (props) => (
+  <IfTrue v={props.is ?? true}>
+    <Button
+      className={crCn(CL_BT_TAB, props.className)}
+      style={props.style}
+      onClick={props.onClick}
+    >
+       {props.caption}
+       <IfTrue v={props.isMenu}>
+         <span className={CL_ARROW_DOWN} />
+       </IfTrue>
+    </Button>
+  </IfTrue>
+)
+
+/*
+ButtonTab.propTypes = {
+ is: PropTypes.bool,
+ isMenu: PropTypes.bool,
+ className: PropTypes.string,
+ style: PropTypes.object,
+ caption: PropTypes.string,
+ onClick: PropTypes.func
+}
+*/
 
 export default ButtonTab
