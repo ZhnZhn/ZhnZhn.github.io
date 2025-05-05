a88a3c1801da958dd15b4838a4fae471aeff860e
Yevhenii Molodyi <eugmolody@gmail.com>
2025-05-05 12:53:09 +0300

    Add function component IfTrue to function component Link

--- js/components/zhn/Link.js
+++ js/components/zhn/Link.js
@@ -3,6 +3,7 @@
 exports.__esModule = true;
 exports.default = void 0;
 var _uiApi = require("../uiApi");
+var _IfTrue = require("./IfTrue");
 var _jsxRuntime = require("react/jsx-runtime");
 const Link = _ref => {
   let {
@@ -13,13 +14,16 @@ const Link = _ref => {
     children
   } = _ref;
   const _href = (0, _uiApi.toHref)(href);
-  return _href && (caption || children) ? /*#__PURE__*/(0, _jsxRuntime.jsxs)("a", {
-    target: "_blank",
-    className: className,
-    style: style,
-    href: _href,
-    children: [caption, children]
-  }) : null;
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)(_IfTrue.IfTrue, {
+    v: _href && (caption || children),
+    children: /*#__PURE__*/(0, _jsxRuntime.jsxs)("a", {
+      target: "_blank",
+      className: className,
+      style: style,
+      href: _href,
+      children: [caption, children]
+    })
+  });
 };
 var _default = exports.default = Link;
 //# sourceMappingURL=Link.js.map
--- src/components/zhn/Link.jsx
+++ src/components/zhn/Link.jsx
@@ -1,4 +1,5 @@
 import { toHref } from '../uiApi';
+import { IfTrue } from './IfTrue';
 
 const Link = ({
   className,
@@ -8,17 +9,19 @@ const Link = ({
   children
 }) => {
   const _href = toHref(href);
-  return _href && (caption || children) ? (
-    <a
-      target="_blank"
-      className={className}
-      style={style}
-      href={_href}
-    >
-      {caption}
-      {children}
-    </a>
-  ) : null;
+  return (
+    <IfTrue v={_href && (caption || children)}>
+      <a
+        target="_blank"
+        className={className}
+        style={style}
+        href={_href}
+      >
+        {caption}
+        {children}
+      </a>
+    </IfTrue>
+  );
 }
 
 export default Link
