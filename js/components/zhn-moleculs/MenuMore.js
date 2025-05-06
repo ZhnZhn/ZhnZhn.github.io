0024b0b535da17315fd631c80f66c32d4e0ba98c
Yevhenii Molodyi <eugmolody@gmail.com>
2025-05-06 10:06:42 +0300

    Add function component IfTrue to function component MenuMore

--- js/components/zhn-moleculs/MenuMore.js
+++ js/components/zhn-moleculs/MenuMore.js
@@ -3,31 +3,27 @@
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 exports.__esModule = true;
 exports.default = void 0;
+var _uiApi = require("../uiApi");
 var _styleFn = require("../styleFn");
 var _SvgMore = _interopRequireDefault(require("../zhn/SvgMore"));
 var _ModalSlider = require("../zhn-modal-slider/ModalSlider");
 var _jsxRuntime = require("react/jsx-runtime");
 const CL_MENU_MORE = 'dialog__menu-more',
   S_BT_MORE = (0, _styleFn.crAbsoluteTopLeftStyle)(4, 2);
-const MenuMore = _ref => {
-  let {
-    refEl,
-    isMore,
-    menuModel,
-    toggle
-  } = _ref;
-  return menuModel ? /*#__PURE__*/(0, _jsxRuntime.jsxs)(_jsxRuntime.Fragment, {
+const MenuMore = props => /*#__PURE__*/(0, _jsxRuntime.jsx)(_uiApi.IfTrue, {
+  v: props.menuModel,
+  children: /*#__PURE__*/(0, _jsxRuntime.jsxs)(_jsxRuntime.Fragment, {
     children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_SvgMore.default, {
-      refEl: refEl,
+      refEl: props.refEl,
       style: S_BT_MORE,
-      onClick: toggle
+      onClick: props.toggle
     }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_ModalSlider.ModalSlider, {
-      isShow: isMore,
+      isShow: props.isMore,
       className: CL_MENU_MORE,
-      model: menuModel,
-      onClose: toggle
+      model: props.menuModel,
+      onClose: props.toggle
     })]
-  }) : null;
-};
+  })
+});
 var _default = exports.default = MenuMore;
 //# sourceMappingURL=MenuMore.js.map
--- src/components/zhn-moleculs/MenuMore.jsx
+++ src/components/zhn-moleculs/MenuMore.jsx
@@ -1,6 +1,5 @@
-import {
-  crAbsoluteTopLeftStyle
-} from '../styleFn';
+import { IfTrue } from '../uiApi';
+import { crAbsoluteTopLeftStyle } from '../styleFn';
 
 import SvgMore from '../zhn/SvgMore';
 import { ModalSlider } from '../zhn-modal-slider/ModalSlider';
@@ -8,25 +7,22 @@ import { ModalSlider } from '../zhn-modal-slider/ModalSlider';
 const CL_MENU_MORE = 'dialog__menu-more'
 , S_BT_MORE = crAbsoluteTopLeftStyle(4, 2);
 
-const MenuMore = ({
-  refEl,
-  isMore,
-  menuModel,
-  toggle
-}) => menuModel ? (
-  <>
-    <SvgMore
-      refEl={refEl}
-      style={S_BT_MORE}
-      onClick={toggle}
-    />
-    <ModalSlider
-      isShow={isMore}
-      className={CL_MENU_MORE}
-      model={menuModel}
-      onClose={toggle}
-    />
-  </>
-) : null;
+const MenuMore = (props) => (
+  <IfTrue v={props.menuModel}>
+    <>
+      <SvgMore
+        refEl={props.refEl}
+        style={S_BT_MORE}
+        onClick={props.toggle}
+      />
+      <ModalSlider
+        isShow={props.isMore}
+        className={CL_MENU_MORE}
+        model={props.menuModel}
+        onClose={props.toggle}
+      />
+    </>
+  </IfTrue>
+)
 
 export default MenuMore
