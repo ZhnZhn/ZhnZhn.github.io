4035a5183d4458dcd03db4d4ceb7eef45f735b0d
Yevhenii Molodyi <eugmolody@gmail.com>
2025-05-16 12:24:38 +0300

    Add utils bindTo to charts dateFormat

--- js/charts/dateFormat.js
+++ js/charts/dateFormat.js
@@ -1,26 +1,17 @@
 "use strict";
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
 exports.__esModule = true;
 exports.toTdmyIf = exports.toTdmy = exports.toTd = exports.toDmy = exports.formatDate = void 0;
-
 var _highcharts = _interopRequireDefault(require("highcharts"));
-
-const format = _highcharts.default.dateFormat,
-      DMY_FORMAT = '%A, %b %d, %Y',
-      TDMY_FORMAT = '%H:%M, %A, %b %d, %Y',
-      TD_FORMAT = '%H:%M:%S %d-%m-%Y';
-const formatDate = format;
-exports.formatDate = formatDate;
-const toDmy = format.bind(null, DMY_FORMAT);
-exports.toDmy = toDmy;
-const toTdmy = format.bind(null, TDMY_FORMAT);
-exports.toTdmy = toTdmy;
-
-const toTdmyIf = mls => format('%H:%M', mls) === '00:00' ? toDmy(mls) : toTdmy(mls);
-
+var _bindTo = require("../utils/bindTo");
+const formatDate = exports.formatDate = _highcharts.default.dateFormat;
+const DMY_FORMAT_CONFIG = '%A, %b %d, %Y',
+  TDMY_FORMAT_CONFIG = '%H:%M, %A, %b %d, %Y',
+  TD_FORMAT_CONFIG = '%H:%M:%S %d-%m-%Y';
+const toDmy = exports.toDmy = (0, _bindTo.bindTo)(formatDate, DMY_FORMAT_CONFIG);
+const toTdmy = exports.toTdmy = (0, _bindTo.bindTo)(formatDate, TDMY_FORMAT_CONFIG);
+const toTdmyIf = mls => formatDate('%H:%M', mls) === '00:00' ? toDmy(mls) : toTdmy(mls);
 exports.toTdmyIf = toTdmyIf;
-const toTd = format.bind(null, TD_FORMAT);
-exports.toTd = toTd;
+const toTd = exports.toTd = (0, _bindTo.bindTo)(formatDate, TD_FORMAT_CONFIG);
 //# sourceMappingURL=dateFormat.js.map
--- src/charts/dateFormat.js
+++ src/charts/dateFormat.js
@@ -1,14 +1,15 @@
 import Highcharts from 'highcharts';
+import { bindTo } from '../utils/bindTo';
 
-const format = Highcharts.dateFormat
-, DMY_FORMAT = '%A, %b %d, %Y'
-, TDMY_FORMAT = '%H:%M, %A, %b %d, %Y'
-, TD_FORMAT = '%H:%M:%S %d-%m-%Y';
+export const formatDate = Highcharts.dateFormat
 
-export const formatDate = format
-export const toDmy = format.bind(null, DMY_FORMAT)
-export const toTdmy = format.bind(null, TDMY_FORMAT)
-export const toTdmyIf = mls => format('%H:%M', mls) === '00:00'
+const DMY_FORMAT_CONFIG = '%A, %b %d, %Y'
+, TDMY_FORMAT_CONFIG = '%H:%M, %A, %b %d, %Y'
+, TD_FORMAT_CONFIG = '%H:%M:%S %d-%m-%Y';
+
+export const toDmy = bindTo(formatDate, DMY_FORMAT_CONFIG)
+export const toTdmy = bindTo(formatDate, TDMY_FORMAT_CONFIG)
+export const toTdmyIf = mls => formatDate('%H:%M', mls) === '00:00'
   ? toDmy(mls)
   : toTdmy(mls)
-export const toTd = format.bind(null, TD_FORMAT)
+export const toTd = bindTo(formatDate, TD_FORMAT_CONFIG)
