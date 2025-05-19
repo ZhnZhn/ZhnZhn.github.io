2c672c93326abf04a2f05aee10bfe6bd64221715
Yevhenii Molodyi <eugmolody@gmail.com>
2025-05-19 16:03:27 +0300

    Add function _compareValue to components table compareFactory

--- src/components/zhn-table/compareFactory.js
+++ src/components/zhn-table/compareFactory.js
@@ -1,35 +1,44 @@
 const _isNaN = Number.isNaN;
 
-const _compareMaybeNaN = (v1, v2) => {
-  if (_isNaN(v1)) {
-    return _isNaN(v2) ? 0 : 1;
-  }
-  return _isNaN(v2) ? -1 : 2;
-};
+const _compareMaybeNaN = (
+  v1,
+  v2
+) => _isNaN(v1)
+  ? _isNaN(v2) ? 0 : 1
+  : _isNaN(v2) ? -1 : 2;
+
+const _compareValue = (
+  v1,
+  v2
+) => v1 < v2
+  ? 1
+  : v1 > v2
+  ? -1
+  : 0;
 
 const _compareNumber = (v1, v2) => {
   const _r = _compareMaybeNaN(v1, v2);
-  if (_r !== 2) return _r;
-  if ( v1 < v2 ) return 1;
-  if ( v1 > v2 ) return -1;
-  return 0;
+  return _r === 2
+    ? _compareValue(v1, v2)
+    : _r;
 };
 
-const _compareStr = (v1, v2) => {
-  if ( v1 < v2 ) return 1;
-  if ( v1 > v2 ) return -1;
-  return 0;
-};
-
-export const factoryCompareBy = (TOKEN_NAN, pn) => (a, b) => {
+export const factoryCompareBy = (
+  TOKEN_NAN,
+  pn
+) => (a, b) => {
   const v1 = a[pn], v2 = b[pn];
   return (typeof v1 === 'number' || v1 === TOKEN_NAN)
     ? _compareNumber(v1, v2)
-    : _compareStr(v1, v2);
+    : _compareValue(v1, v2);
 }
 
-export const factoryOpCompareBy = (pn, fn) => (a, b) => {
+export const factoryOpCompareBy = (
+  pn,
+  fn
+) => (a, b) => {
   const _r = _compareMaybeNaN(a[pn], b[pn]);
-  if (_r !== 2) return _r;
-  return fn(b, a);
+  return _r === 2
+    ? fn(b, a)
+    : _r;
 }
