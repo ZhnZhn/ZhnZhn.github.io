6400de90ba965446c3f1531daa6e70c8d29dbc85
Yevhenii Molodyi <eugmolody@gmail.com>
2026-01-07 15:50:57 +0200

    Add function isTokenInStr to flux stores chart fCompareBy module

--- js/flux/stores/chart/fCompareBy.js
+++ js/flux/stores/chart/fCompareBy.js
@@ -1,43 +1,29 @@
 "use strict";
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
 exports.__esModule = true;
 exports.default = void 0;
-
 var _big = _interopRequireDefault(require("big.js"));
-
 var _DirectionType = require("../../../constants/DirectionType");
-
+var _strFn = require("../../../utils/strFn");
 const MIN_STR = String(Number.MIN_SAFE_INTEGER);
 const ABS_PROP = 'Abs';
-
 const _getValueMoving = item => (item || {}).valueMoving || {};
-
 const _crBigForValue = (item, propName) => (0, _big.default)(_getValueMoving(item)[propName] || MIN_STR);
-
 const _crBigForAbsValue = (item, propName) => {
   const _b = _crBigForValue(item, propName),
-        {
-    direction
-  } = _getValueMoving(item);
-
+    {
+      direction
+    } = _getValueMoving(item);
   return direction === _DirectionType.DT_DOWN ? _b.times(-1) : _b;
 };
-
 const fCompareBy = propName => {
-  const _crBig = propName.indexOf(ABS_PROP) !== -1 ? _crBigForAbsValue : _crBigForValue;
-
+  const _crBig = (0, _strFn.isTokenInStr)(propName, ABS_PROP) ? _crBigForAbsValue : _crBigForValue;
   return (aC, bC) => {
     const a = _crBig(aC, propName),
-          b = _crBig(bC, propName);
-
-    if (a.gt(b)) return 1;
-    if (b.gt(a)) return -1;
-    return 0;
+      b = _crBig(bC, propName);
+    return a.gt(b) ? 1 : b.gt(a) ? -1 : 0;
   };
 };
-
-var _default = fCompareBy;
-exports.default = _default;
+var _default = exports.default = fCompareBy;
 //# sourceMappingURL=fCompareBy.js.map
--- src/flux/stores/chart/fCompareBy.js
+++ src/flux/stores/chart/fCompareBy.js
@@ -3,17 +3,26 @@ import Big from 'big.js'
 import {
   DT_DOWN
 } from '../../../constants/DirectionType';
+import {
+  isTokenInStr
+} from '../../../utils/strFn';
 
 const MIN_STR = String(Number.MIN_SAFE_INTEGER);
 const ABS_PROP = 'Abs';
 
-const _getValueMoving = item =>
-  (item || {}).valueMoving || {};
+const _getValueMoving = (
+  item
+)=> (item || {}).valueMoving || {};
 
-const _crBigForValue = (item, propName) =>
-  Big(_getValueMoving(item)[propName] || MIN_STR);
+const _crBigForValue = (
+  item,
+  propName
+) => Big(_getValueMoving(item)[propName] || MIN_STR);
 
-const _crBigForAbsValue = (item, propName) => {
+const _crBigForAbsValue = (
+  item,
+  propName
+) => {
   const _b = _crBigForValue(item, propName)
   , { direction } = _getValueMoving(item);
   return direction === DT_DOWN
@@ -22,16 +31,17 @@ const _crBigForAbsValue = (item, propName) => {
 };
 
 const fCompareBy = (propName) => {
-  const _crBig = propName.indexOf(ABS_PROP) !== -1
+  const _crBig = isTokenInStr(propName, ABS_PROP)
     ? _crBigForAbsValue
     : _crBigForValue;
   return (aC, bC) => {
     const a = _crBig(aC, propName)
     , b = _crBig(bC, propName);
-
-    if (a.gt(b)) return 1;
-    if (b.gt(a)) return -1;
-    return 0;
+    return a.gt(b)
+      ? 1
+      : b.gt(a)
+      ? -1
+      : 0;
   }
 };
 
