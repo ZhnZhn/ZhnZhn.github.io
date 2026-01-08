a11ca9282f4c3b61fe03dae772183dd91d1713da
Yevhenii Molodyi <eugmolody@gmail.com>
2026-01-08 12:11:30 +0200

    Improve details

--- src/components/AppErc.jsx
+++ src/components/AppErc.jsx
@@ -29,7 +29,7 @@ import DialogContainer from "./dialogs/DialogContainer";
 
 import checkBuild from "./checkBuild";
 
-const BUILD_DATE = "30-12-2025"
+const BUILD_DATE = "08-01-2026"
 , CL_COMP_CONTAINER = crFlexRowCn("app-container");
 
 const _showSettings = bindTo(
