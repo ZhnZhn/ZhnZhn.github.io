9cf86139eedd03b9153e41e1cd569be2c2f563a3
Yevhenii Molodyi <eugmolody@gmail.com>
2026-03-11 15:34:38 +0200

    Edit eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -46,9 +46,7 @@ module.exports = defineConfig([
   ],
      
   rules: {
-    //edit eslint rules
-    "for-direction": 0,
-
+    //edit eslint rules    
     "no-mixed-spaces-and-tabs": [2, "smart-tabs"],
     "no-console": 0,
     "no-whitespace-before-property": 2,
@@ -60,36 +58,16 @@ module.exports = defineConfig([
     "no-use-before-define": ["error", { "variables": false }],
    
     //set react rules
-    "react/display-name": 0,
-    "react/jsx-boolean-value": [1, "always"],
-    "react/jsx-no-undef": 0,
-
-    "react/jsx-sort-prop-types": 0,
-    "react/jsx-sort-props": 0,
+    "react/jsx-boolean-value": [1, "always"],    
     "react/jsx-uses-react": 1,
     "react/jsx-uses-vars": 1,
-
-    "react/no-did-mount-set-state": 0,
-    "react/no-did-update-set-state": 0,
-
-    "react/no-multi-comp": 0,
-    "react/no-unknown-property": 0,
-
-    "react/prop-types": 0,
-    "react/react-in-jsx-scope": 1,
-    "react/self-closing-comp": 1,
-
-    "react/sort-comp": 0,
+    
     "react/jsx-wrap-multilines": ["error", {
        "declaration": true,
        "assignment": true,
        "return": true,
        "arrow": true
-    }],
-
-    //jsx-transform
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off",
+    }],    
 
     //set react-hooks rules
     "react-hooks/rules-of-hooks": "error",
