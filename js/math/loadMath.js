70c65a4e784c58ce9d65ffba5d059f97ff016f5b
Yevhenii Molodyi <eugmolody@gmail.com>
2026-03-17 16:37:11 +0200

    Add JENKS named export to loadMath module

--- js/math/loadMath.js
+++ js/math/loadMath.js
@@ -1,10 +1,11 @@
 "use strict";
 
 exports.__esModule = true;
-exports.loadMath = exports.K_MEANS = void 0;
+exports.loadMath = exports.K_MEANS = exports.JENKS = void 0;
 var _crRouter = require("../utils/crRouter");
 function _interopRequireWildcard(e, t) { if ("function" == typeof WeakMap) var r = new WeakMap(), n = new WeakMap(); return (_interopRequireWildcard = function (e, t) { if (!t && e && e.__esModule) return e; var o, i, f = { __proto__: null, default: e }; if (null === e || "object" != typeof e && "function" != typeof e) return f; if (o = t ? n : r) { if (o.has(e)) return o.get(e); o.set(e, f); } for (const t in e) "default" !== t && {}.hasOwnProperty.call(e, t) && ((i = (o = Object.defineProperty) && Object.getOwnPropertyDescriptor(e, t)) && (i.get || i.set) ? o(f, t, i) : f[t] = e[t]); return f; })(e, t); }
 const K_MEANS = exports.K_MEANS = "kmeans";
+const JENKS = exports.JENKS = "jenks";
 const loadMath = exports.loadMath = (0, _crRouter.crGetRoute)({
   get [K_MEANS]() {
     /*eslint-disable no-undef */
@@ -15,6 +16,16 @@ const loadMath = exports.loadMath = (0, _crRouter.crGetRoute)({
     return Promise.resolve().then(() => _interopRequireWildcard(require(/* webpackChunkName: "math-kmeans" */
     /* webpackMode: "lazy" */
     "./k-means"))).then(module => module.default);
+  },
+  get [JENKS]() {
+    /*eslint-disable no-undef */
+    if (process.env.NODE_ENV === '_development') {
+      return Promise.resolve().then(() => _interopRequireWildcard(require("js/math/addJenksColorTo.js"))).then(module => module.default);
+    }
+    /*eslint-enable no-undef */
+    return Promise.resolve().then(() => _interopRequireWildcard(require(/* webpackChunkName: "math-jenks" */
+    /* webpackMode: "lazy" */
+    "./addJenksColorTo"))).then(module => module.default);
   }
 });
 //# sourceMappingURL=loadMath.js.map
--- src/math/loadMath.js
+++ src/math/loadMath.js
@@ -1,6 +1,7 @@
 import { crGetRoute } from '../utils/crRouter';
 
 export const K_MEANS = "kmeans"
+export const JENKS = "jenks"
 
 export const loadMath = crGetRoute({
   get [K_MEANS]() {
@@ -15,5 +16,18 @@ export const loadMath = crGetRoute({
        /* webpackMode: "lazy" */
        "./k-means"
      ).then(module => module.default)
-  }
+  },
+  get [JENKS]() {
+    /*eslint-disable no-undef */
+    if ( process.env.NODE_ENV === '_development') {
+      return import("js/math/addJenksColorTo.js")
+        .then(module => module.default)
+    }
+    /*eslint-enable no-undef */
+    return import(
+       /* webpackChunkName: "math-jenks" */
+       /* webpackMode: "lazy" */
+       "./addJenksColorTo"
+     ).then(module => module.default)
+  },
 })
