c4d9673b884668a76b1608b26ceb0822bb5aa078
Yevhenii Molodyi <eugmolody@gmail.com>
2026-04-16 16:08:52 +0300

    Add isNumber function to LimitRemaining function component

--- js/components/header/LimitRemainingLabel.js
+++ js/components/header/LimitRemainingLabel.js
@@ -2,18 +2,22 @@
 
 exports.__esModule = true;
 exports.default = void 0;
+var _isTypeFn = require("../../utils/isTypeFn");
 var _loadingStore = require("../../flux/stores/loadingStore");
 var _jsxRuntime = require("react/jsx-runtime");
 const S_LABEL = {
   display: 'inline-block',
   color: '#2f7ed8',
-  paddingRight: 8,
+  paddingRight: 5,
   fontSize: '16px',
   fontWeight: 'bold'
 };
-const LimitRemainingLabel = () => /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-  style: S_LABEL,
-  children: (0, _loadingStore.useLimitRemaining)()
-});
+const LimitRemainingLabel = () => {
+  const _limitRemaining = (0, _loadingStore.useLimitRemaining)();
+  return (0, _isTypeFn.isNumber)(_limitRemaining) ? /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+    style: S_LABEL,
+    children: _limitRemaining
+  }) : null;
+};
 var _default = exports.default = LimitRemainingLabel;
 //# sourceMappingURL=LimitRemainingLabel.js.map
--- src/components/header/LimitRemainingLabel.jsx
+++ src/components/header/LimitRemainingLabel.jsx
@@ -1,17 +1,19 @@
+import { isNumber } from '../../utils/isTypeFn';
 import { useLimitRemaining } from '../../flux/stores/loadingStore';
 
 const S_LABEL = {
   display: 'inline-block',
   color: '#2f7ed8',
-  paddingRight: 8,
+  paddingRight: 5,
   fontSize: '16px',
   fontWeight: 'bold'
 };
 
-const LimitRemainingLabel = () => (
-  <span style={S_LABEL}>
-    {useLimitRemaining()}
-  </span>
-);
+const LimitRemainingLabel = () => {
+  const _limitRemaining = useLimitRemaining();
+  return isNumber(_limitRemaining)
+    ? (<span style={S_LABEL}>{_limitRemaining}</span>)
+    : null
+}
 
 export default LimitRemainingLabel
