f3c4744cb47f7544b50de3efcc0abb40d3e841ef
Yevhenii Molodyi <eugmolody@gmail.com>
2026-09-04 16:22:13 +0300

    Add crProviderApi to InseeApi module

--- js/adapters/insee/InseeApi.js
+++ js/adapters/insee/InseeApi.js
@@ -4,10 +4,11 @@ exports.__esModule = true;
 exports.default = void 0;
 var _isTypeFn = require("../../utils/isTypeFn");
 var _itemFn = require("../../utils/itemFn");
+var _AdapterFn = require("../AdapterFn");
+var _ApiFn = require("../ApiFn");
 const DATA_URL = 'https://bdm.insee.fr/series/sdmx/data/SERIES_BDM';
 const _crUrl = option => `${DATA_URL}/${option.value}?startPeriod=${option.fromDate}`;
-const InseeApi = {
-  getRequestUrl(option) {
+const getRequestUrl = option => {
     const {
       items
     } = option;
@@ -18,7 +19,12 @@ const InseeApi = {
       return _crUrl(option);
     }
     return `${_crUrl(option)}&endPeriod=${option.toDate}`;
-  }
-};
+  },
+  checkResponse = str => {
+    if (!(0, _isTypeFn.isStr)(str)) {
+      throw (0, _AdapterFn.crError)();
+    }
+  },
+  InseeApi = (0, _ApiFn.crProviderApi)(getRequestUrl, checkResponse);
 var _default = exports.default = InseeApi;
 //# sourceMappingURL=InseeApi.js.map
--- src/adapters/insee/InseeApi.js
+++ src/adapters/insee/InseeApi.js
@@ -1,22 +1,41 @@
-import { isArr } from '../../utils/isTypeFn';
-import { getValue } from '../../utils/itemFn';
+import {
+  isArr,
+  isStr
+} from '../../utils/isTypeFn';
+import {
+  getValue
+} from '../../utils/itemFn';
+
+import {
+  crError
+} from '../AdapterFn';
+import {
+  crProviderApi
+} from '../ApiFn';
 
 const DATA_URL = 'https://bdm.insee.fr/series/sdmx/data/SERIES_BDM';
 const _crUrl = (
   option
 ) => `${DATA_URL}/${option.value}?startPeriod=${option.fromDate}`;
 
-const InseeApi = {
-  getRequestUrl(option) {
-    const { items } = option;
-    if (isArr(items)) {
-      const value = getValue(items[0]);
-      option.value = value
-      option.itemCaption = value
-      return _crUrl(option);
-    }
-    return `${_crUrl(option)}&endPeriod=${option.toDate}`;
+const getRequestUrl = (option) => {
+  const { items } = option;
+  if (isArr(items)) {
+    const value = getValue(items[0]);
+    option.value = value
+    option.itemCaption = value
+    return _crUrl(option);
   }
-};
+  return `${_crUrl(option)}&endPeriod=${option.toDate}`;
+}
+, checkResponse = (str) => {
+  if (!isStr(str)) {
+    throw crError();
+  }
+}
+, InseeApi = crProviderApi(
+  getRequestUrl,
+  checkResponse
+);
 
 export default InseeApi
