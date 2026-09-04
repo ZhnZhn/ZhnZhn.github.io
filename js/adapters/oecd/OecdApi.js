e7be4073f9980f5db1a32b52dd1173d0b72d3def
Yevhenii Molodyi <eugmolody@gmail.com>
2026-09-04 16:37:45 +0300

    Add crProviderApi to OecdApi module

--- js/adapters/oecd/OecdApi.js
+++ js/adapters/oecd/OecdApi.js
@@ -2,17 +2,14 @@
 
 exports.__esModule = true;
 exports.default = void 0;
+var _ApiFn = require("../ApiFn");
 var _CategoryFn = require("../CategoryFn");
 var _fnAdapter = require("./fnAdapter");
 const API_URL = "https://sdmx.oecd.org/public/rest/data";
-const OecdApi = {
-  getRequestUrl(option) {
-    const {
-        time
-      } = option,
-      queryDate = (0, _CategoryFn.isCategory)(option) ? `startPeriod=${time}&endPeriod=${time}` : "startPeriod=2005";
+const getRequestUrl = option => {
+    const queryDate = (0, _CategoryFn.isCategory)(option) ? `startPeriod=${time}&endPeriod=${option.time}` : "startPeriod=2005";
     return `${API_URL}/${option.dfDs}/${(0, _fnAdapter.crItemId)(option)}?${queryDate}&format=jsondata`;
-  }
-};
+  },
+  OecdApi = (0, _ApiFn.crProviderApi)(getRequestUrl, null);
 var _default = exports.default = OecdApi;
 //# sourceMappingURL=OecdApi.js.map
--- src/adapters/oecd/OecdApi.js
+++ src/adapters/oecd/OecdApi.js
@@ -1,18 +1,19 @@
+import { crProviderApi } from '../ApiFn';
 import { isCategory } from '../CategoryFn';
+
 import { crItemId } from './fnAdapter';
 
 const API_URL = "https://sdmx.oecd.org/public/rest/data";
 
-const OecdApi = {
-  getRequestUrl(option){
-    const {
-      time
-    } = option
-    , queryDate = isCategory(option)
-        ? `startPeriod=${time}&endPeriod=${time}`
-        : "startPeriod=2005";
-    return `${API_URL}/${option.dfDs}/${crItemId(option)}?${queryDate}&format=jsondata`;
-  }
-};
+const getRequestUrl = (option) => {
+  const queryDate = isCategory(option)
+    ? `startPeriod=${time}&endPeriod=${option.time}`
+    : "startPeriod=2005";
+  return `${API_URL}/${option.dfDs}/${crItemId(option)}?${queryDate}&format=jsondata`;
+}
+, OecdApi = crProviderApi(
+  getRequestUrl,
+  null
+);
 
 export default OecdApi
