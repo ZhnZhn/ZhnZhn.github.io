42f022223a47bf0b00c511d8bdc1f36c217595e4
Yevhenii Molodyi <eugmolody@gmail.com>
2026-09-04 16:51:34 +0300

    Add crProviderApi function to BisApi module

--- js/adapters/bis/BisApi.js
+++ js/adapters/bis/BisApi.js
@@ -3,11 +3,11 @@
 exports.__esModule = true;
 exports.default = void 0;
 var _AdapterFn = require("../AdapterFn");
+var _ApiFn = require("../ApiFn");
 var _CategoryFn = require("../CategoryFn");
 var _fnAdapter = require("./fnAdapter");
 const BIS_API = "https://stats.bis.org/api/v2/data/dataflow/BIS";
-const BisApi = {
-  getRequestUrl(option) {
+const getRequestUrl = option => {
     const _isCategoryChart = (0, _CategoryFn.isCategory)(option),
       queryToken = _isCategoryChart ? `c%5BTIME_PERIOD%5D=${option.time}` : `c%5BTIME_PERIOD%5D=ge%3A${option.fromDate}`;
     if (option.dfTopic === "XRU") {
@@ -17,7 +17,7 @@ const BisApi = {
       }
     }
     return `${option.proxy}${BIS_API}/${option.dfCase}/1.0/${(0, _fnAdapter.crItemId)(option)}?${queryToken}`;
-  }
-};
+  },
+  BisApi = (0, _ApiFn.crProviderApi)(getRequestUrl, _ApiFn.checkResponseIsStr);
 var _default = exports.default = BisApi;
 //# sourceMappingURL=BisApi.js.map
--- src/adapters/bis/BisApi.js
+++ src/adapters/bis/BisApi.js
@@ -1,25 +1,37 @@
-import { setItemCaptionCurrencyRateTo } from '../AdapterFn';
-import { isCategory } from '../CategoryFn';
-import { crItemId } from './fnAdapter';
+import {
+  setItemCaptionCurrencyRateTo
+} from '../AdapterFn';
+import {
+  crProviderApi,
+  checkResponseIsStr
+} from '../ApiFn';
+import {
+  isCategory 
+} from '../CategoryFn';
+import {
+  crItemId
+} from './fnAdapter';
 
 const BIS_API = "https://stats.bis.org/api/v2/data/dataflow/BIS";
 
-const BisApi = {
-  getRequestUrl(option){
-    const _isCategoryChart = isCategory(option)
-    , queryToken = _isCategoryChart
-      ? `c%5BTIME_PERIOD%5D=${option.time}`
-      : `c%5BTIME_PERIOD%5D=ge%3A${option.fromDate}`;
+const getRequestUrl = (option) => {
+  const _isCategoryChart = isCategory(option)
+  , queryToken = _isCategoryChart
+    ? `c%5BTIME_PERIOD%5D=${option.time}`
+    : `c%5BTIME_PERIOD%5D=ge%3A${option.fromDate}`;
 
-    if (option.dfTopic === "XRU") {
-      setItemCaptionCurrencyRateTo(option, "USD")
-      if (_isCategoryChart) {
-        option.items[1].v = "M"
-      }
+  if (option.dfTopic === "XRU") {
+    setItemCaptionCurrencyRateTo(option, "USD")
+    if (_isCategoryChart) {
+      option.items[1].v = "M"
     }
-
-    return `${option.proxy}${BIS_API}/${option.dfCase}/1.0/${crItemId(option)}?${queryToken}`;
   }
-};
+
+  return `${option.proxy}${BIS_API}/${option.dfCase}/1.0/${crItemId(option)}?${queryToken}`;
+}
+, BisApi = crProviderApi(
+  getRequestUrl,
+  checkResponseIsStr
+);
 
 export default BisApi
